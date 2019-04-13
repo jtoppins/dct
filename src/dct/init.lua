f6965fc5a88de5cdc5298c8de3800510492d1a7a
terrificfool <rthomasstevens@gmail.com>
2019-04-13 18:47:04 -0400

    Added init.lua

---
+++ src/dct/init.lua
@@ -0,0 +1,24 @@
+--[[
+Top level initialization and kickoff package
+
+----
+init function initializes game state
+
+----
+start/run function starts the DCT engine
+
+]]--
+local init = {}
+function init.init (dctsettings)
+
+  --Parse the dctsettings and act on them
+  
+  --Check to see if there's a game state
+  --If yes, load it
+  --If no, generate a new one and save it out
+  
+  --Call some function that kicks off the campaign engine
+  
+  return true
+end
+return init
