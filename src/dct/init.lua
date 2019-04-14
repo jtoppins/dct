2cf89e303648705b38e90361caf54fd8a3babfad
Jonathan Toppins <jtoppins@users.sourceforge.net>
2019-04-14 01:29:51 -0400

    init: cleanup whitespace

--- src/dct/init.lua
+++ src/dct/init.lua
@@ -14,24 +14,23 @@ local template = require("dct.template")
 
 local init = {}
 function init.init (dctsettings)
+	--Parse the dctsettings and act on them
 
-  --Parse the dctsettings and act on them
-  
-  --Check to see if there's a game state
-  --If yes, load it
-  --If no, generate a new one and save it out
-  
-  --Call some function that kicks off the campaign engine
-  init.testSpawn()
-  return true
+	--Check to see if there's a game state
+	--If yes, load it
+	--If no, generate a new one and save it out
+
+	--Call some function that kicks off the campaign engine
+	init.testSpawn()
+	return true
 end
 
 function init.testSpawn ()
-  local stmFile = lfs.writedir() .. "DctTemplates\\test.stm"
-  local dctfile = lfs.writedir() .. "DctTemplates\\test.dct"
-  
-  local t = template.Template(stmFile, dctFile)
-  t:spawn()
-  return true
+	local stmFile = lfs.writedir() .. "DctTemplates\\test.stm"
+	local dctfile = lfs.writedir() .. "DctTemplates\\test.dct"
+
+	local t = template.Template(stmFile, dctFile)
+	t:spawn()
+	return true
 end
 return init
