982fa100a9dc71dde06761898af87b9c33ba04c5
terrificfool <rthomasstevens@gmail.com>
2019-04-13 20:48:17 -0400

    Updated files to support spawning a template test case.

--- src/dct/init.lua
+++ src/dct/init.lua
@@ -8,6 +8,10 @@ init function initializes game state
 start/run function starts the DCT engine
 
 ]]--
+--require("os")
+
+local template = require("dct.template")
+
 local init = {}
 function init.init (dctsettings)
 
@@ -18,7 +22,18 @@ function init.init (dctsettings)
   --If no, generate a new one and save it out
   
   --Call some function that kicks off the campaign engine
+  env.info("About to test this testSpawn")
+  init.testSpawn()
+  return true
+end
+
+function init.testSpawn ()
+  env.info("Testing this testSpawn")
+  local stmFile = lfs.writedir() .. "DctTemplates\\test.stm"
+  local dctfile = lfs.writedir() .. "DctTemplates\\test.dct"
   
+  local t = template.Template(stmFile, dctFile)
+  t:spawn()
   return true
 end
 return init
