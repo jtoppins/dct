jtoppins/dct

commit 270c135cc0729f0c22539c55dac1ae4a03ce200d

template: fix spawning groups
The MSE coalition.addGroup() uses a number for the category id.

src/dct/template.lua
@@ -44,6 +44,14 @@ do
 	local utils    = require("libs.utils")
 	--local settings = require("dct.settings")
 
+	local categorymap = {
+		["HELICOPTER"] = 'HELICOPTER',
+		["SHIP"]       = 'SHIP',
+		["VEHICLE"]    = 'GROUND_UNIT',
+		["PLANE"]      = 'AIRPLANE',
+		["STATIC"]     = 'STRUCTURE',
+	}
+
 	local function lookupname(name, namelist)
 		namelist = namelist or {}
 		if namelist[name] ~= nil then
@@ -81,7 +89,9 @@ do
 		assert(id)
 		assert(group)
 		assert(category)
-		coalition.addGroup(group.countryid, category, group.data)
+		coalition.addGroup(group.countryid,
+				   Unit.Category[categorymap[string.upper(category)]],
+				   group.data)
 	end
 	local function spawnStatic(id, static)
 		assert(id)
@@ -150,14 +160,14 @@ do
 		end
 	end
 	function Template:__processCountry(side, ctry, names)
-		local categories = {"ship", "vehicle", "air"}
 		local ctx = {}
 		ctx.side      = side
 		ctx.category  = "none"
 		ctx.countryid = ctry.id
 		ctx.names     = names
 
-		for i, cat in ipairs(categories) do
+		for cat, _ in pairs(categorymap) do
+			cat = string.lower(cat)
 			if ctry[cat] and type(ctry[cat]) == 'table' and
 			   ctry[cat].group then
 				ctx.category = cat
@@ -229,8 +239,18 @@ do
 			      ctx)
 		tbl.countryid = ctx.countryid
 		-- TODO: setup metadata like parsing group/unit names
-		-- TODO: now override group/unit names to be unique to the
-		-- 	 template
+
+
+		-- now override group/unit names to be unique to the
+		-- template
+		tbl.data.name = self.name .. " " .. ctx.category .. " " ..
+				#self.tpldata[ctx.side][ctx.category]
+		utils.foreach(tbl.data.units,
+			ipairs,
+			function(i, obj, base)
+				obj.name = base .. "-" .. i
+			end,
+			tbl.data.name)
 		table.insert(self.tpldata[ctx.side][ctx.category], tbl)
 	end
 	function Template:__createTable(ctx)

tests/test-template.lua
@@ -1,17 +1,24 @@
 require("os")
-
-local template = require("dct.template")
+require("testlibs.test")
+require("testlibs.dcsstubs")
 local json = require("libs.json")
+local template = require("dct.template")
+
+local check = {}
+check.spawngroups  = 0
+check.spawnstatics = 0
 
 coalition = {}
 function coalition.addGroup(cntry, cat, data)
-	print("SPAWN: spawn group, type:" .. cat .. ", name: " .. data.name)
-	--print(json:encode_pretty(data))
+	test.debug("SPAWN: spawn group, type:" .. cat .. ", name: " .. data.name)
+	test.debug(json:encode_pretty(data))
+	check.spawngroups = check.spawngroups + 1
 end
 
 function coalition.addStaticObject(cntry, data)
-	print("SPAWN: spawn static, type:" .. type(data) .. ", name: " .. data.name)
-	--print(json:encode_pretty(data))
+	test.debug("SPAWN: spawn static, type:" .. type(data) .. ", name: " .. data.name)
+	test.debug(json:encode_pretty(data))
+	check.spawnstatics = check.spawnstatics + 1
 end
 
 local function main()
@@ -19,8 +26,9 @@ local function main()
 	local dctfile = "./data/test.dct"
 
 	local t = template.Template(stmfile, dctfile)
-	--print(json:encode_pretty(t))
 	t:spawn()
+	assert(check.spawngroups == 1, "group spawn broken")
+	assert(check.spawnstatics == 22, "static spawn broken")
 	return 0
 end
 

tests/testlibs/dcsstubs.lua
@@ -1,0 +1,8 @@
+Unit = {}
+Unit.Category = {
+	["AIRPLANE"]    = 0,
+	["HELICOPTER"]  = 1,
+	["GROUND_UNIT"] = 2,
+	["SHIP"]        = 3,
+	["STRUCTURE"]   = 4,
+}

tests/testlibs/test.lua
@@ -1,0 +1,6 @@
+test = {}
+function test.debug(str)
+	if os.getenv("DCT_DEBUG") then
+		print("DEBUG: " .. str)
+	end
+end

commit 9e4770816bd0ab07e49ad1375cd2884f5994c12e

template: fixup reading in static templates
The Template class was double counting statics, now the correct number
of statics will be created when spawned.

Fixes: 270c135cc072 ("template: fix spawning groups")

src/dct/template.lua
@@ -167,18 +167,16 @@ function Template:__processCountry(side, ctry, names)
 		if ctry[cat] and type(ctry[cat]) == 'table' and
 		   ctry[cat].group then
 			ctx.category = cat
-			self:__processGroups(ctry[cat].group,
-					     self.__addOneGroup,
-					     ctx)
+			if cat == "static" then
+				self:__processGroups(ctry.static.group,
+						self.__addOneStatic,
+						ctx)
+			else
+				self:__processGroups(ctry[cat].group,
+						self.__addOneGroup,
+						ctx)
+			end
 		end
-	end
-
-	if ctry.static and type(ctry.static) == 'table' and
-	   ctry.static.group then
-		ctx.category = "static"
-		self:__processGroups(ctry.static.group,
-				     self.__addOneStatic,
-				     ctx)
 	end
 end
 

tests/test-template.lua
@@ -28,7 +28,7 @@ local function main()
 	local t = template.Template(stmfile, dctfile)
 	t:spawn()
 	assert(check.spawngroups == 1, "group spawn broken")
-	assert(check.spawnstatics == 22, "static spawn broken")
+	assert(check.spawnstatics == 11, "static spawn broken")
 	return 0
 end
 