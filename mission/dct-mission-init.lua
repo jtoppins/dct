ad00059c368593166cef1fd4c790f253604c4c38
Jonathan Toppins <jtoppins@users.sourceforge.net>
2019-05-01 19:31:13 -0400

    dct: move the error message so it doesn't exist forever
    Also added some comments to note how the file should be used.

--- mission/dct-mission-init.lua
+++ mission/dct-mission-init.lua
@@ -1,18 +1,25 @@
--- SPDX-License-Identifier: LGPL-3.0
 --[[
--- test and verify the server's environment supports the calls
--- required by DCT framework
+-- SPDX-License-Identifier: LGPL-3.0
+--
+-- DCT mission script
+--
+-- This script is intended to be included in a DCS mission file via
+-- the trigger system. This file will test and verify the server's
+-- environment supports the calls required by DCT framework. It will
+-- then setup and start the framework.
 --]]
-local assertmsg = "DCT requires DCS mission scripting environment to be" ..
-			" modified, the file needing to be changed can be found at" ..
-			" $DCS_ROOT\\Scripts\\MissionScripting.lua. Comment out the" ..
-			" removal of lfs and io and the setting of 'require' to nil."
+
 if not lfs or not io or not require then
+	local assertmsg = "DCT requires DCS mission scripting environment to be" ..
+		" modified, the file needing to be changed can be found at" ..
+		" $DCS_ROOT\\Scripts\\MissionScripting.lua. Comment out the" ..
+		" removal of lfs and io and the setting of 'require' to nil."
 	assert(false, assertmsg)
 end
 
--- Provide some default settings if the mission maker doesn't want
--- to add any
+-- 'dctsettings' can be defined in the mission to override any of the
+-- possible dct settings. Including 'luapath' which can override
+-- the package path location.
 local s = dctsettings or {}
 if s.luapath == nil then
 	s.luapath = lfs.writedir() .. "Scripts\\?.lua;"
