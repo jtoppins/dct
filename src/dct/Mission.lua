adbdfd1769bb17d2903affdfc6b555bc1989be43
Jonathan Toppins <jtoppins@users.noreply.github.com>
2019-12-23 10:12:02 -0500

    mission: stubs
    Create stubs for all Mission class interfaces.

--- src/dct/Mission.lua
+++ src/dct/Mission.lua
@@ -1,16 +1,12 @@
 --[[
 -- SPDX-License-Identifier: LGPL-3.0
 --
--- Represents a Mission within the game and this associates and
+-- Represents a Mission within the game and this associates an
 -- Objective to as assigned group of units responsible for
 -- completing the Objective.
 --]]
 
 -- TODO: a mission consists of
---  * a target asset
---  * an owner - which AI commander initiated the mission and whom's assets
---    will be assigned to complete the mission
---  * a list of assigned assets to accomplish this mission
 --  * 'success' critera for each side, so which ever side's success
 --    critera is met first is the one that 'wins' that mission.
 --
@@ -32,21 +28,62 @@
 --    could be an option to override the release and go without CAP.
 --    This I think would engourage teamwork without forcing it.
 
-local class = require("libs.class")
-local Observable = require("observable")
+-- TODO: issues
+--  * how does a mission access the global asset manager?
+--	  - give a reference
+--  * what stats does a mission need to track?
+--    - timeout
+--    - world state of mission
+--  * does a mission need to "listen" to events from a target
+--    or assigned assets?
+--    - probably not, just makes things complicated instead
+--      periodically check missions in a commander
 
-local Mission = class(Observable)
-function Mission:__init(groupName, objective)
-	Observable.__init(self)
+local class = require("libs.class")
+
+local MISSION_LIMIT = 60*60*3  -- 3 hours in seconds
+
+local function gen_mission_id()
+	return "A1234"
+end
+
+local Mission = class()
+function Mission:__init(cmdr, missiontype, grpname, tgtname)
+	-- reference to owning commander
+	self.cmdr      = cmdr
+	self.id        = gen_mission_id()
+	self.type      = missiontype
+	self.target    = tgtname
+	self.assigned  = grpname
+	self.timestart = timer.getTime()
+	self.timeend   = self.timestart + MISSION_LIMIT
+	self.breifing  = ""
+
+	--local tgt = self.cmdr:getAsset(self.target)
+	-- TODO: setup remaining mission parameters
 end
 
 function Mission:getID()
+	return self.id
 end
 
+--[[
+-- Abort - aborts a mission putting the targeted asset back into
+--   the pool.
+--
+-- Things that need to be managed;
+--  * removing the mission from the owning commander's mission
+--    list(s)
+--  * releasing the targeted asset by resetting the asset's targeted
+--    bit
+--]]
 function Mission:abort()
-end
-
-function Mission:join(groupName)
+	self.cmdr:removeMission(self.id)
+	local tgt = self.cmdr:getAsset(self.target)
+	if tgt then
+		tgt:setTargeted(false)
+	end
+	return self.id
 end
 
 function Mission:update()
@@ -56,15 +93,40 @@ end
 function Mission:isComplete()
 end
 
---[[
--- API used by Missions assigned to humans
---]]
-
-function Mission:reportBriefing()
+-- provides target info information
+-- Data supplied:
+--   * target location - centroid of the asset
+--   * target callsign
+--   * target description - not the mission description, a short two word
+--       description of the individual target like;
+--       factory, ammo bunker, etc.
+--   * target status - a numercal value from 0 to 100 representing
+--       percentage completion
+function Mission:getTargetInfo()
+	local tgtinfo = {}
+	tgtinfo.location = { ["x"] = 100, ["z"] = 100 }
+	tgtinfo.callsign = "test-callsign"
+	tgtinfo.status = 50
+	return tgtinfo
 end
 
-function Mission:reportStatus()
+function Mission:getTimeout()
+	return self.timeend
 end
 
-function Mission:reportAssigned()
+function Mission:addTime(time)
+	self.timeend = self.timeend + time
+	return time
 end
+
+function Mission:checkin(time)
+end
+
+function Mission:checkout(time)
+end
+
+function Mission:getDescription()
+	return "TODO description"
+end
+
+return Mission
