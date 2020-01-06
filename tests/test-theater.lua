c74aa598c8e9e24320bf5ec95670a719ef449cf2
Jonathan Toppins <jtoppins@users.noreply.github.com>
2020-01-06 02:19:31 -0500

    tests-theater: add ATO restriction test

--- tests/test-theater.lua
+++ tests/test-theater.lua
@@ -9,14 +9,12 @@ local function main()
 	assert(dctcheck.spawngroups == 1, "group spawn broken")
 	assert(dctcheck.spawnstatics == 11, "static spawn broken")
 
-	--[[
 	local restriction =
 		theater:getATORestrictions(coalition.side.BLUE, "A-10C")
-	print(require("libs.json"):encode_pretty(restriction))
-	restriction =
-		theater:getATORestrictions(coalition.side.BLUE, "F-15C")
-	print(require("libs.json"):encode_pretty(restriction))
-	--]]
+	local validtbl = { ["BAI"] = 5, ["CAS"] = 1, }
+	for k, v in pairs(restriction) do
+		assert(validtbl[k] == v, "ATO Restriction error")
+	end
 	return 0
 end
 
