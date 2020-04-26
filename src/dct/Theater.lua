jtoppins/dct

commit d3420705b2610709c793ad43ce74bc145a7d2c14

save the date on which the state file was created
Closes: #40

src/dct/Theater.lua
@@ -56,6 +56,7 @@ function Theater:__init()
 	self.assetmgr  = AssetManager(self)
 	self.cmdrs     = {}
 	self.playergps = {}
+	self.startdate = os.date("*t")
 
 	for _, val in pairs(coalition.side) do
 		self.cmdrs[val] = Commander(self, val)
@@ -181,6 +182,7 @@ function Theater:export(_)
 		["theater"]  = env.mission.theatre,
 		["sortie"]   = env.getValueDictByKey(env.mission.sortie),
 		["assetmgr"] = self:getAssetMgr():marshal(),
+		["startdate"] = self.startdate
 	}
 
 	statefile:write(json:encode_pretty(exporttbl))

commit eeb133602d672aac1428b9255c20d15349ec57bd

theater: restore start date
When the state is reloaded the start date was not restored. Causing the
state date to always be regenerated.

Fixes: d3420705b261 ("save the date on which the state file was
created")

src/dct/Theater.lua
@@ -147,6 +147,7 @@ end
 
 function Theater:_initFromState()
 	self.statef = true
+	self.startdate = self.statetbl.startdate
 	self:getAssetMgr():unmarshal(self.statetbl.assetmgr)
 end
 