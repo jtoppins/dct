jtoppins/dct

commit 2070317ff04e5a43f16c6737496b42c6d92acbca

modify storage of groups in templates
This changes the storage format from a set of arrays based on
category type to a single list of group entries. Also, expose
'processCoalition' in STM module so that is can be reused to
process things like .miz files.

src/dct/dcscollections/StaticCollection.lua
@@ -11,7 +11,6 @@ local class    = require("libs.class")
 local utils    = require("libs.utils")
 local Logger   = require("dct.Logger").getByName("Asset")
 local dctutils = require("dct.utils")
-local STM      = require("dct.STM")
 local Goal     = require("dct.Goal")
 local IDCSObjectCollection = require("dct.dcscollections.IDCSObjectCollection")
 local settings = _G.dct.settings
@@ -135,11 +134,10 @@ end
 -- asset. This list will be needed later to save state.
 --]]
 function StaticCollection:_setup()
-	for cat_idx, cat_data in pairs(self._tpldata) do
-		for _, grp in ipairs(cat_data) do
-			self:_setupDeathGoal(grp.data, cat_idx == 'static')
-			self._assets[grp.data.name] = grp.data
-		end
+	for _, grp in ipairs(self._tpldata) do
+		self:_setupDeathGoal(grp.data,
+			grp.category == Unit.Category.STRUCTURE)
+		self._assets[grp.data.name] = grp.data
 	end
 	assert(next(self._deathgoals) ~= nil,
 		"runtime error: StaticCollection must have a deathgoal: "..
@@ -259,16 +257,12 @@ local function removeDCTKeys(grp)
 end
 
 function StaticCollection:_spawn()
-	for cat_idx, cat_data in pairs(self._tpldata) do
-		for _, grp in ipairs(cat_data) do
-			local gcpy = removeDCTKeys(grp)
-			if cat_idx == 'static' then
-				coalition.addStaticObject(gcpy.countryid, gcpy.data)
-			else
-				coalition.addGroup(gcpy.countryid,
-					Unit.Category[STM.categorymap[string.upper(cat_idx)]],
-					gcpy.data)
-			end
+	for _, grp in ipairs(self._tpldata) do
+		local gcpy = removeDCTKeys(grp)
+		if gcpy.category == Unit.Category.STRUCTURE then
+			coalition.addStaticObject(gcpy.countryid, gcpy.data)
+		else
+			coalition.addGroup(gcpy.countryid, gcpy.category, gcpy.data)
 		end
 	end
 
@@ -340,14 +334,8 @@ end
 local function filterTemplateData(tpldata)
 	local cpytbl = {}
 
-	for cat_idx, cat_data in pairs(tpldata) do
-		cpytbl[cat_idx] = {}
-		for _, grp in ipairs(cat_data) do
-			filterDeadObjects(cpytbl[cat_idx], grp)
-		end
-		if not next(cpytbl[cat_idx]) then
-			cpytbl[cat_idx] = nil
-		end
+	for _, grp in ipairs(tpldata) do
+		filterDeadObjects(cpytbl, grp)
 	end
 	if not next(cpytbl) then
 		cpytbl = nil

src/dct/STM.lua
@@ -15,107 +15,86 @@ local categorymap = {
 	["STATIC"]     = 'STRUCTURE',
 }
 
-local function lookupname(name, names)
-	assert(name and type(name) == "string", "name must be provided")
-	local newname = name
-	local namelist = names or {}
-	if namelist[name] ~= nil then
-		newname = namelist[name]
-	end
-	return newname
-end
-
-local function createTable(tpl, ctx)
-	local data = tpl.tpldata
-	if data[ctx.category] == nil then
-		data[ctx.category] = {}
-	end
-end
-
-local function convertNames(data, names)
-	data.name = lookupname(data.name, names)
+local function convertNames(data, namefunc)
+	data.name = namefunc(data.name)
 
 	for _, unit in ipairs(data.units or {}) do
-		unit.name = lookupname(unit.name, names)
+		unit.name = namefunc(unit.name)
 	end
 
 	if data.route then
 		for _, wypt in ipairs(data.route.points or {}) do
-			wypt.name = lookupname(wypt.name, names)
+			wypt.name = namefunc(wypt.name)
 		end
 	end
 end
 
-local function addOneStatic(tpl, _, grp, ctx)
-	local tbl = {}
-	createTable(tpl, ctx)
-	tbl.data      = grp.units[1]
-	tbl.data.dead = grp.dead
-	tbl.countryid = ctx.countryid
-	convertNames(tbl.data, tpl.names)
-	table.insert(tpl.tpldata[ctx.category], tbl)
+local function modifyStatic(grpdata, _, dcscategory)
+	if dcscategory ~= Unit.Category.STRUCTURE then
+		return grpdata
+	end
+	local grpcpy = utils.deepcopy(grpdata.units[1])
+	grpcpy.dead = grpdata.dead
+	return grpcpy
 end
 
-local function addOneGroup(tpl, _, grp, ctx)
-	local tbl = {}
-	createTable(tpl, ctx)
-	tbl.data = grp
-	tbl.countryid = ctx.countryid
-	convertNames(tbl.data, tpl.names)
-	table.insert(tpl.tpldata[ctx.category], tbl)
-end
-
-local function processCountry(tpl, side, ctry)
-	local funcmap = {
-		["HELICOPTER"] = addOneGroup,
-		["SHIP"]       = addOneGroup,
-		["VEHICLE"]    = addOneGroup,
-		["PLANE"]      = addOneGroup,
-		["STATIC"]     = addOneStatic,
-	}
-
-	local ctx = {}
-	ctx.side      = side
-	ctx.category  = "none"
-	ctx.countryid = ctry.id
-
-	for cat, _ in pairs(categorymap) do
-		cat = string.lower(cat)
-		if ctry[cat] and type(ctry[cat]) == 'table' and
-		   ctry[cat].group then
-			if tpl.coalition == nil then
-				tpl.coalition = side
+local function processCategory(grplist, cattbl, cntryid, dcscategory, ops)
+	if type(cattbl) ~= 'table' or cattbl.group == nil then
+		return
+	end
+	for _, grp in ipairs(cattbl.group) do
+		if ops.grpfilter == nil or
+			ops.grpfilter(grp, cntryid, dcscategory) == true then
+			if type(ops.grpmodify) == 'function' then
+				grp = ops.grpmodify(grp, cntryid, dcscategory)
 			end
-			assert(tpl.coalition == side, "country("..ctry.id..") does not"..
-				" belong to template.coalition("..tpl.coalition.."),"..
-				" country belongs to "..side)
-			ctx.category = cat
-			utils.foreach(tpl,
-				ipairs,
-				funcmap[string.upper(cat)],
-				ctry[cat].group,
-				ctx)
+			local grptbl = {
+				["data"]      = utils.deepcopy(grp),
+				["countryid"] = cntryid,
+				["category"]  = dcscategory,
+			}
+			convertNames(grptbl.data, ops.namefunc)
+			table.insert(grplist, grptbl)
 		end
 	end
 end
 
-local function processCoalition(tpl, side, coa)
-	if side ~= 'red' and side ~= 'blue' and
-	   type(coa) ~= 'table' then
-		return
-	end
-
-	if coa.country == nil or type(coa.country) ~= 'table' then
-		return
-	end
-
-	for _, ctry_data in ipairs(coa.country) do
-		side = coalition.getCountryCoalition(ctry_data.id)
-		processCountry(tpl, side, ctry_data)
-	end
-end
 
 local STM = {}
+
+-- return all groups matching `grpfilter` from `tbl`
+-- grpfilter(grpdata, countryid, Unit.Category)
+--   returns true if the filter matches and the group entry should be kept
+-- grpmodify(grpdata, countryid, Unit.Category)
+--   returns a copy of the group data modified as needed
+-- always returns a table, even if it is empty
+function STM.processCoalition(tbl, namefunc, grpfilter, grpmodify)
+	assert(type(tbl) == 'table', "value error: `tbl` must be a table")
+	assert(tbl.country ~= nil and type(tbl.country) == 'table',
+		"value error: `tbl` must have a member `country` that is a table")
+
+	local grplist = {}
+	if namefunc == nil then
+		namefunc = env.getValueDictByKey
+	end
+	local ops = {
+		["namefunc"] = namefunc,
+		["grpfilter"] = grpfilter,
+		["grpmodify"] = grpmodify,
+	}
+
+	for _, cntrytbl in ipairs(tbl.country) do
+		for cat, unitcat in pairs(categorymap) do
+			processCategory(grplist,
+				cntrytbl[string.lower(cat)],
+				cntrytbl.id,
+				Unit.Category[unitcat],
+				ops)
+		end
+	end
+	return grplist
+end
+
 
 --[[
 -- Convert STM data format
@@ -134,30 +113,52 @@ local STM = {}
 -- to an internal, simplier, storage format
 --
 --    tpldata = {
---      category = {
---        # = {
---          countryid = id,
---          data      = {
---            # group def members
+--      [#] = {
+--        category  = Unit.Category[STM_category],
+--        countryid = id,
+--        data      = {
+--            # group definition
 --            dct_deathgoal = goalspec
---    }}}}
---
--- where category == {vehicle, plane, ...}
---
+--    }}}
 --]]
 
 function STM.transform(stmdata)
 	local template   = {}
-	template.names   = stmdata.localization.DEFAULT
-	template.name    = lookupname(stmdata.name, template.names)
-	template.theater = lookupname(stmdata.theatre, template.names)
-	template.desc    = lookupname(stmdata.desc, template.names)
+	local lookupname =  function(name)
+		assert(name and type(name) == "string",
+			"value error: name must be provided and a string")
+		local newname = name
+		local namelist = stmdata.localization.DEFAULT
+		if namelist[name] ~= nil then
+			newname = namelist[name]
+		end
+		return newname
+	end
+	local trackUniqueCoalition = function(_, cntryid, _)
+		local side = coalition.getCountryCoalition(cntryid)
+		if template.coalition == nil then
+			template.coalition = side
+		end
+		assert(template.coalition == side,
+			"runtime error: invalid template STM; country("..cntryid..")"..
+			" does not belong to template.coalition("..template.coalition..
+			"), country belongs to "..side)
+		return true
+	end
+
+	template.name    = lookupname(stmdata.name)
+	template.theater = lookupname(stmdata.theatre)
+	template.desc    = lookupname(stmdata.desc)
 	template.tpldata = {}
 
-	for coa_key, coa_data in pairs(stmdata.coalition) do
-		processCoalition(template, coa_key, coa_data)
+	for _, coa_data in pairs(stmdata.coalition) do
+		for _, grp in ipairs(STM.processCoalition(coa_data,
+				lookupname,
+				trackUniqueCoalition,
+				modifyStatic)) do
+			table.insert(template.tpldata, grp)
+		end
 	end
-	template.names = nil
 	return template
 end
 

src/dct/Template.lua
@@ -83,12 +83,10 @@ local function getcntr()
 end
 
 local function makeNamesUnique(data)
-	for _, cat_data in pairs(data) do
-		for _, grp in ipairs(cat_data) do
-			grp.data.name = grp.data.name .. " #" .. getcntr()
-			for _, v in ipairs(grp.data.units or {}) do
-				v.name = v.name .. " #" .. getcntr()
-			end
+	for _, grp in ipairs(data) do
+		grp.data.name = grp.data.name .. " #" .. getcntr()
+		for _, v in ipairs(grp.data.units or {}) do
+			v.name = v.name .. " #" .. getcntr()
 		end
 	end
 end
@@ -105,7 +103,7 @@ local function overrideUnitOptions(unit, key, tpl, basename)
 	unit.name = basename.."-"..key
 end
 
-local function overrideGroupOptions(grp, idx, tpl, category)
+local function overrideGroupOptions(grp, idx, tpl)
 	local opts = {
 		visible        = true,
 		uncontrollable = true,
@@ -117,30 +115,28 @@ local function overrideGroupOptions(grp, idx, tpl, category)
 	end
 
 	local goaltype = Goal.objtype.GROUP
-	if string.lower(category) == "static" then
+	if grp.category == Unit.Category.STRUCTURE then
 		goaltype = Goal.objtype.STATIC
 	end
 
-	grp.groupId = nil
-	grp.start_time = 0
-	grp.dct_deathgoal = goalFromName(grp.name, goaltype)
-	if grp.dct_deathgoal ~= nil then
+	grp.data.groupId = nil
+	grp.data.start_time = 0
+	grp.data.dct_deathgoal = goalFromName(grp.data.name, goaltype)
+	if grp.data.dct_deathgoal ~= nil then
 		tpl.hasDeathGoals = true
 	end
-	grp.name = tpl.regionname.."_"..tpl.name.." "..tpl.coalition.." "..
-		category.." "..tostring(idx)
+	grp.data.name = tpl.regionname.."_"..tpl.name.." "..tpl.coalition.." "..
+		dctutils.getkey(Unit.Category, grp.category).." "..tostring(idx)
 
-	for i, unit in ipairs(grp.units or {}) do
-		overrideUnitOptions(unit, i, tpl, grp.name)
+	for i, unit in ipairs(grp.data.units or {}) do
+		overrideUnitOptions(unit, i, tpl, grp.data.name)
 	end
 end
 
-local function checktpldata(_, tbl)
+local function checktpldata(_, tpl)
 	-- loop over all tpldata and process names and existence of deathgoals
-	for cat, cat_data in pairs(tbl.tpldata) do
-		for idx, grp in ipairs(cat_data) do
-			overrideGroupOptions(grp.data, idx, tbl, cat)
-		end
+	for idx, grp in ipairs(tpl.tpldata) do
+		overrideGroupOptions(grp, idx, tpl)
 	end
 	return true
 end
@@ -256,13 +252,13 @@ end
 --    Storage
 --    -------
 --    tpldata = {
---      category = {
---        # = {
+--      # = {
+--          category = Unit.Category
 --          countryid = id,
 --          data      = {
 --            # group def members
 --            dct_deathgoal = goalspec
---    }}}}
+--    }}}
 --
 --    DCT File
 --    --------

tests/test-events.lua
@@ -14,7 +14,7 @@ local testcases = {
 		["event"] = {
 			["id"] = world.event.S_EVENT_HIT,
 			["object"] = {
-				["name"] = "Test region_Abu Musa Ammo Dump 1 static 1",
+				["name"] = "Test region_Abu Musa Ammo Dump 1 STRUCTURE 1",
 				["objtype"] = Object.Category.STATIC,
 				["life"] = 1,
 			},
@@ -24,7 +24,7 @@ local testcases = {
 		["event"] = {
 			["id"] = world.event.S_EVENT_DEAD,
 			["object"] = {
-				["name"] = "Test region_Abu Musa Ammo Dump 1 static 1",
+				["name"] = "Test region_Abu Musa Ammo Dump 1 STRUCTURE 1",
 				["objtype"] = Object.Category.STATIC,
 			},
 		},

tests/test-template.lua
@@ -4,6 +4,7 @@ require("dcttestlibs")
 require("dct")
 local utils = require("libs.utils")
 local enum = require("dct.enum")
+local STM = require("dct.STM")
 local Template = require("dct.Template")
 
 local sampletpls = {
@@ -85,9 +86,13 @@ local function main()
 		-- test: group values read
 		for grpname, value in pairs(data.groups) do
 			--print("name: "..grpname.." val: "..value)
+			local cat =
+				Unit.Category[STM.categorymap[string.upper(grpname)]]
 			local val = 0
-			if t.tpldata[grpname] ~= nil then
-				val = #t.tpldata[grpname]
+			for _, grp in ipairs(t.tpldata) do
+				if grp.category == cat then
+					val = val + 1
+				end
 			end
 			assert(val == value,
 				grpname.." expected: "..value.."; got: "..val)

commit 7df8b59d31d7b5952cbd339d3cca55b48229bbd6

fix state saving
With the changing to composition in the Asset class, state saving was
broken. This is caused when all units of an asset were killed but the
asset is kept in the list. In addition when all units of a group were
marked as dead the group entry was not removed. This was further
compounded by changing the storage format of template data for a flat
list.

Therefore there are three changes;
1. In StaticCollection:marshal(), test if nil instead of testing
   if the table returned is empty as this nil could be returned
2. In filterDeadObjects(), if there is a units table (meaning the
   entry is a group) test if the units table is empty after filtering
   meaning the group data should not be stored
3. In Asset:marshal(), if nil is returned from the collection, the
   asset should be deleted, exactly as before all the conversions

Closes: #73
Fixes: 2070317ff04e ("modify storage of groups in templates")
Fixes: 942e4c4d27be ("asset: use composition to represent different
asset types")

src/dct/Asset.lua
@@ -221,6 +221,9 @@ function Asset:marshal()
 	assert(self._initcomplete == true, "runtime error: init not complete")
 	local tbl = {}
 	tbl.collection = self._collection:marshal()
+	if tbl.collection == nil then
+		return nil
+	end
 	for _, attribute in pairs(self._marshalnames) do
 		tbl[attribute] = self[attribute]
 	end

src/dct/dcscollections/StaticCollection.lua
@@ -327,6 +327,10 @@ local function filterDeadObjects(tbl, grp)
 				table.insert(gcpy.data.units, utils.deepcopy(unit))
 			end
 		end
+		if not next(gcpy.data.units) then
+			-- there are no alive units do not add the group
+			return
+		end
 	end
 	table.insert(tbl, gcpy)
 end
@@ -346,7 +350,7 @@ end
 function StaticCollection:marshal()
 	local tbl = {}
 	tbl._tpldata = filterTemplateData(self._tpldata)
-	if next(tbl._tpldata) == nil then
+	if tbl._tpldata == nil then
 		return nil
 	end
 	return utils.mergetables(tbl, IDCSObjectCollection.marshal(self))