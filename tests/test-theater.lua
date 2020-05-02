jtoppins/dct

commit 942e4c4d27beb8adda7b48d0ec2f4d8adaf6a507

asset: use composition to represent different asset types
Instead of deriving a new Asset class for each kind of asset type
just isolate the primary thing that changes which is the association
with DCS in-game objects. This collection of DCS objects can then be
operated on and delegated to without having to rewrite portions
of the Asset class over and over.

This also includes updates to the Asset API to include;
* setting initial intel state from a template
* splitting priority into individual elements

Closes: #46
Closes: #49

src/dct/Asset.lua
@@ -9,24 +9,9 @@
 require("math")
 local class    = require("libs.class")
 local utils    = require("libs.utils")
+local dctenum  = require("dct.enum")
 local dctutils = require("dct.utils")
-local STM      = require("dct.STM")
-local Goal     = require("dct.Goal")
-local Logger   = require("dct.Logger").getByName("Asset")
 local settings = _G.dct.settings
-
-local function defaultgoal(static)
-	local goal = {}
-	goal.priority = Goal.priority.PRIMARY
-	goal.goaltype = Goal.goaltype.DAMAGE
-	goal.objtype  = Goal.objtype.GROUP
-	goal.value    = 90
-
-	if static then
-		goal.objtype = Goal.objtype.STATIC
-	end
-	return goal
-end
 
 local function generateCodename(objtype)
 	local codenamedb = settings.codenamedb
@@ -37,394 +22,242 @@ local function generateCodename(objtype)
 	end
 
 	local idx = math.random(1, #typetbl)
-	local codename = typetbl[idx]
-
-	return codename
+	return typetbl[idx]
 end
+
+local function getcollection(assettype, asset, template, region)
+	local collection = nil
+	if dctenum.assetClass.STRATEGIC[assettype] then
+		collection = require("dct.dcscollections.StaticCollection")
+	else
+		assert(false, "unsupported asset type: "..assettype)
+	end
+	return collection(asset, template, region)
+end
+
+--[[
+Asset:
+	attributes(private):
+	- _collection<IObjectCollection>
+
+	attributes(public, read-only):
+	- type
+	- briefing
+	- owner
+	- rgnname
+	- tplname
+	- name
+	- codename
+
+	methods(public):
+	- getPriority(side)
+		- get the priority of the asset
+	- setPriority(side, tbl)
+		- modify the priority of the asset
+
+	Intel - an intel level of zero implies the given side has no
+	idea about the asset
+	- getIntel(side)
+		- get the intel level the specified side has on this asset
+	- setIntel(side, val)
+
+	- isTargeted(side)
+		- is the specified side currently targeting the asset?
+	- setTargeted(side, val)
+		- set the targeted state for a side for an asset
+	- marshal()
+		- marshal the asset for serialization
+	- unmarshal(data)
+		- unmarshal the asset from a seralized stream
+	- IObjectCollection methods
+--]]
 
 local Asset = class()
 function Asset:__init(template, region)
+	self._marshalnames = {
+		"_targeted", "_intel", "_priority", "type", "briefing",
+		"owner", "rgnname", "tplname", "name", "codename",
+	}
+
+	self._targeted   = {}
+	self._intel      = {}
+	self._priority   = {}
+	for _, side in pairs(coalition.side) do
+		-- TODO: convert targeted to a number instead of a bool
+		--   check so we can count how many times a given asset
+		--   is targeted, used for CAP stations.
+		self._targeted[side] = false
+		self._intel[side]    = 0
+		self._priority[side] = {
+			["region"] = 0,
+			["asset"]  = 0,
+		}
+	end
 	self._initcomplete = false
-	self._spawned    = false
-	self._dead       = false
-	self._targeted   = false
-	self._maxdeathgoals = 0
-	self._curdeathgoals = 0
-	self._deathgoals = {}
-	self._assets     = {}
-
 	if template ~= nil and region ~= nil then
-		self._hasDeathGoals = template.hasDeathGoals
-		self._tpldata   = template:copyData()
-		self._briefing  = template.desc
-		self.owner      = template.coalition
-		self.type       = template.objtype
-		self.name       = region.name.."_"..self.owner.."_"..template.name
-		self.regionname = region.name
-		self.codename   = generateCodename(self.type)
-		self.priority   = region.priority * 65536 + template.priority
-		self:_setupmaps()
-		self._initcomplete = true
-		assert(next(self._deathgoals) ~= nil,
-			"runtime error: Asset must have a deathgoal")
-	end
-end
-
---[[
--- ignore all but primary targets when it comes to determining
--- if an Asset is "dead"
---]]
-function Asset:_addDeathGoal(name, goalspec)
-	assert(name ~= nil and type(name) == "string",
-		"value error: name must be provided")
-	assert(goalspec ~= nil, "value error: goalspec must be provided")
-
-	if goalspec.priority ~= Goal.priority.PRIMARY then
-		return
-	end
-
-	self._deathgoals[name] = Goal.factory(name, goalspec)
-	self._curdeathgoals = self._curdeathgoals + 1
-	self._maxdeathgoals = math.max(self._curdeathgoals, self._maxdeathgoals)
-end
-
---[[
--- This function needs to do three things:
---   mark the object(unit/static/group) in the template dead,
---      dct_dead == true
---   remove deathgoal entry
---   upon no more deathgoals set dead
---]]
-function Asset:_removeDeathGoal(name, goal)
-	assert(name ~= nil and type(name) == "string",
-		"value error: name must be provided")
-	assert(goal ~= nil, "value error: goal must be provided")
-
-	Logger:debug("_removeDeathGoal() - obj name: "..name)
-	if self:isDead() then
-		Logger:error("_removeDeathGoal() called and Asset("..
-			self:getName()..") marked as dead")
-		return
-	end
-
-	local grpdata = self._assets[goal:getGroupName()]
-	if grpdata.name == name then
-		grpdata.dct_dead = true
-	else
-		assert(grpdata.units ~= nil, "no units found, this is a problem")
-		for _, unit in ipairs(grpdata.units) do
-			if unit.name == name then
-				unit.dct_dead = true
-				break
-			end
+		self.type     = template.objtype
+		self.briefing = template.desc
+		self.owner    = template.coalition
+		self.rgnname  = region.name
+		self.tplname  = template.name
+		self.name     = region.name.."_"..self.owner.."_"..template.name
+		self.codename = generateCodename(self.type)
+		self._intel[self.owner] = dctutils.INTELMAX
+		if self.owner ~= coalition.side.NEUTRAL and template.intel then
+			self._intel[dctutils.getenemy(self.owner)] = template.intel
 		end
-	end
-
-	self._deathgoals[name] = nil
-	self._curdeathgoals = self._curdeathgoals - 1
-	if next(self._deathgoals) == nil then
-		self:_setDead()
-	end
-end
-
---[[
--- Adds a death goal to an Asset class, which determines
--- when an Asset is determined to be dead. If no death
--- goals have been defined a default of 90% damaged for all
--- objects in the Asset is used.
---]]
-function Asset:_setupDeathGoal(grpdata, static)
-	if self._hasDeathGoals then
-		if grpdata.dct_deathgoal ~= nil then
-			self:_addDeathGoal(grpdata.name, grpdata.dct_deathgoal)
-		end
-		for _, unit in ipairs(grpdata.units or {}) do
-			if unit.dct_deathgoal ~= nil then
-				self:_addDeathGoal(unit.name, unit.dct_deathgoal)
-			end
-		end
-	else
-		self:_addDeathGoal(grpdata.name, defaultgoal(static))
-	end
-end
-
---[[
--- Adds an object (group or static) to the monitored list for this
--- asset. This list will be needed later to save state.
---]]
-function Asset:_setupmaps()
-	for cat_idx, cat_data in pairs(self._tpldata) do
-		for _, grp in ipairs(cat_data) do
-			self:_setupDeathGoal(grp.data, cat_idx == 'static')
-			self._assets[grp.data.name] = grp.data
-		end
-	end
-end
-
--- TODO: if this is a movable asset we should not calculate
--- the centroid, not sure what location we should return -
--- the starting position or something else? We shouldn't return
--- the current position I do not think.
-function Asset:getLocation()
-	if self.centroid == nil then
-		local points = {}
-		for k,v in pairs(self._assets) do
-			points[k] = {
-				["x"] = v.x, ["y"] = 0, ["z"] = v.y,
+		for _, side in pairs(coalition.side) do
+			self._priority[side] = {
+				["region"] = region.priority,
+				["asset"]  = template.priority,
 			}
 		end
-
-		self.centroid = dctutils.centroid(points)
+		self._collection = getcollection(self.type, self, template, region)
+		self._initcomplete = true
 	end
-	return self.centroid
 end
 
-function Asset:getCallsign()
-	return self.codename
+-- TODO: not sure intel and priority should be stored with a given
+--  Asset because each side may have a different view and ordering
+--  for the Asset.
+function Asset:getPriority(side)
+	return ((self._priority[side].region * 65536) +
+		self._priority[side].asset)
 end
 
-function Asset:getIntelLevel()
-	return 4
-	-- TODO: base this on a value related to the asset type
+function Asset:setPriority(side, tbl)
+	utils.mergetables(self._priority[side], tbl)
 end
 
---[[
--- getStatus - percentage remaining of the asset from 0 - 100
---]]
-function Asset:getStatus()
-	return math.floor((1 - (self._curdeathgoals / self._maxdeathgoals)) * 100)
+function Asset:getIntel(side)
+	return self._intel[side]
 end
 
-function Asset:getBriefing()
-	return self._briefing
+function Asset:setIntel(side, val)
+	assert(type(val) == "number", "value error: must be a number")
+	self._intel[side] = val
 end
 
-function Asset:isTargeted()
-	return self._targeted
+function Asset:isTargeted(side)
+	return self._targeted[side]
 end
 
-function Asset:setTargeted(val)
+function Asset:setTargeted(side, val)
 	assert(type(val) == "boolean",
 		"value error: argument must be of type bool")
-	self._targeted = val
+	self._targeted[side] = val
 end
 
-function Asset:isSpawned()
-	return self._spawned
+function Asset:getCollection()
+	return self._collection
 end
 
-function Asset:getName()
-	return self.name
+function Asset:getLocation()
+	return self._collection:getLocation()
 end
 
-function Asset:getPriority()
-	return self.priority
-end
-
-function Asset:_setDead()
-	self._dead = true
+function Asset:getStatus()
+	return self._collection:getStatus()
 end
 
 function Asset:isDead()
-	return self._dead
+	return self._collection:isDead()
+end
+
+function Asset:setDead(val)
+	return self._collection:setDead(val)
 end
 
 function Asset:checkDead()
-	assert(self:isSpawned() == true, "runtime error: asset must be spawned")
-
-	local cnt = 0
-	for name, goal in pairs(self._deathgoals) do
-		cnt = cnt + 1
-		if goal:checkComplete() then
-			self:_removeDeathGoal(name, goal)
-		end
-	end
-	Logger:debug(string.format("checkDead(%s) - max goals: %d; "..
-		"cur goals: %d; checked: %d", self:getName(),
-		self._maxdeathgoals, self._curdeathgoals, cnt))
+	self._collection:checkDead()
 end
 
--- Get the names of all DCS objects associated with this
--- Asset class.
 function Asset:getObjectNames()
-	local keyset = {}
-	local n      = 0
-	for k,_ in pairs(self._assets) do
-		n = n+1
-		keyset[n] = k
-	end
-	return keyset
+	return self._collection:getObjectNames()
 end
 
 function Asset:onDCSEvent(event)
-	-- only handle DEAD events
-	if event.id ~= world.event.S_EVENT_DEAD then
-		Logger:debug(string.format("onDCSEvent() - Asset(%s) not DEAD "..
-			"event, ignoring", self:getName()))
-		return
-	end
-
-	local obj = event.initiator
-
-	-- mark the unit/group/static as dead in the template, dct_dead
-	local unitname = obj:getName()
-	if obj:getCategory() == Object.Category.UNIT then
-		local grpname = obj:getGroup():getName()
-		local grp = self._assets[grpname]
-		for _, unit in pairs(grp.units) do
-			if unit.name == unitname then
-				unit.dct_dead = true
-				break
-			end
-		end
-	else
-		self._assets[unitname].dct_dead = true
-	end
-
-	-- delete any deathgoal related to the unit notified as dead,
-	-- this may work around any bug in DCS where the object is still
-	-- kept and its health reports a non-zero value
-	local goal = self._deathgoals[unitname]
-	if goal ~= nil then
-		self:_removeDeathGoal(unitname, goal)
-	end
+	self._collection:onDCSEvent(event)
 end
 
-local dctkeys = {
-	["dct_deathgoal"] = true,
-	["dct_dead"]      = true
-}
-
--- modifies 'tbl' with 'keys' keys removed from 'tbl'
-local function removekeys(tbl, keys)
-	for k, _ in pairs(keys) do
-		tbl[k] = nil
-	end
-end
-
--- returns a copy of 'grp' with all dct table keys removed
-local function removeDCTKeys(grp)
-	local g = utils.deepcopy(grp)
-	removekeys(g.data, dctkeys)
-	for _, unit in ipairs(g.data.units or {}) do
-		removekeys(unit, dctkeys)
-	end
-	return g
-end
-
-function Asset:_spawn()
-	for cat_idx, cat_data in pairs(self._tpldata) do
-		for _, grp in ipairs(cat_data) do
-			local gcpy = removeDCTKeys(grp)
-			if cat_idx == 'static' then
-				coalition.addStaticObject(gcpy.countryid, gcpy.data)
-			else
-				coalition.addGroup(gcpy.countryid,
-					Unit.Category[STM.categorymap[string.upper(cat_idx)]],
-					gcpy.data)
-			end
-		end
-	end
-
-	self._spawned = true
-	for _, goal in pairs(self._deathgoals) do
-		goal:onSpawn()
-	end
+function Asset:isSpawned()
+	return self._collection:isSpawned()
 end
 
 function Asset:spawn()
-	if self:isSpawned() then
-		Logger:error("runtime bug - asset already spawned")
-		return
-	end
-	self:_spawn()
+	self._collection:spawn()
 end
 
-local function filterDeadObjects(tbl, grp)
-	-- remove groups that are dead
-	if grp.data.dct_dead == true then
-		-- we either skip or if a static object that is a primary target
-		-- we set dead
-		if settings.spawndead == false or
-			grp.data.dct_deathgoal == nil then
-			return
-		end
-
-		if not (grp.data.dct_deathgoal.priority == Goal.priority.PRIMARY and
-			grp.data.dct_deathgoal.objtype == Goal.objtype.STATIC) then
-			return
-		end
-
-		local gcpy = utils.deepcopy(grp)
-		gcpy.data.dead = true
-		table.insert(tbl, gcpy)
-		-- we need to return here because the object is dead
-		-- so nothing else to do, skip rest of function
-		return
-	end
-
-	local gcpy = utils.deepcopy(grp)
-	-- remove dead units from the group
-	if grp.data.units then
-		gcpy.data.units = {}
-		for _, unit in ipairs(grp.data.units) do
-			if unit.dct_dead ~= true then
-				table.insert(gcpy.data.units, utils.deepcopy(unit))
-			end
-		end
-	end
-	table.insert(tbl, gcpy)
-end
-
-local function filterTemplateData(tpldata)
-	local cpytbl = {}
-
-	for cat_idx, cat_data in pairs(tpldata) do
-		cpytbl[cat_idx] = {}
-		for _, grp in ipairs(cat_data) do
-			filterDeadObjects(cpytbl[cat_idx], grp)
-		end
-		if not next(cpytbl[cat_idx]) then
-			cpytbl[cat_idx] = nil
-		end
-	end
-	if not next(cpytbl) then
-		cpytbl = nil
-	end
-	return cpytbl
+function Asset:destroy()
+	self._collection:destroy()
 end
 
 function Asset:marshal()
 	assert(self._initcomplete == true, "runtime error: init not complete")
 	local tbl = {}
-
-	tbl._tpldata       = filterTemplateData(self._tpldata)
-	if next(tbl._tpldata) == nil then
-		return nil
+	tbl.collection = self._collection:marshal()
+	for _, attribute in pairs(self._marshalnames) do
+		tbl[attribute] = self[attribute]
 	end
-	tbl._dead          = self._dead
-	tbl._spawned       = self._spawned
-	tbl._maxdeathgoals = self._maxdeathgoals
-	tbl._hasDeathGoals = self._hasDeathGoals
-	tbl._briefing      = self._briefing
-	tbl.name           = self.name
-	tbl.regionname     = self.regionname
-	tbl.codename       = self.codename
-	tbl.owner          = self.owner
-	tbl.type           = self.type
-	tbl.priority       = self.priority
 	return tbl
 end
 
 function Asset:unmarshal(data)
 	assert(self._initcomplete == false,
 		"runtime error: init completed already")
+	local collectiondata = data.collection
+	data.collection = nil
 	utils.mergetables(self, data)
-	self:_setupmaps()
-	if self:isSpawned() then
-		self:_spawn()
-	end
+	self._collection = getcollection(data.type, self)
+	self._collection:unmarshal(collectiondata)
 	self._initcomplete = true
 end
 
 return Asset
+
+--[[
+-- DynamicAsset
+--   represents assets that can move
+--  difference from BaseAsset
+   * DCS-objects, has associated DCS objects
+     * objects move
+     * has death goals due to having DCS objects
+   * associates a "team leader" AI with the asset to control the
+     spawned DCS objects
+
+-- PlayerAsset
+--  inherents from DynamicAsset
+--   flight groups with player slots in them
+--  difference from BaseAsset and DynamicAsset
+   * DCS-objects, has associated DCS objects
+     * objects move
+     * has death goals due to having DCS objects
+     * spawn, nothing to spawn
+   * invincible, asset cannot die (i.e. be deleted)
+   * no associated "team leader" AI
+   * player specific isSpawned() test - why?
+   * enabled, asset can be enabled/disabled
+     * DCS flag associated to control if the slot is enabled
+       (think airbase captured so slot should not be joinable)
+   * registers with an airbase asset
+
+-- AirbaseAsset
+-- is a composite asset consisting of multiple other assets
+--   (squadrons, players, defense forces, etc)
+--  inherents from BaseAsset, difference from:
+   * depending on underlying DCS object type the deathgoal will either
+     be the death of the underlying unit or the asset can never die it
+     just triggers an internal event notifying the observers of the
+     change in side
+   * ability to register an associated asset; registration would
+     consist of:
+       - asset name and asset type (player, squadron, defense force,
+         underlying asset)
+   * on death the assets associated are marked as dead or disabled
+   * provides custom functions to spawn an aircraft group
+
+-- SquadronAsset
+--  inherets from BaseAsset
+   * being spawned means the asset is "active"
+   * the deathgoal is when all aircraft in the squadron are destroyed
+--]]

src/dct/AssetManager.lua
@@ -15,16 +15,11 @@
 
 local class    = require("libs.class")
 local enum     = require("dct.enum")
+local dctutils = require("dct.utils")
 local Logger   = require("dct.Logger").getByName("AssetManager")
 local Command  = require("dct.Command")
 local Stats    = require("dct.Stats")
 local Asset    = require("dct.Asset")
-
-local enemymap = {
-	[coalition.side.NEUTRAL] = false,
-	[coalition.side.BLUE]    = coalition.side.RED,
-	[coalition.side.RED]     = coalition.side.BLUE,
-}
 
 local ASSET_CHECK_PERIOD = 12*60  -- seconds
 
@@ -103,11 +98,11 @@ function AssetManager:remove(asset)
 	--  strategic target. This supports the feature where dead
 	--  strategic assets can be spawned in as dead.
 	if not isstrat then
-		self._assetset[asset:getName()] = nil
+		self._assetset[asset.name] = nil
 	end
 
 	-- remove asset name from per-side asset list
-	self._sideassets[asset.owner].assets[asset:getName()] = nil
+	self._sideassets[asset.owner].assets[asset.name] = nil
 	self._sideassets[asset.owner].stats:dec(asset.type.."."..substats.ALIVE)
 
 	-- remove asset object names from name list
@@ -120,20 +115,20 @@ function AssetManager:add(asset)
 	assert(asset ~= nil, "value error: asset object must be provided")
 
 	-- add asset to master list
-	assert(self._assetset[asset:getName()] == nil, "asset name ('"..
-		asset:getName().."') already exists")
-	self._assetset[asset:getName()] = asset
+	assert(self._assetset[asset.name] == nil, "asset name ('"..
+		asset.name.."') already exists")
+	self._assetset[asset.name] = asset
 
 	-- add asset to approperate side lists
 	if not asset:isDead() then
-		self._sideassets[asset.owner].assets[asset:getName()] = asset.type
+		self._sideassets[asset.owner].assets[asset.name] = asset.type
 		self._sideassets[asset.owner].stats:inc(asset.type.."."..
 			substats.ALIVE)
 
 		-- read Asset's object names and setup object to asset mapping
 		-- to be used in handling DCS events and other uses
 		for _, objname in pairs(asset:getObjectNames()) do
-			self._object2asset[objname] = asset:getName()
+			self._object2asset[objname] = asset.name
 		end
 	end
 end
@@ -157,7 +152,7 @@ end
 --    filter list requested
 --]]
 function AssetManager:getTargets(requestingside, assettypelist)
-	local enemy = enemymap[requestingside]
+	local enemy = dctutils.getenemy(requestingside)
 	local tgtlist = {}
 	local filterlist
 
@@ -271,7 +266,8 @@ function AssetManager:marshal()
 	}
 
 	for name, asset in pairs(self._assetset) do
-		if enum.assetClass.STRATEGIC[asset.type] ~= nil then
+		if enum.assetClass.STRATEGIC[asset.type] ~= nil or
+		   enum.assetType.AIRSPACE == asset.type then
 			tbl.assets[name] = asset:marshal()
 		end
 	end

src/dct/Mission.lua
@@ -68,7 +68,7 @@ function Mission:__init(cmdr, missiontype, grpname, tgtname)
 	-- compose the briefing at mission creation to represent
 	-- known intel the pilots were given before departing
 	self.briefing  = self:_composeBriefing()
-	self.cmdr:getAsset(tgtname):setTargeted(true)
+	self.cmdr:getAsset(tgtname):setTargeted(self.cmdr.owner, true)
 
 	-- TODO: setup remaining mission parameters;
 	--   * mission world states
@@ -76,7 +76,7 @@ end
 
 function Mission:_composeBriefing()
 	local tgt = self.cmdr:getAsset(self.target)
-	local briefing = tgt:getBriefing()
+	local briefing = tgt.briefing
 	local interptbl = {
 		["LOCATIONMETHOD"] = genLocationMethod(),
 		["TOT"] = os.date("%Y-%m-%d %H:%M:%Sz",
@@ -103,7 +103,7 @@ function Mission:abort()
 	self.cmdr:removeMission(self.id)
 	local tgt = self.cmdr:getAsset(self.target)
 	if tgt then
-		tgt:setTargeted(false)
+		tgt:setTargeted(self.cmdr.owner, false)
 	end
 	return self.id
 end
@@ -160,9 +160,9 @@ function Mission:getTargetInfo()
 	local asset = self.cmdr:getAsset(self.target)
 	local tgtinfo = {}
 	tgtinfo.location = asset:getLocation()
-	tgtinfo.callsign = asset:getCallsign()
+	tgtinfo.callsign = asset.codename
 	tgtinfo.status   = asset:getStatus()
-	tgtinfo.intellvl = asset:getIntelLevel()
+	tgtinfo.intellvl = asset:getIntel(self.cmdr.owner)
 	return tgtinfo
 end
 

src/dct/ai/Commander.lua
@@ -21,8 +21,8 @@ local function heapsort_tgtlist(assetmgr, owner, filterlist)
 	-- priority sort target list
 	for tgtname, _ in pairs(tgtlist) do
 		local tgt = assetmgr:getAsset(tgtname)
-		if tgt ~= nil and not tgt:isDead() and not tgt:isTargeted() then
-			pq:push(tgt:getPriority(), tgt)
+		if tgt ~= nil and not tgt:isDead() and not tgt:isTargeted(owner) then
+			pq:push(tgt:getPriority(owner), tgt)
 		end
 	end
 
@@ -140,9 +140,9 @@ function Commander:requestMission(grpname, missiontype)
 		return nil
 	end
 	Logger:debug(string.format("requestMission() - tgt name: '%s'; "..
-		"isTargeted: %s", tgt:getName(), tostring(tgt:isTargeted())))
+		"isTargeted: %s", tgt.name, tostring(tgt:isTargeted())))
 
-	local mission = Mission(self, missiontype, grpname, tgt:getName())
+	local mission = Mission(self, missiontype, grpname, tgt.name)
 	self:addMission(mission)
 	return mission
 end

src/dct/dcscollections/IDCSObjectCollection.lua
@@ -1,0 +1,129 @@
+--[[
+-- SPDX-License-Identifier: LGPL-3.0
+--
+-- Represents a collection of DCS objects or not. Provides
+-- an interface to interact with this collection.
+--]]
+
+local class = require("libs.class")
+local utils = require("libs.utils")
+local Goal  = require("dct.Goal")
+
+--[[
+IDCSObjectCollection:
+	methods(public):
+	- __init(template, region)
+	- spawn()
+		- spawn the DCS objects
+	- isSpawned()
+		- have the DCS objects been spawned?
+	- destroy()
+	- isDead()
+		- has the asset meet its deathgoal?
+	- setDead(val)
+		- sets if the object should be thought of as dead or not
+	- checkDead()
+		- check the asset death goals
+	- onDCSEvent(event)
+		- process a DCS event associated w/ this asset
+	- getStatus()
+		- get the "status" of the asset, that being a percentage
+		  completion of the death goal
+	- getObjectNames()
+		- get DCS object names associated with this asset
+	- getLocation()
+		- get the centroid location of the asset
+	- marshal()
+	- unmarshal(data)
+--]]
+
+local IDCSObjectCollection = class()
+function IDCSObjectCollection:__init(asset, template, region)
+	assert(asset ~= nil and type(asset) == "table",
+		"value error: asset must be provided")
+	self._asset = asset
+	self._initcomplete  = false
+	if template ~= nil and region ~= nil then
+		self:_completeinit(template, region)
+		self:_setup()
+		self._initcomplete  = true
+	end
+end
+
+function IDCSObjectCollection:_completeinit(_ --[[template]],
+	_ --[[region]])
+end
+
+function IDCSObjectCollection:_setup()
+end
+
+function IDCSObjectCollection:spawn(_ --[[ignore]])
+end
+
+function IDCSObjectCollection:isSpawned()
+	return true
+end
+
+function IDCSObjectCollection:destroy()
+end
+
+function IDCSObjectCollection:isDead()
+	return false
+end
+
+function IDCSObjectCollection:setDead(_ --[[val]])
+end
+
+function IDCSObjectCollection:checkDead()
+end
+
+function IDCSObjectCollection:onDCSEvent(_ --[[event]])
+end
+
+function IDCSObjectCollection:getStatus()
+	return 0
+end
+
+function IDCSObjectCollection:getObjectNames()
+	return {}
+end
+
+function IDCSObjectCollection:getLocation()
+	return nil
+end
+
+function IDCSObjectCollection:marshal()
+	assert(self._initcomplete == true,
+		"runtime error: init not complete")
+	local tbl = {}
+	for _, attribute in pairs(self._marshalnames or {}) do
+		tbl[attribute] = self[attribute]
+	end
+	return tbl
+end
+
+function IDCSObjectCollection:unmarshal(data)
+	assert(self._initcomplete == false,
+		"runtime error: init completed already")
+	utils.mergetables(self, data)
+	self:_setup()
+	self._initcomplete = true
+	if self:isSpawned() then
+		self:spawn(true)
+	end
+end
+
+function IDCSObjectCollection.defaultgoal(static)
+	local goal = {}
+	goal.priority = Goal.priority.PRIMARY
+	goal.goaltype = Goal.goaltype.DAMAGE
+	goal.objtype  = Goal.objtype.GROUP
+	goal.value    = 90
+
+	if static then
+		goal.objtype = Goal.objtype.STATIC
+	end
+	return goal
+end
+
+return IDCSObjectCollection

commit 12d788efecc551cbd83138790164d7864039e40e

asset: fix reading saved state
The problem identified by the stack trace below is caused by the json
library changing the key type to a string for both the _priority and
_intel tables. Since LUA differentiates between a number and string
when looking up a key in a table you can actually have a table
construction like the following;
	{ 1 = 1234, "1" = 789, }

This will result in two different values being returned depending on
if you asked for the key `"1"` or `1`.

The solution is to turn these table keys back into numbers when reading
in the state file.

Stack trace:
2020-05-01 22:45:15.811 ERROR   SCRIPTING: DCT|Theater: protected call - [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Asset.lua"]:149: attempt to index field '?' (a nil value)
stack traceback:
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:338: in function <[string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:336>
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Asset.lua"]:149: in function 'getPriority'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\ai\Commander.lua"]:25: in function 'heapsort_tgtlist'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\ai\Commander.lua"]:191: in function 'requestMission'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\ui\cmds.lua"]:171: in function '_execute'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\ui\cmds.lua"]:48: in function 'execute'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:318: in function '_exec'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:341: in function <[string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:340>
    [C]: in function 'xpcall'
    [string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:344: in function <[string "C:\Saved Games\DCS.openbeta\Mods\tech\DCT\lua\dct\Theater.lua"]:335>

Finally, add a unit-test to catch this issue in the future by triggering
a user command after the state file has been read back in.

Closes: #74
Fixes: 942e4c4d27be

src/dct/Asset.lua
@@ -236,6 +236,16 @@ function Asset:unmarshal(data)
 	local collectiondata = data.collection
 	data.collection = nil
 	utils.mergetables(self, data)
+	-- need to handle the intel and priority tables special because even
+	-- though their keys were numbers when the state was serialized
+	-- in json's wisdom it decided to convert them to strings. So we need to
+	-- convert back so we can access the data in our lookups.
+	for _, tbl in ipairs({"_intel", "_priority"}) do
+		self[tbl] = {}
+		for k, v in pairs(data[tbl]) do
+			self[tbl][tonumber(k)] = v
+		end
+	end
 	self._collection = getcollection(data.type, self)
 	self._collection:unmarshal(collectiondata)
 	self._initcomplete = true

tests/test-theater.lua
@@ -5,6 +5,7 @@ require("io")
 local md5 = require("md5")
 require("dcttestlibs")
 require("dct")
+local enum   = require("dct.enum")
 local settings = _G.dct.settings
 
 local events = {
@@ -204,7 +205,7 @@ end
 local function main()
 	local playergrp = Group(4, {
 		["id"] = 15,
-		["name"] = "Uzi 35",
+		["name"] = "Uzi 41",
 		["coalition"] = coalition.side.BLUE,
 		["exists"] = true,
 	})
@@ -242,6 +243,30 @@ local function main()
 	-- verify the units read in do not include the asset we killed off
 	assert(newtheater:getAssetMgr():getAsset(name) == nil,
 		"state saving has an issue")
+
+	-- attempt to get theater status
+	newtheater:onEvent({
+		["id"]        = world.event.S_EVENT_BIRTH,
+		["initiator"] = player1,
+	})
+
+	local status = {
+		["data"] = {
+			["name"]   = playergrp:getName(),
+			["type"]   = enum.uiRequestType.THEATERSTATUS,
+		},
+		["assert"]     = true,
+		["expected"]   = "== Theater Threat Status ==\n  Sea:    medium\n"..
+			"  Air:    parity\n  ELINT:  medium\n  SAM:    medium\n\n"..
+			"== Current Active Air Missions ==\n  No Active Missions\n\n"..
+			"Recommended Mission Type: CAP\n",
+	}
+	local uicmds = require("dct.ui.cmds")
+	trigger.action.setassert(status.assert)
+	trigger.action.setmsgbuffer(status.expected)
+	local cmd = uicmds[status.data.type](newtheater, status.data)
+	cmd:execute(400)
+
 	newtheater:export()
 	f = io.open(settings.statepath, "r")
 	local sumsave = md5.sum(f:read("*all"))