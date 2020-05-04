jtoppins/dct

commit f988a799b88bfb3aa17fde825d24288281606d02

mission: track station time
Make check-in and check-out actually do something and track how long a
player was on station.

Closes: #17

src/dct/Mission.lua
@@ -56,7 +56,11 @@ function Mission:__init(cmdr, missiontype, grpname, tgtname)
 	self.assigned  = grpname
 	self.timestart = timer.getTime()
 	self.timeend   = self.timestart + MISSION_LIMIT
-	self.onstation = false
+	self.station   = {
+		["onstation"] = false,
+		["total"]     = 0,
+		["start"]     = 0,
+	}
 
 	-- compose the briefing at mission creation to represent
 	-- known intel the pilots were given before departing
@@ -141,11 +145,20 @@ function Mission:addTime(time)
 end
 
 function Mission:checkin(time)
-	-- TODO: write this
+	if self.station.onstation == true then
+		return
+	end
+	self.station.onstation = true
+	self.station.start = time
 end
 
 function Mission:checkout(time)
-	-- TODO: write this
+	if self.station.onstation == false then
+		return
+	end
+	self.station.onstation = false
+	self.station.total = self.station.total + (time - self.station.start)
+	return self.station.total
 end
 
 function Mission:getDescription(actype, locprecision)

src/dct/ui/cmds.lua
@@ -209,8 +209,8 @@ function MissionCheckoutCmd:__init(theater, data)
 end
 
 function MissionCheckoutCmd:_mission(time, cmdr, msn)
-	msn:checkout(time)
-	return string.format("off-station received")
+	return string.format("off-station received, vul time: %d",
+		msn:checkout(time))
 end
 
 local cmds = {

tests/test-ui-cmds.lua
@@ -101,7 +101,7 @@ local testcmds = {
 			["type"]   = enum.uiRequestType.MISSIONCHECKOUT,
 		},
 		["assert"]     = true,
-		["expected"]   = "off-station received",
+		["expected"]   = "off-station received, vul time: 0",
 	},
 	[8] = {
 		["data"] = {

commit e22b464e7db596e28c268c5631c01fa4479a7d3a

mission: bad argument #2 to format (no value)
Mission:checkout() needs to always return a value.

Closes: #80
Fixes: f988a799b88b ("mission: track station time")

src/dct/Mission.lua
@@ -153,7 +153,7 @@ end
 
 function Mission:checkout(time)
 	if self.station.onstation == false then
-		return
+		return 0
 	end
 	self.station.onstation = false
 	self.station.total = self.station.total + (time - self.station.start)