jtoppins/dct

commit baa65448269fbf5202c5c108b29ed5b159bc1ab8

player asset: treat player slots like other assets
This commit combines all of the various data stored about a player
and puts it all under one object. These player assets are generated
by reading the mission file and are not stored in the statefile.

src/dct/Theater.lua
@@ -14,9 +14,12 @@ local json        = require("libs.json")
 local enum        = require("dct.enum")
 local dctutils    = require("dct.utils")
 local uicmds      = require("dct.ui.cmds")
-local uimenu      = require("dct.ui.groupmenu")
+local uiscratchpad= require("dct.ui.scratchpad")
 local Observable  = require("dct.Observable")
+local STM         = require("dct.STM")
+local Template    = require("dct.Template")
 local Region      = require("dct.Region")
+local Asset       = require("dct.Asset")
 local AssetManager= require("dct.AssetManager")
 local Commander   = require("dct.ai.Commander")
 local Command     = require("dct.Command")
@@ -55,7 +58,6 @@ function Theater:__init()
 	self.ltime     = 0
 	self.assetmgr  = AssetManager(self)
 	self.cmdrs     = {}
-	self.playergps = {}
 	self.scratchpad= {}
 	self.startdate = os.date("*t")
 
@@ -66,7 +68,8 @@ function Theater:__init()
 	self:_loadGoals()
 	self:_loadRegions()
 	self:_loadOrGenerate()
-	uimenu(self)
+	self:_loadPlayerSlots()
+	uiscratchpad(self)
 	self:queueCommand(100, Command(self.export, self))
 	Profiler:profileStop("Theater:init()")
 end
@@ -165,6 +168,38 @@ function Theater:_loadOrGenerate()
 	self.statetbl = nil
 end
 
+local function isPlayerGroup(grp, _, _)
+	for _, unit in ipairs(grp.units) do
+		if unit.skill == "Client" then
+			return true
+		end
+	end
+	return false
+end
+
+function Theater:_loadPlayerSlots()
+	local cnt = 0
+	for _, coa_data in pairs(env.mission.coalition) do
+		local grps = STM.processCoalition(coa_data,
+			env.getValueDictByKey,
+			isPlayerGroup,
+			nil)
+		for _, grp in ipairs(grps) do
+			local asset = Asset(Template({
+				["objtype"]   = "playergroup",
+				["name"]      = grp.data.name,
+				["regionname"]= "theater",
+				["coalition"] = coalition.getCountryCoalition(grp.countryid),
+				["desc"]      = "Player group",
+				["tpldata"]   = grp.data,
+			}), {["name"] = "theater", ["priority"] = 1000,})
+			self:getAssetMgr():add(asset)
+			cnt = cnt + 1
+		end
+	end
+	Logger:info(string.format("_loadPlayerSlots(); found %d slots", cnt))
+end
+
 function Theater:export(_)
 	local statefile
 	local msg
@@ -230,7 +265,9 @@ function Theater:playerRequest(data)
 	Logger:debug("playerRequest(); Received player request: "..
 		json:encode_pretty(data))
 
-	if self.playergps[data.name].cmdpending == true then
+	local playerasset = self:getAssetMgr():getAsset(data.name)
+
+	if playerasset.cmdpending == true then
 		Logger:debug("playerRequest(); request pending, ignoring")
 		trigger.action.outTextForGroup(data.id,
 			"F10 request already pending, please wait.", 20, true)
@@ -239,7 +276,7 @@ function Theater:playerRequest(data)
 
 	local cmd = uicmds[data.type](self, data)
 	self:queueCommand(self.uicmddelay, cmd)
-	self.playergps[data.name].cmdpending = true
+	playerasset.cmdpending = true
 end
 
 function Theater:getATORestrictions(side, unittype)

src/dct/Asset.lua
@@ -33,6 +33,8 @@ local function getcollection(assettype, asset, template, region)
 		collection = require("dct.dcscollections.AirspaceCollection")
 	elseif dctenum.assetClass.STRATEGIC[assettype] then
 		collection = require("dct.dcscollections.StaticCollection")
+	elseif assettype == dctenum.assetType.PLAYERGROUP then
+		collection = require("dct.dcscollections.PlayerCollection")
 	else
 		assert(false, "unsupported asset type: "..assettype)
 	end
@@ -119,7 +121,11 @@ function Asset:__init(template, region)
 		self.owner    = template.coalition
 		self.rgnname  = region.name
 		self.tplname  = template.name
-		self.name     = region.name.."_"..self.owner.."_"..template.name
+		if self.type == dctenum.assetType.PLAYERGROUP then
+			self.name = self.tplname
+		else
+			self.name = region.name.."_"..self.owner.."_"..template.name
+		end
 		self.codename = generateCodename(self.type)
 		self._intel[self.owner] = dctutils.INTELMAX
 		if self.owner ~= coalition.side.NEUTRAL and template.intel then
@@ -195,8 +201,8 @@ function Asset:getObjectNames()
 	return self._collection:getObjectNames()
 end
 
-function Asset:onDCSEvent(event)
-	self._collection:onDCSEvent(event)
+function Asset:onDCSEvent(event, theater)
+	self._collection:onDCSEvent(event, theater)
 end
 
 function Asset:isSpawned()
@@ -243,22 +249,6 @@ return Asset
      * has death goals due to having DCS objects
    * associates a "team leader" AI with the asset to control the
      spawned DCS objects
-
--- PlayerAsset
---  inherents from DynamicAsset
---   flight groups with player slots in them
---  difference from BaseAsset and DynamicAsset
-   * DCS-objects, has associated DCS objects
-     * objects move
-     * has death goals due to having DCS objects
-     * spawn, nothing to spawn
-   * invincible, asset cannot die (i.e. be deleted)
-   * no associated "team leader" AI
-   * player specific isSpawned() test - why?
-   * enabled, asset can be enabled/disabled
-     * DCS flag associated to control if the slot is enabled
-       (think airbase captured so slot should not be joinable)
-   * registers with an airbase asset
 
 -- AirbaseAsset
 -- is a composite asset consisting of multiple other assets

src/dct/AssetManager.lua
@@ -211,10 +211,45 @@ function AssetManager:checkAssets(time)
 	return ASSET_CHECK_PERIOD
 end
 
+local function handleDead(self, event)
+	local obj = event.initiator
+
+	-- TODO: I am not sure this is correct, and will at least need
+	--   to be changed to handle player groups. Also, this is an
+	--   group object to asset name mapping so I don't understand
+	--   how a single dead event on a unit would cause the removal
+	--   of something from this table.
+	-- remove object from object2asset list if the event is a DEAD event
+	if event.id == world.event.S_EVENT_DEAD and
+	   self._object2asset[obj:getName()] ~= nil then
+		self._object2asset[obj:getName()] = nil
+	end
+end
+
+local handlers = {
+	[world.event.S_EVENT_DEAD] = handleDead,
+}
+
+-- TODO: need to fix this handler to listen to all events given that
+-- we are going to track all players
 function AssetManager:onDCSEvent(event)
 	local relevents = {
-		[world.event.S_EVENT_HIT] = true,
-		[world.event.S_EVENT_DEAD] = true,
+		[world.event.S_EVENT_BIRTH]           = true,
+		[world.event.S_EVENT_ENGINE_STARTUP]  = true,
+		[world.event.S_EVENT_ENGINE_SHUTDOWN] = true,
+		[world.event.S_EVENT_TAKEOFF]         = true,
+		[world.event.S_EVENT_LAND]            = true,
+		[world.event.S_EVENT_CRASH]           = true,
+		[world.event.S_EVENT_KILL]            = true,
+		[world.event.S_EVENT_PILOT_DEAD]      = true,
+		[world.event.S_EVENT_EJECTION]        = true,
+		[world.event.S_EVENT_HIT]             = true,
+		[world.event.S_EVENT_DEAD]            = true,
+		--[world.event.S_EVENT_UNIT_LOST]     = true,
+	}
+	local objmap = {
+		[world.event.S_EVENT_HIT]  = "target",
+		[world.event.S_EVENT_KILL] = "target",
 	}
 	local objcat = {
 		[Object.Category.UNIT]   = true,
@@ -227,13 +262,13 @@ function AssetManager:onDCSEvent(event)
 	end
 
 	local obj = event.initiator
-	if event.id == world.event.S_EVENT_HIT then
-		obj = event.target
+	if objmap[event.id] ~= nil then
+		obj = event[objmap[event.id]]
 	end
 
 	if not obj or objcat[obj:getCategory()] == nil then
 		Logger:debug(string.format("onDCSEvent - bad object (%s) or"..
-			" category; event id: %d", tostring(event.initiator), event.id))
+			" category; event id: %d", tostring(obj), event.id))
 		return
 	end
 
@@ -247,19 +282,18 @@ function AssetManager:onDCSEvent(event)
 		Logger:debug("onDCSEvent - not tracked object, obj name: "..name)
 		return
 	end
-	asset = self._assetset[asset]
+	asset = self:getAsset(asset)
 	if asset == nil then
 		Logger:debug("onDCSEvent - asset doesn't exist, name: "..name)
 		return
 	end
 
-	-- remove object from object2asset list if the event is a DEAD event
-	if event.id == world.event.S_EVENT_DEAD and
-	   self._object2asset[obj:getName()] ~= nil then
-		self._object2asset[obj:getName()] = nil
+	local handler = handlers[event.id]
+	if handler ~= nil then
+		handler(self, event)
 	end
 
-	asset:onDCSEvent(event)
+	asset:onDCSEvent(event, self._theater)
 end
 
 function AssetManager:marshal()

src/dct/dcscollections/PlayerCollection.lua
@@ -1,0 +1,103 @@
+--[[
+-- SPDX-License-Identifier: LGPL-3.0
+--
+-- Represents a null collection.
+-- A null collection that doesn't die, is always spawned, never
+-- reduces status, and is not associated with any DCS objects
+--]]
+
+--[[
+   PlayerAsset
+   * flight groups with player slots in them
+   * DCS-objects, has associated DCS objects
+     * objects move
+     * no death goals, has death goals due to having DCS objects
+     * spawn, nothing to spawn
+   * invincible, asset cannot die (i.e. be deleted)
+   * no associated "team leader" AI
+   * player specific isSpawned() test - why?
+   * enabled, asset can be enabled/disabled
+     * DCS flag associated to control if the slot is enabled
+       (think airbase captured so slot should not be joinable)
+   * registers with an airbase asset
+--]]
+
+local class = require("libs.class")
+local IDCSObjectCollection = require("dct.dcscollections.IDCSObjectCollection")
+local uimenu  = require("dct.ui.groupmenu")
+local Logger  = require("dct.Logger").getByName("Asset")
+
+local PlayerCollection = class(IDCSObjectCollection)
+function PlayerCollection:__init(asset, template, region)
+	table.insert(asset._marshalnames, "unittype")
+	table.insert(asset._marshalnames, "cmdpending")
+	table.insert(asset._marshalnames, "groupId")
+	IDCSObjectCollection.__init(self, asset, template, region)
+end
+
+function PlayerCollection:_completeinit(template, _)
+	-- we assume all slots in a player group are the same
+	self._asset.unittype   = template.tpldata.units[1].type
+	self._asset.cmdpending = false
+	self._asset.groupId    = template.tpldata.groupId
+end
+
+function PlayerCollection:getObjectNames()
+	return {[1] = self._asset.name}
+end
+
+local function handleBirth(self, event, theater)
+	if not (event.initiator and event.initiator.getGroup) then
+		Logger:debug(string.format("(%s) - invalid initiator",
+			self._asset.name))
+		return
+	end
+
+	local pname = event.initiator:getPlayerName()
+	local grp = event.initiator:getGroup()
+	if not grp or not pname or pname == "" then
+		Logger:debug(
+			string.format("(%s) - bad player name (%s) or group (%s)",
+				self._asset.name, pname, grp))
+		return
+	end
+
+	local id = grp:getID()
+	if self._asset.groupId ~= id then
+		Logger:warn(
+			string.format("(%s) - asset.groupId(%d) != object:getID(%d)",
+				self._asset.name, self._asset.groupId, id))
+	end
+	self._asset.groupId = id
+	uimenu.createMenu(theater, self._asset)
+	local cmdr = theater:getCommander(grp:getCoalition())
+	local msn  = cmdr:getAssigned(grp:getName())
+
+	if msn then
+		trigger.action.outTextForGroup(grp:getID(),
+			"Welcome. A mission is already assigned to this slot, "..
+			"use the F10 menu to get the briefing or find another.",
+			20, true)
+	else
+		trigger.action.outTextForGroup(grp:getID(),
+			"Welcome. Use the F10 Menu to get a theater update and "..
+			"request a mission.",
+			20, true)
+	end
+end
+
+local handlers = {
+	[world.event.S_EVENT_BIRTH] = handleBirth,
+}
+
+function PlayerCollection:onDCSEvent(event, theater)
+	-- TODO: need to move the creation of the ui to menus to here
+	-- test if the groupid of the player object is the same as the
+	--   one we read from the mission
+	local handler = handlers[event.id]
+	if handler ~= nil then
+		handler(self, event, theater)
+	end
+end
+
+return PlayerCollection

src/dct/enum.lua
@@ -34,6 +34,7 @@ local assetType = {
 	["AIRSPACE"]    = 18,
 	["SHORAD"]      = 19,
 	["AIRBASE"]     = 20,
+	["PLAYERGROUP"] = 21,
 }
 
 --[[
@@ -92,6 +93,10 @@ local assetClass = {
 	["BASES"] = {
 		[assetType.AIRBASE]     = true,
 	},
+	-- agents never get seralized to the state file
+	["AGENTS"] = {
+		[assetType.PLAYERGROUP] = true,
+	}
 	--[[
 	-- Means ground tactical units
 	["TACTICAL"] = {

src/dct/ui/cmds.lua
@@ -10,22 +10,22 @@ local enum     = require("dct.enum")
 local dctutils = require("dct.utils")
 local human    = require("dct.ui.human")
 local Command  = require("dct.Command")
-local Logger   = require("dct.Logger").getByName("uiCmds")
+local Logger   = require("dct.Logger").getByName("UI")
 
 local UICmd = class(Command)
 function UICmd:__init(theater, data)
 	assert(theater ~= nil, "value error: theater required")
 	assert(data ~= nil, "value error: data required")
-	local grpdata = theater.playergps[data.name]
+	local asset = theater:getAssetMgr():getAsset(data.name)
 
 	self.theater      = theater
 	self.type         = data.type
-	self.grpid        = grpdata.id
+	self.grpid        = asset.groupId
 	self.grpname      = data.name
-	self.side         = grpdata.side
+	self.side         = asset.owner
 	self.displaytime  = 30
 	self.displayclear = true
-	self.actype       = grpdata.unittype
+	self.actype       = asset.unittype
 end
 
 function UICmd:isAlive()
@@ -33,13 +33,14 @@ function UICmd:isAlive()
 end
 
 function UICmd:execute(time)
+	local asset = self.theater:getAssetMgr():getAsset(self.grpname)
 	-- only process commands from live players unless they are abort
 	-- commands
 	if not self:isAlive() and
 	   self.type ~= enum.uiRequestType.MISSIONABORT then
 		Logger:debug("UICmd thinks player is dead, ignore cmd; "..
 			debug.traceback())
-		self.theater.playergps[self.grpname].cmdpending = false
+		asset.cmdpending = false
 		return nil
 	end
 
@@ -48,7 +49,7 @@ function UICmd:execute(time)
 	assert(msg ~= nil and type(msg) == "string", "msg must be a string")
 	trigger.action.outTextForGroup(self.grpid, msg, self.displaytime,
 		self.displayclear)
-	self.theater.playergps[self.grpname].cmdpending = false
+	asset.cmdpending = false
 	return nil
 end
 
@@ -58,8 +59,9 @@ function ScratchPadDisplay:__init(theater, data)
 end
 
 function ScratchPadDisplay:_execute(_, _)
+	local asset = self.theater:getAssetMgr():getAsset(self.grpname)
 	local msg = string.format("Scratch Pad: '%s'",
-		tostring(self.theater.playergps[self.grpname].scratchpad))
+		tostring(asset.scratchpad))
 	return msg
 end
 

commit 9ecbad2253fe6152fec00d1bef0489c579ca64f0

fix call to outTextForGroup that causes crash
Passing an invalid group id, nil, causes the server to crash. Use the
correct group id found in the playerasset.

Fixes: baa65448269f ("player asset: treat player slots like other assets")

src/dct/Theater.lua
@@ -256,7 +256,7 @@ function Theater:playerRequest(data)
 
 	if playerasset.cmdpending == true then
 		Logger:debug("playerRequest(); request pending, ignoring")
-		trigger.action.outTextForGroup(data.id,
+		trigger.action.outTextForGroup(playerasset.groupId,
 			"F10 request already pending, please wait.", 20, true)
 		return
 	end