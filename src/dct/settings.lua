d46dafdc66c37857e3bc6c7f3e180e6e9404f57e
Kijaxu <49261498+Kijaxu@users.noreply.github.com>
2020-09-15 21:17:52 +0100

    Set correct Coordinate format for F-16 and F-14.

--- src/dct/settings.lua
+++ src/dct/settings.lua
@@ -45,8 +45,9 @@ local function settings(missioncfg)
 			["M-2000C"]       = dctutils.posfmt.DDM,
 			["A-10C"]         = dctutils.posfmt.MGRS,
 			["AJS37"]         = dctutils.posfmt.DMS,
-			["F-14B"]         = dctutils.posfmt.DDM,
+			["F-14B"]         = dctutils.posfmt.DMS,
 			["FA-18C_hornet"] = dctutils.posfmt.DDM,
+			["F-16C"] 		  = dctutils.posfmt.DDM,
 		},
 		["codenamedb"] = codenamedb,
 		["atorestrictions"] = {
