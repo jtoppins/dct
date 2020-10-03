b7bbc3c81d1f7bfccfa5d34daceb1e75e2abdd41
terrificfool <rthomasstevens@gmail.com>
2020-10-03 12:27:16 -0400

    Change F-14B coordinates to DMS

--- tests/test-settings.lua
+++ tests/test-settings.lua
@@ -27,7 +27,7 @@ local function main()
 			["M-2000C"]       = dctutils.posfmt.DDM,
 			["A-10C"]         = dctutils.posfmt.MGRS,
 			["AJS37"]         = dctutils.posfmt.DMS,
-			["F-14B"]         = dctutils.posfmt.DDM,
+			["F-14B"]         = dctutils.posfmt.DMS,
 			["FA-18C_hornet"] = dctutils.posfmt.DDM,
 			["F-16C_50"]      = dctutils.posfmt.DDM,
 			["UH-1H"]         = dctutils.posfmt.DDM,
