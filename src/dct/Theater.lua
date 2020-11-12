jtoppins/dct

commit 377217c809ed096df9f4b27bd550182629339315

theater: assert on more than one slot in player group
DCT does not support more than one client unit in a player group. This
is for various reasons mainly due to the limitations of DCS's scripting.

src/dct/Theater.lua
@@ -178,10 +178,18 @@ function Theater:_loadOrGenerate()
 end
 
 local function isPlayerGroup(grp, _, _)
+	local slotcnt = 0
 	for _, unit in ipairs(grp.units) do
 		if unit.skill == "Client" then
-			return true
+			slotcnt = slotcnt + 1
 		end
+	end
+	if slotcnt > 0 then
+		assert(slotcnt == 1,
+			string.format("DCT requires 1 slot groups. Group '%s' "..
+				" of type a/c (%s) has more than one player slot.",
+				grp.name, grp.units[1].type))
+		return true
 	end
 	return false
 end

commit 10472d93cb2f93a2b8e6779b929366f43e4b5941

theater: warn versus assert on multiple player slots per group
Instead of assert print warning when multiple players slots exist in
a single group.

Fixes: 377217c809ed ("theater: assert on more than one slot in player
group")

src/dct/Theater.lua
@@ -188,14 +188,13 @@ local function isPlayerGroup(grp, _, _)
 			slotcnt = slotcnt + 1
 		end
 	end
-	if slotcnt > 0 then
-		assert(slotcnt == 1,
-			string.format("DCT requires 1 slot groups. Group '%s' "..
-				" of type a/c (%s) has more than one player slot.",
-				grp.name, grp.units[1].type))
-		return true
+	if slotcnt > 1 then
+		Logger:warn(string.format("DCT requires 1 slot groups. Group '%s'"..
+			" of type a/c (%s) has more than one player slot.",
+			grp.name, grp.units[1].type))
+		return false
 	end
-	return false
+	return true
 end
 
 function Theater:queuekick(playerasset)