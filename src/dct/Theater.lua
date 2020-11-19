jtoppins/dct

commit 10472d93cb2f93a2b8e6779b929366f43e4b5941

theater: warn versus assert on multiple player slots per group
Instead of assert print warning when multiple players slots exist in
a single group.

Fixes: 377217c809ed ("theater: assert on more than one slot in player
group")

src/dct/Theater.lua
@@ -188,14 +188,13 @@ local function isPlayerGroup(grp, _, _)
 			slotcnt = slotcnt + 1
 		end
 	end
-	if slotcnt > 0 then
-		assert(slotcnt == 1,
-			string.format("DCT requires 1 slot groups. Group '%s' "..
-				" of type a/c (%s) has more than one player slot.",
-				grp.name, grp.units[1].type))
-		return true
+	if slotcnt > 1 then
+		Logger:warn(string.format("DCT requires 1 slot groups. Group '%s'"..
+			" of type a/c (%s) has more than one player slot.",
+			grp.name, grp.units[1].type))
+		return false
 	end
-	return false
+	return true
 end
 
 function Theater:queuekick(playerasset)

commit d6829d96d40345b1d320cfb8af7a22126c2d9199

theater: fix filtering of player groups
Fixes: 10472d93cb2f ("theater: warn versus assert on multiple player
slots per group")

src/dct/Theater.lua
@@ -187,13 +187,15 @@ local function isPlayerGroup(grp, _, _)
 			slotcnt = slotcnt + 1
 		end
 	end
-	if slotcnt > 1 then
-		Logger:warn(string.format("DCT requires 1 slot groups. Group '%s'"..
-			" of type a/c (%s) has more than one player slot.",
-			grp.name, grp.units[1].type))
-		return false
+	if slotcnt > 0 then
+		if slotcnt > 1 then
+			Logger:warn(string.format("DCT requires 1 slot groups. Group "..
+				"'%s' of type a/c (%s) has more than one player slot.",
+				grp.name, grp.units[1].type))
+		end
+		return true
 	end
-	return true
+	return false
 end
 
 function Theater:_loadPlayerSlots()