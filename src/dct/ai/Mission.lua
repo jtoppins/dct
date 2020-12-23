jtoppins/dct

commit c2b1f0a3e5eac03c345e1dc3f2c1055a7d61fcf1

human: remove grid2actype
Move the grid format setup to the Player class and get rid of
grid2actype as we really do not need it anymore.

src/dct/ai/Mission.lua
@@ -210,15 +210,16 @@ function Mission:checkout(time)
 	return self.station.total
 end
 
-function Mission:getDescription(actype)
+function Mission:getDescription(fmt)
 	local tgt = self.cmdr:getAsset(self.target)
 	if tgt == nil then
 		return "Target destroyed abort mission"
 	end
 	local interptbl = {
-		["LOCATION"] = uihuman.grid2actype(actype,
+		["LOCATION"] = dctutils.fmtposition(
 			tgt:getLocation(),
-			tgt:getIntel(self.cmdr.owner))
+			tgt:getIntel(self.cmdr.owner),
+			fmt)
 	}
 	return dctutils.interp(self.briefing, interptbl)
 end

src/dct/ui/human.lua
@@ -62,15 +62,6 @@ function human.locationhdr(msntype)
 	return hdr
 end
 
-function human.grid2actype(actype, location, precision)
-	local fmt = settings.gridfmt[actype]
-	precision = precision or 3
-	if fmt == nil then
-		fmt = dctutils.posfmt.DMS
-	end
-	return dctutils.fmtposition(location, precision, fmt)
-end
-
 function human.drawTargetIntel(msn, grpid, readonly)
 	local tgtinfo = msn:getTargetInfo()
 	local degpos = dctutils.degrade_position(tgtinfo.location,

src/dct/assets/Player.lua
@@ -35,6 +35,7 @@
 require("math")
 local class = require("libs.class")
 local AssetBase = require("dct.assets.AssetBase")
+local dctutils= require("dct.utils")
 local uimenu  = require("dct.ui.groupmenu")
 local Logger  = dct.Logger.getByName("Asset")
 local loadout = require("dct.systems.loadouts")
@@ -226,6 +227,8 @@ function Player:_completeinit(template, region)
 		self.ato = require("dct.enum").missionType
 	end
 	self.payloadlimits = settings.payloadlimits
+	self.gridfmt    = settings.ui.gridfmt[self.unittype] or
+		dctutils.posfmt.DMS
 end
 
 function Player:_setup()

src/dct/ui/cmds.lua
@@ -165,11 +165,12 @@ local function briefingmsg(msn, asset)
 			msn.iffcodes.m1, msn.iffcodes.m3)..
 		string.format("%s: %s (%s)\n",
 			human.locationhdr(msn.type),
-			human.grid2actype(asset.unittype,
-				tgtinfo.location, tgtinfo.intellvl),
+			dctutils.fmtposition(
+				tgtinfo.location,
+				tgtinfo.intellvl,
+				asset.gridfmt),
 			tgtinfo.callsign)..
-		"Briefing:\n"..msn:getDescription(asset.unittype,
-			tgtinfo.intellvl)
+		"Briefing:\n"..msn:getDescription(asset.gridfmt)
 	return msg
 end
 

commit 86ec914815b2a9e202b767b448778a5953607579

fix luacheck errors
Fixes: c2b1f0a3e5ea ("human: remove grid2actype")

src/dct/ai/Mission.lua
@@ -12,7 +12,6 @@ local class    = require("libs.class")
 local utils    = require("libs.utils")
 local enum     = require("dct.enum")
 local dctutils = require("dct.utils")
-local uihuman  = require("dct.ui.human")
 local uicmds   = require("dct.ui.cmds")
 
 local MISSION_LIMIT = 60*60*3  -- 3 hours in seconds

src/dct/ui/human.lua
@@ -8,7 +8,6 @@ require("math")
 local utils    = require("libs.utils")
 local enum     = require("dct.enum")
 local dctutils = require("dct.utils")
-local settings = _G.dct.settings.ui
 
 local human = {}
 