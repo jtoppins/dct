jtoppins/dct

commit 5d3264a3c27d4268ff8e0f561c469b0a6ae0af0c

command: introduce a timed and prioritized command

src/dct/Command.lua
@@ -2,18 +2,27 @@
 -- SPDX-License-Identifier: LGPL-3.0
 --
 -- Provides a basic Command class to call an arbitrary function
--- at a later time via the Command's ececute function.
+-- at a later time via the Command's execute function.
 --]]
 
 local class = require("libs.class")
 local utils = require("libs.utils")
+
+-- lower value is higher priority; total of 127 priority values
+local cmdpriority = {
+	["UI"]     = 10,
+	["NORMAL"] = 64,
+}
 
 local Command = class()
 function Command:__init(func, ...)
 	assert(type(func) == "function",
 		"value error: the first argument must be a function")
 	self.func = func
+	self.name = "Unnamed Command"
+	self.prio = cmdpriority.NORMAL
 	self.args = {select(1, ...)}
+	self.PRIORITY = nil
 end
 
 function Command:execute(time)
@@ -21,5 +30,24 @@ function Command:execute(time)
 	table.insert(args, time)
 	return self.func(unpack(args))
 end
+Command.PRIORITY = cmdpriority
 
-return Command
+local cmd = Command
+
+if _G.settings and _G.settings.server and
+   _G.settings.server.debug == true then
+	require("os")
+	local TimmedCommand = class(Command)
+	function TimmedCommand:execute(time)
+		local tstart = os.clock()
+		local rc = Command.execute(self, time)
+		dct.Logger.getByName("Command"):info(
+			string.format("'%s' exec time: %f",
+			self.name, os.clock()-tstart))
+		return rc
+	end
+	TimmedCommand.PRIORITY = cmdpriority
+	cmd = TimmedCommand
+end
+
+return cmd

commit 04e2283eb1274f152f53c11bee9743a8fdb760b1

command: fix overriding with TimedCommand
When 5d3264a3c27d ("command: introduce a timed and prioritized command")
was written it introduced a timed command to be used when the global
debug was enabled. The original if check used the wrong table and so
the TimedCommand never was used. In addition we fixup some logging
messages in this commit.

Fixes: 5d3264a3c27d ("command: introduce a timed and prioritized command")

src/dct/Command.lua
@@ -7,6 +7,8 @@
 
 local class = require("libs.class")
 local utils = require("libs.utils")
+local check = require("libs.check")
+local Logger= dct.Logger.getByName("Command")
 
 -- lower value is higher priority; total of 127 priority values
 local cmdpriority = {
@@ -16,9 +18,7 @@ local cmdpriority = {
 
 local Command = class()
 function Command:__init(func, ...)
-	assert(type(func) == "function",
-		"value error: the first argument must be a function")
-	self.func = func
+	self.func = check.func(func)
 	self.name = "Unnamed Command"
 	self.prio = cmdpriority.NORMAL
 	self.args = {select(1, ...)}
@@ -27,6 +27,7 @@ end
 
 function Command:execute(time)
 	local args = utils.shallowclone(self.args)
+	Logger:debug(string.format("executing: %s", self.name))
 	table.insert(args, time)
 	return self.func(unpack(args))
 end
@@ -34,20 +35,19 @@ Command.PRIORITY = cmdpriority
 
 local cmd = Command
 
-if _G.settings and _G.settings.server and
-   _G.settings.server.debug == true then
+if dct.settings and dct.settings.server and
+   dct.settings.server.debug == true then
 	require("os")
-	local TimmedCommand = class(Command)
-	function TimmedCommand:execute(time)
+	local TimedCommand = class(Command)
+	function TimedCommand:execute(time)
 		local tstart = os.clock()
 		local rc = Command.execute(self, time)
-		dct.Logger.getByName("Command"):info(
-			string.format("'%s' exec time: %f",
-			self.name, os.clock()-tstart))
+		Logger:info(string.format("'%s' exec time: %5.2fms",
+			self.name, (os.clock()-tstart)*1000))
 		return rc
 	end
-	TimmedCommand.PRIORITY = cmdpriority
-	cmd = TimmedCommand
+	TimedCommand.PRIORITY = cmdpriority
+	cmd = TimedCommand
 end
 
 return cmd

tests/test-0002-data.lua
@@ -2,6 +2,8 @@
 
 require("os")
 require("io")
+require("dcttestlibs")
+require("dct")
 local Command = require("dct.Command")
 
 local function f(a, b, c, time)