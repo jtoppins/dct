jtoppins/dct

commit 903ade6231c7188990f1bec2617041b202963bcf

tickets system: add basic ticket accounting system
This adds support for DCT to track a side's number of remaining tickets.
There is also the ability to limit a campaign to a fixed period of time.
In addition the campaign designer can select from 4 preset difficulties
or specify their own difficulty. If a preset difficulty is selected
this will override any custom changes you have made.

Closes: #88

src/dct/Theater.lua
@@ -22,111 +22,24 @@ local Region      = require("dct.templates.Region")
 local AssetManager= require("dct.assets.AssetManager")
 local Commander   = require("dct.ai.Commander")
 local Command     = require("dct.Command")
+local Tickets     = require("dct.systems.tickets")
 local Logger      = dct.Logger.getByName("Theater")
 local settings    = _G.dct.settings.server
 
---[[
---  Theater class
---    base class that reads in all region and template information
---    and provides a base interface for manipulating data at a theater
---    level.
---
---  Storage of theater:
---		goals = {
---			<goalname> = State(),
---		},
---		regions = {
---			<regionname> = Region(),
---		},
---]]
-local Theater = class()
-function Theater:__init()
-	self._observers = {}
-	self.savestatefreq = 7*60 -- seconds
-	self.cmdmindelay   = 2
-	self.uicmddelay    = self.cmdmindelay
-	self:setTimings(settings.schedfreq, settings.tgtfps,
-		settings.percentTimeAllowed)
-	self.complete  = false
-	self.statef    = false
-	self.regions   = {}
-	self.cmdq      = containers.PriorityQueue()
-	self.ctime     = timer.getTime()
-	self.ltime     = 0
-	self.assetmgr  = AssetManager(self)
-	self.cmdrs     = {}
-	self.scratchpad= {}
-	self.startdate = os.date("!*t")
-	self.bldgPersist= bldgPersist(self)
-	self.namecntr  = 1000
-
-	for _, val in pairs(coalition.side) do
-		self.cmdrs[val] = Commander(self, val)
-	end
-
-	-- TODO: remove spawning from generation of the theater
-	self:_loadGoals()
-	self:_loadRegions()
-	self:queueCommand(20, Command(self._delayedInit, self))
-end
-
-function Theater.singleton()
-	if _G.dct.theater ~= nil then
-		return _G.dct.theater
-	end
-	_G.dct.theater = Theater()
-	return _G.dct.theater
-end
-
-function Theater:_delayedInit()
-	self:_loadOrGenerate()
-	self:_loadPlayerSlots()
-	uiscratchpad(self)
-	self:queueCommand(100, Command(self.export, self))
-end
-
--- a description of the world state that signifies a particular side wins
--- TODO: create a common function that will read in a lua file like below
--- verify it was read correctly, contains the token expected, returns the
--- token on the stack and clears the global token space
-function Theater:_loadGoals()
-	local goalpath = settings.theaterpath..utils.sep.."theater.goals"
-	local rc = pcall(dofile, goalpath)
-	assert(rc, "failed to parse: theater goal file, '" ..
-			goalpath .. "' path likely doesn't exist")
-	assert(theatergoals ~= nil, "no theatergoals structure defined")
-
-	self.goals = {}
-	-- TODO: translate goal definitions written in the lua files to any
-	-- needed internal state.
-	-- Theater goals are goals written in a success format, meaning the
-	-- first side to complete all their goals wins
-	theatergoals = nil
-end
-
-function Theater:_loadRegions()
-	for filename in lfs.dir(settings.theaterpath) do
-		if filename ~= "." and filename ~= ".." and
-			filename ~= ".git" and filename ~= "settings" then
-			local fpath = settings.theaterpath..utils.sep..filename
-			local fattr = lfs.attributes(fpath)
-			if fattr.mode == "directory" then
-				local r = Region(fpath)
-				assert(self.regions[r.name] == nil, "duplicate regions " ..
-					"defined for theater: " .. settings.theaterpath)
-				self.regions[r.name] = r
-			end
+local function isPlayerGroup(grp, _, _)
+	local slotcnt = 0
+	for _, unit in ipairs(grp.units) do
+		if unit.skill == "Client" then
+			slotcnt = slotcnt + 1
 		end
 	end
-end
-
-function Theater:setTimings(cmdfreq, tgtfps, percent)
-	self._cmdqfreq    = cmdfreq
-	self._targetfps   = tgtfps
-	self._tallowed    = percent
-	self.cmdqdelay    = 1/self._cmdqfreq
-	self.quanta       = self._tallowed * ((1 / self._targetfps) *
-		self.cmdqdelay)
+	if slotcnt > 1 then
+		Logger:warn(string.format("DCT requires 1 slot groups. Group '%s'"..
+			" of type a/c (%s) has more than one player slot.",
+			grp.name, grp.units[1].type))
+		return false
+	end
+	return true
 end
 
 local function isStateValid(state)
@@ -156,16 +69,103 @@ local function isStateValid(state)
 	return true
 end
 
-function Theater:_initFromState()
-	self.statef = true
-	self.startdate = self.statetbl.startdate
-	self.namecntr  = self.statetbl.namecntr
-	self:getAssetMgr():unmarshal(self.statetbl.assetmgr)
-	self.bldgPersist:restoreState(self.statetbl.bldgDest)
+--[[
+--  Theater class
+--    base class that reads in all region and template information
+--    and provides a base interface for manipulating data at a theater
+--    level.
+--]]
+local Theater = class()
+function Theater:__init()
+	self._observers = {}
+	self.savestatefreq = 7*60 -- seconds
+	self.cmdmindelay   = 2
+	self.uicmddelay    = self.cmdmindelay
+	self:setTimings(settings.schedfreq, settings.tgtfps,
+		settings.percentTimeAllowed)
+	self.statef    = false
+	self.regions   = {}
+	self.cmdq      = containers.PriorityQueue()
+	self.ctime     = timer.getTime()
+	self.ltime     = 0
+	self.assetmgr  = AssetManager(self)
+	self.tickets   = Tickets(self,
+		settings.theaterpath..utils.sep.."theater.goals")
+	self.cmdrs     = {}
+	self.scratchpad= {}
+	self.startdate = os.date("!*t")
+	self.bldgPersist= bldgPersist(self)
+	self.namecntr  = 1000
+
+	for _, val in pairs(coalition.side) do
+		self.cmdrs[val] = Commander(self, val)
+	end
+
+	self:loadRegions()
+	self:queueCommand(20, Command(self.delayedInit, self))
 end
 
+function Theater.singleton()
+	if _G.dct.theater ~= nil then
+		return _G.dct.theater
+	end
+	_G.dct.theater = Theater()
+	return _G.dct.theater
+end
 
-function Theater:_loadOrGenerate()
+function Theater:setTimings(cmdfreq, tgtfps, percent)
+	self._cmdqfreq    = cmdfreq
+	self._targetfps   = tgtfps
+	self._tallowed    = percent
+	self.cmdqdelay    = 1/self._cmdqfreq
+	self.quanta       = self._tallowed * ((1 / self._targetfps) *
+		self.cmdqdelay)
+end
+
+function Theater:loadRegions()
+	for filename in lfs.dir(settings.theaterpath) do
+		if filename ~= "." and filename ~= ".." and
+			filename ~= ".git" and filename ~= "settings" then
+			local fpath = settings.theaterpath..utils.sep..filename
+			local fattr = lfs.attributes(fpath)
+			if fattr.mode == "directory" then
+				local r = Region(fpath)
+				assert(self.regions[r.name] == nil, "duplicate regions " ..
+					"defined for theater: " .. settings.theaterpath)
+				self.regions[r.name] = r
+			end
+		end
+	end
+end
+
+function Theater:loadPlayerSlots()
+	local cnt = 0
+	for _, coa_data in pairs(env.mission.coalition) do
+		local grps = STM.processCoalition(coa_data,
+			env.getValueDictByKey,
+			isPlayerGroup,
+			nil)
+		for _, grp in ipairs(grps) do
+			local side = coalition.getCountryCoalition(grp.countryid)
+			local asset =
+			self:getAssetMgr():factory(enum.assetType.PLAYERGROUP)(Template({
+				["objtype"]   = "playergroup",
+				["name"]      = grp.data.name,
+				["regionname"]= "theater",
+				["coalition"] = side,
+				["cost"]      = self.tickets:getPlayerCost(side),
+				["desc"]      = "Player group",
+				["tpldata"]   = grp,
+			}), {["name"] = "theater", ["priority"] = 1000,})
+			self:getAssetMgr():add(asset)
+			asset:spawn()
+			cnt = cnt + 1
+		end
+	end
+	Logger:info(string.format("loadPlayerSlots(); found %d slots", cnt))
+end
+
+function Theater:loadOrGenerate()
 	local statefile = io.open(settings.statepath)
 
 	if statefile ~= nil then
@@ -175,55 +175,26 @@ function Theater:_loadOrGenerate()
 
 	if isStateValid(self.statetbl) then
 		Logger:info("restoring saved state")
-		self:_initFromState()
+		self.statef = true
+		self.startdate = self.statetbl.startdate
+		self.namecntr  = self.statetbl.namecntr
+		self:getAssetMgr():unmarshal(self.statetbl.assetmgr)
+		self.tickets:unmarshal(self.statetbl.tickets)
+		self.bldgPersist:restoreState(self.statetbl.bldgDest)
 	else
-		Logger:info("saved state was invalid, generating new theater")
-		self:generate()
+		Logger:info("generating new theater")
+		for _, r in pairs(self.regions) do
+			r:generate(self.assetmgr)
+		end
 	end
 	self.statetbl = nil
 end
 
-local function isPlayerGroup(grp, _, _)
-	local slotcnt = 0
-	for _, unit in ipairs(grp.units) do
-		if unit.skill == "Client" then
-			slotcnt = slotcnt + 1
-		end
-	end
-	if slotcnt > 0 then
-		if slotcnt > 1 then
-			Logger:warn(string.format("DCT requires 1 slot groups. Group "..
-				"'%s' of type a/c (%s) has more than one player slot.",
-				grp.name, grp.units[1].type))
-		end
-		return true
-	end
-	return false
-end
-
-function Theater:_loadPlayerSlots()
-	local cnt = 0
-	for _, coa_data in pairs(env.mission.coalition) do
-		local grps = STM.processCoalition(coa_data,
-			env.getValueDictByKey,
-			isPlayerGroup,
-			nil)
-		for _, grp in ipairs(grps) do
-			local asset =
-			self:getAssetMgr():factory(enum.assetType.PLAYERGROUP)(Template({
-				["objtype"]   = "playergroup",
-				["name"]      = grp.data.name,
-				["regionname"]= "theater",
-				["coalition"] = coalition.getCountryCoalition(grp.countryid),
-				["desc"]      = "Player group",
-				["tpldata"]   = grp,
-			}), {["name"] = "theater", ["priority"] = 1000,})
-			self:getAssetMgr():add(asset)
-			asset:spawn()
-			cnt = cnt + 1
-		end
-	end
-	Logger:info(string.format("_loadPlayerSlots(); found %d slots", cnt))
+function Theater:delayedInit()
+	uiscratchpad(self)
+	self:loadPlayerSlots()
+	self:loadOrGenerate()
+	self:queueCommand(100, Command(self.export, self))
 end
 
 function Theater:registerHandler(func, ctx, name)
@@ -253,6 +224,12 @@ function Theater:onEvent(event)
 		Logger:debug("executing handler: "..val.name)
 		handler(val.ctx, event)
 	end
+	if event.id == world.event.S_EVENT_MISSION_END then
+		local ok, err = os.remove(settings.statepath)
+		if not ok then
+			Logger:error("unable to remove statepath; "..err)
+		end
+	end
 end
 
 function Theater:export(_)
@@ -268,10 +245,11 @@ function Theater:export(_)
 	end
 
 	local exporttbl = {
-		["complete"] = self.complete,
+		["complete"] = self.tickets:isComplete(),
 		["date"]     = os.date("!*t", dctutils.zulutime(timer.getAbsTime())),
 		["theater"]  = env.mission.theatre,
 		["sortie"]   = env.getValueDictByKey(env.mission.sortie),
+		["tickets"]  = self.tickets:marshal(),
 		["assetmgr"] = self:getAssetMgr():marshal(),
 		["bldgDest"]  = self.bldgPersist:returnList(),
 		["startdate"] = self.startdate,
@@ -282,12 +260,6 @@ function Theater:export(_)
 	statefile:flush()
 	statefile:close()
 	return self.savestatefreq
-end
-
-function Theater:generate()
-	for _, r in pairs(self.regions) do
-		r:generate(self.assetmgr)
-	end
 end
 
 function Theater:getAssetMgr()
@@ -301,6 +273,10 @@ end
 function Theater:getcntr()
 	self.namecntr = self.namecntr + 1
 	return self.namecntr
+end
+
+function Theater:getTickets()
+	return self.tickets
 end
 
 function Theater:playerRequest(data)

data/mission/theater/theater.goals
@@ -1,1 +1,22 @@
-theatergoals = {}
+blue = {
+	["flag"] = 45,
+	["tickets"]     = 100,
+	["player-cost"] = 1,
+	["modifier-reward"] = 0.5,
+	["modifier-loss"]   = 0.2,
+}
+
+neutral = {
+	["flag"] = 20,
+	["tickets"] = 0,
+	["difficulty"] = "realistic",
+}
+
+red = {
+	["flag"] = 60,
+	["tickets"]     = 200,
+	["difficulty"] = "easy",
+	["player-cost"] = 1,
+	["modifier-reward"] = 0.5,
+	["modifier-loss"]   = 0.2,
+}

src/dct/ai/Mission.lua
@@ -17,6 +17,15 @@ local uicmds   = require("dct.ui.cmds")
 
 local MISSION_LIMIT = 60*60*3  -- 3 hours in seconds
 
+local function composeBriefing(msn, tgt)
+	local briefing = tgt.briefing
+	local interptbl = {
+		["TOT"] = os.date("!%F %Rz",
+			dctutils.zulutime(msn:getTimeout()*.6)),
+	}
+	return dctutils.interp(briefing, interptbl)
+end
+
 local Mission = class()
 function Mission:__init(cmdr, missiontype, grpname, tgtname)
 	self._complete = false
@@ -37,22 +46,14 @@ function Mission:__init(cmdr, missiontype, grpname, tgtname)
 
 	-- compose the briefing at mission creation to represent
 	-- known intel the pilots were given before departing
-	self.briefing  = self:_composeBriefing()
-	self.cmdr:getAsset(tgtname):setTargeted(self.cmdr.owner, true)
+	local tgt = self.cmdr:getAsset(tgtname)
+	self.briefing  = composeBriefing(self, tgt)
+	tgt:addObserver(self.onTgtEvent, self, "Mission.onDCTEvent")
+	tgt:setTargeted(self.cmdr.owner, true)
 	self:addAssigned(self.cmdr:getAsset(grpname))
 
 	-- TODO: setup remaining mission parameters;
 	--   * mission world states
-end
-
-function Mission:_composeBriefing()
-	local tgt = self.cmdr:getAsset(self.target)
-	local briefing = tgt.briefing
-	local interptbl = {
-		["TOT"] = os.date("!%F %Rz",
-			dctutils.zulutime(self:getTimeout()*.6)),
-	}
-	return dctutils.interp(briefing, interptbl)
 end
 
 function Mission:getID()
@@ -112,6 +113,32 @@ function Mission:abort(asset)
 	return self.id
 end
 
+function Mission:queueabort(reason)
+	self:_setComplete()
+	local theater = dct.Theater.singleton()
+	for _, name in ipairs(self.assigned) do
+		local request = {
+			["type"]   = enum.uiRequestType.MISSIONABORT,
+			["name"]   = name,
+			["value"]  = reason,
+		}
+		-- We have to use theater:queueCommand() to bypass the
+		-- limiting of players sending too many commands
+		theater:queueCommand(10, uicmds[request.type](theater, request))
+	end
+end
+
+function Mission:onTgtEvent(event)
+	if event.id ~= enum.event.DCT_EVENT_DEAD then
+		return
+	end
+	local tgt = event.initiator
+	dct.Theater.singleton():getTickets():reward(self.cmdr.owner,
+		tgt.cost, true)
+	tgt:removeObserver(self.onTgtEvent)
+	self:queueabort(enum.missionAbortType.COMPLETE)
+end
+
 -- for now just track if the mission has not timmed out and
 -- if it has queue an abort command with abort reason
 function Mission:update(_)
@@ -119,29 +146,14 @@ function Mission:update(_)
 		return
 	end
 
-	local reason
-	local tgt = self.cmdr:getAsset(self.target)
-	if tgt == nil or tgt:isDead() then
-		reason = enum.missionAbortType.COMPLETE
-		self._complete = true
-	elseif timer.getAbsTime() > self.timeend then
-		reason = enum.missionAbortType.TIMEOUT
-	end
-
-	if reason ~= nil then
-		for _, name in ipairs(self.assigned) do
-			local request = {
-				["type"]   = enum.uiRequestType.MISSIONABORT,
-				["name"]   = name,
-				["value"]  = reason,
-			}
-			-- We have to use theater:queueCommand() to bypass the
-			-- limiting of players sending too many commands
-			local theater = require("dct.Theater").singleton()
-			theater:queueCommand(10, uicmds[request.type](theater, request))
-		end
+	if timer.getAbsTime() > self.timeend then
+		self:queueabort(enum.missionAbortType.TIMEOUT)
 	end
 	return
+end
+
+function Mission:_setComplete()
+	self._complete = true
 end
 
 function Mission:isComplete()

src/dct/assets/AssetBase.lua
@@ -93,7 +93,8 @@ function AssetBase:__init(template, region)
 		"rgnname",
 		"tplname",
 		"name",
-		"codename",})
+		"codename",
+		"cost",})
 	self._spawned    = false
 	self._dead       = false
 	self._targeted   = {}
@@ -128,6 +129,7 @@ function AssetBase:_completeinit(template, region)
 	self.owner    = template.coalition
 	self.rgnname  = region.name
 	self.tplname  = template.name
+	self.cost     = math.abs(template.cost)
 	if norenametype[self.type] == true then
 		self.name = self.tplname
 	else

src/dct/assets/AssetManager.lua
@@ -145,7 +145,11 @@ end
 
 local function handleAssetDeath(self, event)
 	local asset = event.initiator
-	self:remove(asset)
+	dct.Theater.singleton():getTickets():loss(asset.owner,
+		asset.cost, false)
+	if asset.type ~= enum.assetType.PLAYERGROUP then
+		self:remove(asset)
+	end
 end
 
 local handlers = {

src/dct/assets/Player.lua
@@ -7,35 +7,209 @@
 -- A player asset doesn't die, is always spawned, never
 -- reduces status, and is associated with a squadron.
 -- Optionally the player can be associated with an airbase.
+--
+-- ## Ticket Consumption
+-- Players only consume a ticket when a when they die or otherwise
+-- leave the slot is an 'invalid' way. A valid way to leave a slot
+-- is at an authorized airbase.
+--
+-- ## Slot Management
+-- Spawned player objects are used as a signal for enabling/disabling
+-- the slot. This is additionally combined with a transient state
+-- potentially defined in the Player's State class (EmptyState or
+-- OccupiedState) to ultimately determine if the slot is 'enabled'.
+-- The hooks script used for slot management can utilize this by
+-- directly asking the Player object if it is enabled via the
+-- 'isEnabled()' API call.
+-- This API call will combine the spawned(S) and kick pending(K)
+-- states to prevent a race between posting a kick request
+-- and affecting the kick in the hooks script. The boolean table
+-- below shows the logic governing when a slot is considered
+-- 'enabled';
+--     EmptyState:     enabled = S & !K
+--     OccupiedState:  enabled = S; we really don't care through
+--                                  because the slot should be
+--                                  occupied
 --]]
 
+require("math")
 local class = require("libs.class")
 local AssetBase = require("dct.assets.AssetBase")
 local uimenu  = require("dct.ui.groupmenu")
 local Logger  = dct.Logger.getByName("Asset")
 local loadout = require("dct.systems.loadouts")
+local State   = require("dct.libs.State")
+local Command = require("dct.Command")
 local settings = _G.dct.settings
+
+local notifymsg =
+	"Please read the loadout limits in the briefing and "..
+	"use the F10 Menu to validate your loadout before departing."
+
+local OccupiedState = class(State)
+local EmptyState    = class(State)
+function EmptyState:enter(asset)
+	asset:kick()
+end
+
+function EmptyState:onDCTEvent(asset, event)
+	if world.event.S_EVENT_BIRTH ~= event.id then
+		return nil
+	end
+
+	local theater = dct.Theater.singleton()
+	local grp = event.initiator:getGroup()
+	local id = grp:getID()
+	if asset.groupId ~= id then
+		Logger:warn(
+			string.format("(%s) - asset.groupId(%d) != object:getID(%d)",
+				asset.name, asset.groupId, id))
+	end
+	asset.groupId = id
+	uimenu.createMenu(asset)
+	local cmdr = theater:getCommander(grp:getCoalition())
+	local msn  = cmdr:getAssigned(asset)
+
+	if msn then
+		trigger.action.outTextForGroup(asset.groupId,
+			"Welcome. A mission is already assigned to this slot, "..
+			"use the F10 menu to get the briefing or find another.",
+			20, false)
+	else
+		trigger.action.outTextForGroup(asset.groupId,
+			"Welcome. Use the F10 Menu to get a theater update and "..
+			"request a mission.",
+			20, false)
+	end
+	trigger.action.outTextForGroup(asset.groupId, notifymsg, 20, false)
+	return OccupiedState(event.initiator:inAir())
+end
+
+function OccupiedState:__init(inair)
+	self.inair = inair
+	self.loseticket = false
+	self.bleedctr = 0
+	self.bleedperiod = 5
+	self.bleedwarn = false
+	self._eventhandlers = {
+		[world.event.S_EVENT_TAKEOFF]           = self.handleTakeoff,
+		[world.event.S_EVENT_EJECTION]          = self.handleLoseTicket,
+		[world.event.S_EVENT_DEAD]              = self.handleLoseTicket,
+		[world.event.S_EVENT_PILOT_DEAD]        = self.handleLoseTicket,
+		[world.event.S_EVENT_CRASH]             = self.handleLoseTicket,
+		[world.event.S_EVENT_PLAYER_LEAVE_UNIT] = self.handleSwitchEmpty,
+		[world.event.S_EVENT_LAND]              = self.handleLand,
+	}
+end
+
+function OccupiedState:enter(asset)
+	asset:setDead(false)
+end
+
+function OccupiedState:exit(asset)
+	if self.loseticket then
+		asset:setDead(true)
+	end
+end
+
+function OccupiedState:_bleed(asset)
+	local theater = dct.Theater.singleton()
+	local tickets = theater:getTickets()
+	if not (tickets:getConfig(asset.owner).bleed and
+		self.inair == true) then
+		return nil
+	end
+
+	local cmdr = theater:getCommander(asset.owner)
+	local msn  = cmdr:getAssigned(asset)
+	if msn then
+		self.bleedctr = 0
+		self.bleedwarn = false
+	else
+		self.bleedctr = self.bleedctr + 1
+	end
+
+	local state = nil
+	if not self.bleedwarn and
+	   self.bleedctr > math.floor(self.bleedperiod / 2) then
+		self.bleedwarn = true
+		trigger.action.outTextForGroup(asset.groupId,
+			"WARNING! You do not have a mission assigned, land or obtain "..
+			"a mission or you will be kicked.",
+			20, true)
+	end
+	if self.bleedctr >= self.bleedperiod then
+		self.loseticket = true
+		self.bleedctr = 0
+		trigger.action.outTextForGroup(asset.groupId,
+			"You have been kicked for not having a mission assigned.",
+			20, true)
+		state = EmptyState()
+	end
+	return state
+end
+
+function OccupiedState:update(asset)
+	local grp = Group.getByName(asset.name)
+	if grp == nil then
+		return EmptyState()
+	end
+	return self:_bleed(asset)
+end
+
+function OccupiedState:onDCTEvent(asset, event)
+	local handler = self._eventhandlers[event.id]
+	Logger:debug(string.format(
+		"OccupiedState:onDCTEvent; event.id: %d, handler: %s",
+		event.id, tostring(handler)))
+	local state
+	if handler ~= nil then
+		state = handler(self, asset, event)
+	end
+	return state
+end
+
+function OccupiedState:handleTakeoff(asset, _ --[[event]])
+	self.loseticket = true
+	self.inair = true
+	local ok = loadout.check(asset)
+	if not ok then
+		trigger.action.outTextForGroup(asset.groupId,
+			"You have been removed to spectator for flying with an "..
+			"invalid loadout. "..notifymsg,
+			20, true)
+		return EmptyState()
+	end
+	return nil
+end
+
+function OccupiedState:handleLand(--[[asset, event]])
+	-- TODO: if returned to an authorized airbase clear loseticket flag
+	-- for now if they land at all we are ok
+	self.loseticket = false
+	self.inair = false
+	return nil
+end
+
+function OccupiedState:handleLoseTicket(--[[asset, event]])
+	self.loseticket = true
+	return EmptyState()
+end
+
+function OccupiedState:handleSwitchEmpty(--[[asset, event]])
+	return EmptyState()
+end
 
 --[[
 -- Player - represents a player slot in DCS
---
--- Slot Management
---   Player objects cannot die however they can be spawned. Spawning
---   is used as the signal for enabling/disabling the slot. The external
---   hooks script will check if this object is spawned before allowing
---   a player to enter the slot.
---
---   This covers disabling the other side is covering kicking a player
---   from a slot.
 --]]
 local Player = class(AssetBase)
 function Player:__init(template, region)
 	self.__clsname = "Player"
-	self._eventhandlers = {
-		[world.event.S_EVENT_BIRTH]   = self.handleBirth,
-		[world.event.S_EVENT_TAKEOFF] = self.handleTakeoff,
-	}
+	self.updateperiod = 90
 	AssetBase.__init(self, template, region)
+	trigger.action.setUserFlag(self.name, false)
+	trigger.action.setUserFlag(self.name.."_kick", false)
 	self.marshal   = nil
 	self.unmarshal = nil
 end
@@ -52,8 +226,13 @@ function Player:_completeinit(template, region)
 		self.ato = require("dct.enum").missionType
 	end
 	self.payloadlimits = settings.payloadlimits
-	trigger.action.setUserFlag(self.name, false)
-	trigger.action.setUserFlag(self.name.."_kick", false)
+end
+
+function Player:_setup()
+	self.state = EmptyState()
+	self.state:enter(self)
+	dct.Theater.singleton():queueCommand(self.updateperiod,
+		Command(self.update, self))
 end
 
 function Player:getObjectNames()
@@ -66,36 +245,22 @@ function Player:getLocation()
 	return self._location
 end
 
-function Player:handleBirth(event)
-	local theater = require("dct.Theater").singleton()
-	local grp = event.initiator:getGroup()
-	local id = grp:getID()
-	if self.groupId ~= id then
-		Logger:warn(
-			string.format("(%s) - asset.groupId(%d) != object:getID(%d)",
-				self.name, self.groupId, id))
+function Player:update()
+	local newstate = self.state:update(self)
+	if newstate ~= nil then
+		self.state:exit(self)
+		self.state = newstate
+		self.state:enter(self)
 	end
-	self.groupId = id
-	uimenu.createMenu(self)
-	local cmdr = theater:getCommander(grp:getCoalition())
-	local msn  = cmdr:getAssigned(self)
-
-	if msn then
-		trigger.action.outTextForGroup(self.groupId,
-			"Welcome. A mission is already assigned to this slot, "..
-			"use the F10 menu to get the briefing or find another.",
-			20, false)
-	else
-		trigger.action.outTextForGroup(self.groupId,
-			"Welcome. Use the F10 Menu to get a theater update and "..
-			"request a mission.",
-			20, false)
-	end
-	loadout.notify(self)
 end
 
-function Player:handleTakeoff(_ --[[event]])
-	loadout.kick(self)
+function Player:onDCTEvent(event)
+	local newstate = self.state:onDCTEvent(self, event)
+	if newstate ~= nil then
+		self.state:exit(self)
+		self.state = newstate
+		self.state:enter(self)
+	end
 end
 
 function Player:spawn()
@@ -109,10 +274,14 @@ function Player:despawn()
 end
 
 --[[
--- kick - cause the player to be removed from the slot
+-- kick - request player to be kicked from slot
 --
--- players are immediately removed from the slot, however, this
--- action does not prevent another player from joing the slot
+-- Posts a request for the player to be kicked from the slot.
+-- This depends on an outside DCS hooks script to be running
+-- which will kick the player from the slot and reset the
+-- kick flag.
+-- This will then allow the player state the be reset allowing
+-- another player to join the slot.
 --]]
 function Player:kick()
 	trigger.action.setUserFlag(self.name.."_kick", true)

src/dct/libs/State.lua
@@ -1,0 +1,26 @@
+--[[
+-- SPDX-License-Identifier: LGPL-3.0
+--
+-- A simple State interface.
+--]]
+
+local class = require("libs.class")
+
+local State = class()
+function State:enter(--[[asset]])
+end
+
+function State:exit(--[[asset]])
+end
+
+-- returns nil or a new state instance
+function State:update(--[[asset]])
+	return nil
+end
+
+-- returns nil or a new state instance
+function State:onDCTEvent(--[[asset, event]])
+	return nil
+end
+
+return State

src/dct/systems/loadouts.lua
@@ -54,48 +54,11 @@ local function validatePayload(grp, limits)
 	return true, total
 end
 
-local notifymsg =
-	"Please read the loadout limits in the briefing and "..
-	"use the F10 Menu to validate your loadout before departing."
 local loadout = {}
 
-function loadout.notify(player)
-	trigger.action.outTextForGroup(player.groupId, notifymsg, 20, false)
-end
-
-function loadout.kick(player)
-	local ok = validatePayload(Group.getByName(player.name),
-		player.payloadlimits)
-	if ok then
-		return
-	end
-
-	trigger.action.outTextForGroup(player.groupId,
-		"You have been removed to spectator for flying with an "..
-		"invalid loadout. "..notifymsg,
-		20, true)
-	player:kick()
-	return ok
-end
-
 function loadout.check(player)
-	local msg
-	local ok, costs = validatePayload(Group.getByName(player.name),
+	return validatePayload(Group.getByName(player.name),
 		player.payloadlimits)
-	if ok then
-		msg = "Valid loadout, you may depart. Good luck!"
-	else
-		msg = "You are over budget! Re-arm before departing, or "..
-			"you will be kicked to spectator!"
-	end
-
-	-- print cost summary
-	msg = msg.."\n== Loadout Summary:"
-	for cat, val in pairs(enum.weaponCategory) do
-		msg = msg ..string.format("\n\t%s cost: %d / %d",
-			cat, costs[val].current, costs[val].max)
-	end
-	return msg
 end
 
 function loadout.addmenu(asset, menu, handler, context)

commit b2122076c9b55d7e88b4f1d9d958d3fa38262b9d

fix mission_end handling
On graceful server termination the state file was getting deleted even
if the campaign was not completed. This is because the mission_end event
is fired any time the mission ends. So we need to filter out cases
where the server is restarting and the campaign is not complete.

Closes: #125
Fixes: 903ade6231c7 ("tickets system: add basic ticket accounting system")

src/dct/Theater.lua
@@ -214,9 +214,14 @@ end
 function Theater:onEvent(event)
 	self:notify(event)
 	if event.id == world.event.S_EVENT_MISSION_END then
-		local ok, err = os.remove(settings.statepath)
-		if not ok then
-			Logger:error("unable to remove statepath; "..err)
+		-- Only delete the state if there is an end mission event
+		-- and tickets are complete, otherwise when a server is
+		-- shutdown gracefully the state will be deleted.
+		if self.tickets:isComplete() then
+			local ok, err = os.remove(settings.statepath)
+			if not ok then
+				Logger:error("unable to remove statepath; "..err)
+			end
 		end
 	end
 end