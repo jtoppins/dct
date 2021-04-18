jtoppins/dct

commit 65ebfaf0d72e9359572e6768c0df7db67467712b

template: set .dct as priority over .stm
Reversed order of variables passed to the function to make DCT File
contents take priority over STM file contents. DCT file is the user
created file so it makes sense that variables manually defined within it
should take priority. This allows values such as the description field
to be defined in the .dct file without having to edit the .stm file.

Closes: #166

src/dct/templates/Template.lua
@@ -423,8 +423,9 @@ function Template.fromFile(regionname, dctfile, stmfile)
 	template.regionname = regionname
 	template.path = dctfile
 	if stmfile ~= nil then
-		template = utils.mergetables(template,
-			STM.transform(utils.readlua(stmfile, "staticTemplate")))
+		template = utils.mergetables(
+			STM.transform(utils.readlua(stmfile, "staticTemplate")),
+			template)
 	end
 	return Template(template)
 end

commit 0aa7d142048b3522a8bafc47cdacf93dad3700d9

template: fix setting of mission description in .dct file
Fixes: 65ebfaf0d72e ("template: set .dct as priority over .stm")

src/dct/templates/Template.lua
@@ -291,7 +291,7 @@ local function getkeys(objtype)
 		}, {
 			["name"]    = "desc",
 			["type"]    = "string",
-			["default"] = "default template description",
+			["default"] = "false",
 		},{
 			["name"]    = "codename",
 			["type"]    = "string",
@@ -439,6 +439,9 @@ function Template.fromFile(region, dctfile, stmfile)
 	template.regionname = region.name
 	template.regionprio = region.priority
 	template.path = dctfile
+	if template.desc == "false" then
+		template.desc = nil
+	end
 	if stmfile ~= nil then
 		template = utils.mergetables(
 			STM.transform(utils.readlua(stmfile, "staticTemplate")),