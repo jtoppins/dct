jtoppins/dct

commit 5797d50c46e466acbf5c4390084ab693beb2e9c1

theater: fixup land/takeoff events
Some FARPs do not emmit takeoff or landing events. This attempts to
detect if a FARP is within a defined radius and amends the event object
to include the airbase.

src/dct/Theater.lua
@@ -251,12 +251,44 @@ function Theater:delayedInit()
 	self:postinitSystems()
 end
 
+local function handlefarps(airbase, event)
+	if event.place ~= nil or
+	   Airbase.Category.HELIPAD ~= airbase:getDesc().category then
+		return
+	end
+	event.place = airbase
+end
+
+local airbase_events = {
+	[world.event.S_EVENT_TAKEOFF] = true,
+	[world.event.S_EVENT_LAND]    = true,
+}
+
+-- some airbases (invisible FARPs seems to be the only one currently)
+-- do not trigger takeoff and land events, this function figured out
+-- if there is a FARP near the event and if so uses that FARP as the
+-- place for the event.
+local function fixup_airbase(event)
+	if airbase_events[event.id] == nil or event.place ~= nil then
+		return
+	end
+	local vol = {
+		id = world.VolumeType.SPHERE,
+		params = {
+			point  = event.initiator:getPoint(),
+			radius = 700, -- meters
+		},
+	}
+	world.searchObjects(Object.Category.BASE, vol, handlefarps, event)
+end
+
 -- DCS looks for this function in any table we register with the world
 -- event handler
 function Theater:onEvent(event)
 	if event.id == world.event.S_EVENT_BASE_CAPTURED then
 		return
 	end
+	fixup_airbase(event)
 	self:notify(event)
 	if event.id == world.event.S_EVENT_MISSION_END then
 		-- Only delete the state if there is an end mission event

commit 33f14c8286c5f449b67e6fbb1a303741408d2fbe

theater: fix detection of farp objects
Detect if there is either a farp or invisible farp within 700 meters and
if so count that as if the unit landed at the farp even though the game
felt the unit did not.

Fixes: 5797d50c46e4 ("theater: fixup land/takeoff events")

src/dct/Theater.lua
@@ -253,9 +253,15 @@ function Theater:delayedInit()
 	self:postinitSystems()
 end
 
+local airbase_cats = {
+	[Airbase.Category.HELIPAD] = true,
+	[Airbase.Category.SHIP]    = true,
+}
+
 local function handlefarps(airbase, event)
 	if event.place ~= nil or
-	   Airbase.Category.HELIPAD ~= airbase:getDesc().category then
+	   airbase:getCategory() ~= Object.Category.BASE or
+	   airbase_cats[airbase:getDesc().category] == nil then
 		return
 	end
 	event.place = airbase