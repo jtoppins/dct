jtoppins/dct

commit adcbe02a61b39b96ff7fb790245dea8a46493d5c

systems: while restoring state ignore scenery events
Also, change the storage of scenery ids to be a set instead of a list.

Closes: #149

src/dct/Theater.lua
@@ -134,6 +134,14 @@ function Theater:addSystem(path)
 	Logger:info("init "..path)
 end
 
+function Theater:postinitSystems()
+	for _, sys in pairs(self._systems) do
+		if type(sys.initpost) == "function" then
+			sys:initpost(self)
+		end
+	end
+end
+
 function Theater:loadSystems()
 	local systems = {
 		"dct.ui.scratchpad",
@@ -232,6 +240,7 @@ end
 function Theater:delayedInit()
 	self:loadPlayerSlots()
 	self:loadOrGenerate()
+	self:postinitSystems()
 end
 
 -- DCS looks for this function in any table we register with the world

src/dct/assets/StaticAsset.lua
@@ -180,6 +180,10 @@ function StaticAsset:handleDead(event)
 		end
 	else
 		self._assets[unitname].data.dct_dead = true
+		if self._assets[unitname].category == enum.UNIT_CAT_SCENERY then
+			dct.Theater.singleton():getSystem(
+				"dct.systems.bldgPersist"):addObject(unitname)
+		end
 		local goal = self._deathgoals[unitname]
 		if goal and goal:checkComplete() then
 			self:_removeDeathGoal(unitname, goal)

src/dct/systems/bldgPersist.lua
@@ -9,23 +9,28 @@ local Marshallable = require("dct.libs.Marshallable")
 local Logger = require("dct.libs.Logger").getByName("System")
 
 local SceneryTracker = class("SceneryTracker", Marshallable)
-function SceneryTracker:__init(theater)
+function SceneryTracker:__init()
 	Marshallable.__init(self)
 	self.destroyed = {}
-	theater:addObserver(self.onDCSEvent, self,
-		self.__clsname..".onDCSEvent")
 	self:_addMarshalNames({
 		"destroyed",
 	})
 end
 
+function SceneryTracker:postinit(theater)
+	theater:addObserver(self.onDCSEvent, self,
+		self.__clsname..".onDCSEvent")
+end
+
 function SceneryTracker:_unmarshalpost()
-	for _, bldg in pairs(self.destroyed) do
-		local id = tonumber(bldg)
-		local obj = {id_ = id}
-		local pt = Object.getPoint(obj)
-		trigger.action.explosion(pt, 500)
+	for bldg, _ in pairs(self.destroyed) do
+		local pt = Object.getPoint({id_ = tonumber(bldg)})
+		trigger.action.explosion(pt, 250)
 	end
+end
+
+function SceneryTracker:addObject(id)
+	self.destroyed[tostring(id)] = true
 end
 
 function SceneryTracker:onDCSEvent(event)
@@ -36,7 +41,7 @@ function SceneryTracker:onDCSEvent(event)
 	end
 	local obj = event.initiator
 	if obj:getCategory() == Object.Category.SCENERY then
-		table.insert(self.destroyed, tostring(obj:getName()))
+		self:addObject(obj:getName())
 	end
 end
 

commit 8c02854689a0f661b22ca59562e8a768fa6973b8

systems: fix calling of postinit method
The original commit incorrectly attempting to call initpost as the
method to be called after initialization was complete. This had the
effect of disabling scenery descruction tracking in DCT.

Fixes: adcbe02a61b3 ("systems: while restoring state ignore scenery events")

src/dct/Theater.lua
@@ -144,8 +144,8 @@ end
 
 function Theater:postinitSystems()
 	for _, sys in pairs(self._systems) do
-		if type(sys.initpost) == "function" then
-			sys:initpost(self)
+		if type(sys.postinit) == "function" then
+			sys:postinit(self)
 		end
 	end
 end