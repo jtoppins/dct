jtoppins/dct

commit be8ec879eae2ac2ef7df166f2929c404fdcd8799

hooks: stop checking enable flag each frame
Check for "DCTFLAG" on simulation resume and cache the result.
This makes the hook as light as possible.

hooks/dct-hook.lua
@@ -364,7 +364,11 @@ end
 
 function DCTHooks:onSimulationResume()
 	log.write(facility, log.DEBUG, "onSimulationResume")
-	if not DCS.isServer() then
+	local dctenabled = do_rpc("server", rpc_get_flag(dctflag), "number")
+	if not DCS.isServer() or not dctenabled then
+		log.write(facility, log.DEBUG,
+			string.format("not DCT enabled; server(%s), enabled(%s)",
+				tostring(DCS.isServer()), tostring(dctenabled)))
 		return
 	end
 	self:start()
@@ -450,8 +454,7 @@ function DCTHooks:onPlayerChangeSlot(id)
 end
 
 function DCTHooks:isEnabled()
-	local dctenabled = do_rpc("server", rpc_get_flag(dctflag), "number")
-	return self.started and dctenabled
+	return self.started
 end
 
 -- Returns: true - allows slot change, false - denies change

commit 20106dddf533eadd109246e29d288f1702774049

hook: fix blocking of slots in non-DCT missions
Fixes: be8ec879eae2 ("hooks: stop checking enable flag each frame")

hooks/dct-hook.lua
@@ -382,7 +382,7 @@ end
 function DCTHooks:onSimulationResume()
 	log.write(facility, log.DEBUG, "onSimulationResume")
 	local dctenabled = do_rpc("server", rpc_get_flag(dctflag), "number")
-	if not DCS.isServer() or not dctenabled then
+	if not DCS.isServer() or dctenabled == 0 then
 		log.write(facility, log.DEBUG,
 			string.format("not DCT enabled; server(%s), enabled(%s)",
 				tostring(DCS.isServer()), tostring(dctenabled)))