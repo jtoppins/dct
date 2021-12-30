jtoppins/dct

commit b5d9f539ada0331bdc7bbcf833e2dfc7322b11be

region: create a region manager
This change moves everything to do with region management under one
class instead of it being sprinkled between the Theater and Region
classes.

data/DCT/theaters/Caucasus_dctdemo/BlackSea/region.def
@@ -1,10 +1,12 @@
-region = {
-		["name"] = "BlackSea",
-		["priority"] = 30,
-		["limits"] = 	{
-			["airbase"] = 		{
-				["min"] = 1,
-				["max"] = 1,
-			}
-		}
+name = "BlackSea"
+priority = 30
+location = { x = -343565.90226497, y = 504579.59151324, }
+limits = 	{
+	["airbase"] = 		{
+		["min"] = 1,
+		["max"] = 1,
 	}
+}
+links = {
+	["air"] = { "Krymsk", "Senaki", },
+}

data/DCT/theaters/Caucasus_dctdemo/Krasnodar/region.def
@@ -1,22 +1,23 @@
 region = {
-		["name"] = "Krasnodar",
-		["priority"] = 10,
-		["limits"] = 	{
-			["fueldump"] = 		{
-				["min"] = 1,
-				["max"] = 1
-			},
-			["ewr"] = 		{
-				["min"] = 1,
-				["max"] = 1
-			},
-			["sam"] = 		{
-				["min"] = 1,
-				["max"] = 1
-			},
-			["factory"] = 		{
-				["min"] = 1,
-				["max"] = 1
-			}
+	["name"] = "Krasnodar",
+	["priority"] = 10,
+	["location"] = { x = 19695.256648279, y = 362381.31643785, },
+	["limits"] = 	{
+		["fueldump"] = 		{
+			["min"] = 1,
+			["max"] = 1
+		},
+		["ewr"] = 		{
+			["min"] = 1,
+			["max"] = 1
+		},
+		["sam"] = 		{
+			["min"] = 1,
+			["max"] = 1
+		},
+		["factory"] = 		{
+			["min"] = 1,
+			["max"] = 1
 		}
 	}
+}

data/DCT/theaters/Caucasus_dctdemo/Krymsk/region.def
@@ -1,10 +1,11 @@
 region = {
-		["name"] = "Krymsk",
-		["priority"] = 30,
-		["limits"] = 	{
-			["airbase"] = 		{
-				["min"] = 1,
-				["max"] = 1
-			}
+	["name"] = "Krymsk",
+	["priority"] = 30,
+	["location"] = { x = -5708.4484423691, y = 289578.30846916, },
+	["limits"] = 	{
+		["airbase"] = 		{
+			["min"] = 1,
+			["max"] = 1
 		}
 	}
+}

data/DCT/theaters/Caucasus_dctdemo/Novorossiysk/region.def
@@ -1,6 +1,7 @@
 region = {
 		["name"] = "Novorossiysk",
 		["priority"] = 20,
+		["location"] = { x = -34693.616778727, y = 279318.25638389, },
 		["limits"] = 	{
 			["port"] = 		{
 				["min"] = 1,

src/dct/templates/Region.lua
@@ -6,16 +6,29 @@
 
 require("lfs")
 require("math")
-local class      = require("libs.class")
+local class      = require("libs.namedclass")
 local utils      = require("libs.utils")
 local dctenums   = require("dct.enum")
-local dctutils   = require("dct.utils")
+local Marshallable = require("dct.libs.Marshallable")
 local Template   = require("dct.templates.Template")
 local Logger     = dct.Logger.getByName("Region")
 
 local tplkind = {
 	["TEMPLATE"]  = 1,
 	["EXCLUSION"] = 2,
+}
+
+local DOMAIN = {
+	["AIR"]  = "air",
+	["LAND"] = "land",
+	["SEA"]  = "sea",
+}
+
+local STATUS = {
+	["CONTESTED"] = -1,
+	["NEUTRAL"]   = coalition.side.NEUTRAL,
+	["RED"]       = coalition.side.RED,
+	["BLUE"]      = coalition.side.BLUE,
 }
 
 local function processlimits(_, tbl)
@@ -36,28 +49,46 @@ local function processlimits(_, tbl)
 	return true
 end
 
+local function processlinks(keydata, tbl)
+	local links = {}
+	for k, v in pairs(tbl[keydata.name]) do
+		local d = string.upper(k)
+		if DOMAIN[d] ~= nil then
+			links[DOMAIN[d]] = v
+		end
+	end
+	tbl[keydata.name] = links
+	return true
+end
+
 local function loadMetadata(self, regiondefpath)
 	Logger:debug("=> regiondefpath: "..regiondefpath)
 	local keys = {
-		[1] = {
+		{
 			["name"] = "name",
 			["type"] = "string",
-		},
-		[2] = {
+		}, {
 			["name"] = "priority",
 			["type"] = "number",
-		},
-		[3] = {
+		}, {
+			["name"] = "location",
+			["type"] = "table",
+			["check"] = Template.checklocation
+		}, {
 			["name"] = "limits",
 			["type"] = "table",
 			["default"] = {},
 			["check"] = processlimits,
-		},
-		[4] = {
-			["name"] = "airspace",
-			["type"] = "boolean",
-			["default"] = true,
-		},
+		}, {
+			["name"] = "altitude_floor",
+			["type"] = "number",
+			["default"] = 914.4, -- meters; 3000ft msl
+		}, {
+			["name"] = "links",
+			["type"] = "table",
+			["check"] = processlinks,
+			["default"] = {},
+		}
 	}
 
 	local region = utils.readlua(regiondefpath)
@@ -65,7 +96,9 @@ local function loadMetadata(self, regiondefpath)
 		region = region.region
 	end
 	region.defpath = regiondefpath
+	region.path = regiondefpath
 	utils.checkkeys(keys, region)
+	region.path = nil
 	utils.mergetables(self, region)
 end
 
@@ -123,8 +156,7 @@ local function registerType(self, kind, ttype, name)
 	table.insert(self._tpltypes[ttype], entry)
 end
 
-local function addAndSpawnAsset(self, name, assetmgr, centroid)
-	centroid = centroid or {}
+local function addAndSpawnAsset(self, name, assetmgr)
 	if name == nil then
 		return nil
 	end
@@ -138,11 +170,6 @@ local function addAndSpawnAsset(self, name, assetmgr, centroid)
 	local asset = mgr:factory(tpl.objtype)(tpl, self)
 	assetmgr:add(asset)
 	asset:generate(assetmgr, self)
-	local location = asset:getLocation()
-	if location then
-		centroid.point, centroid.n = dctutils.centroid2D(location,
-			centroid.point, centroid.n)
-	end
 	return asset
 end
 
@@ -189,17 +216,35 @@ end
 --              assets to spawn from a given asset type
 --              [<objtype>] = { ["min"] = <num>, ["max"] = <num>, }
 --]]
-local Region = class()
+local Region = class("Region", Marshallable)
 function Region:__init(regionpath)
+	Marshallable.__init(self)
+	self:_addMarshalNames({
+		"location",
+		"links",
+	})
+
 	self.path          = regionpath
 	self._templates    = {}
 	self._tpltypes     = {}
 	self._exclusions   = {}
+	self.centroid      = {}
+	self.weight        = {}
+	for _, side in pairs(coalition.side) do
+		self.weight[side] = 0
+	end
+	self.owner         = STATUS.NEUTRAL
+	self.DOMAIN        = nil
+	self.STATUS        = nil
+
 	Logger:debug("=> regionpath: "..regionpath)
 	loadMetadata(self, regionpath..utils.sep.."region.def")
 	getTemplates(self, self.path)
 	Logger:debug("'"..self.name.."' Loaded")
 end
+
+Region.DOMAIN = DOMAIN
+Region.STATUS = STATUS
 
 function Region:addTemplate(tpl)
 	assert(self._templates[tpl.name] == nil,
@@ -230,7 +275,7 @@ function Region:getTemplateByName(name)
 	return self._templates[name]
 end
 
-function Region:_generate(assetmgr, objtype, names, centroid)
+function Region:_generate(assetmgr, objtype, names)
 	local limits = {
 		["min"]     = #names,
 		["max"]     = #names,
@@ -247,7 +292,7 @@ function Region:_generate(assetmgr, objtype, names, centroid)
 	for i, tpl in ipairs(names) do
 		if tpl.kind ~= tplkind.EXCLUSION and
 			self._templates[tpl.name].spawnalways == true then
-			addAndSpawnAsset(self, tpl.name, assetmgr, centroid)
+			addAndSpawnAsset(self, tpl.name, assetmgr)
 			table.remove(names, i)
 			limits.current = 1 + limits.current
 		end
@@ -260,7 +305,7 @@ function Region:_generate(assetmgr, objtype, names, centroid)
 			local i = math.random(1, #self._exclusions[name].names)
 			name = self._exclusions[name]["names"][i]
 		end
-		addAndSpawnAsset(self, name, assetmgr, centroid)
+		addAndSpawnAsset(self, name, assetmgr)
 		table.remove(names, idx)
 		limits.current = 1 + limits.current
 	end
@@ -271,42 +316,99 @@ end
 -- that asset with the asset manager (provided) and spawn
 -- the asset into the game world. Region generation should
 -- be limited to mission startup.
-function Region:generate(assetmgr)
+function Region:generate()
+	local assetmgr = dct.Theater.singleton():getAssetMgr()
 	local tpltypes = utils.deepcopy(self._tpltypes)
-	local centroid = {}
 
 	for objtype, _ in pairs(dctenums.assetClass.INITIALIZE) do
 		local names = tpltypes[objtype]
 		if names ~= nil then
-			self:_generate(assetmgr, objtype, names, centroid)
+			self:_generate(assetmgr, objtype, names)
 		end
 	end
+end
 
-	-- do not create an airspace object if not wanted
-	if self.airspace ~= true then
+function Region:getWeight(side)
+	return self.weight[side]
+end
+
+function Region:getPoint()
+	return self.location
+end
+
+function Region:getEdges(domain)
+	assert(utils.getkey(Region.DOMAIN, domain),
+		"value error: invalid domain")
+	return utils.deepcopy(self.links[domain])
+end
+
+local function get_asset_weight(asset)
+	local weight = asset.cost
+	if weight == 0 then
+		weight = 1
+	end
+	Logger:debug("asset weight("..asset.name.."): "..tostring(weight))
+	return weight
+end
+
+local function handleDead(region, event)
+	local asset = event.initiator
+	region.weight[asset.owner] = region.weight[asset.owner] -
+		get_asset_weight(asset)
+	if region.weight[asset.owner] < 0 then
+		region.weight[asset.owner] = 0
+	end
+	Logger:debug("Region("..region.name..").handleDead - "..
+		"new weight: "..tostring(region.weight[asset.owner]))
+end
+
+local function handleAddAsset(region, event)
+	local asset = event.initiator
+	region.weight[asset.owner] = region.weight[asset.owner] +
+		get_asset_weight(asset)
+	Logger:debug("Region("..region.name..").handleAddAsset - "..
+		"new weight: "..tostring(region.weight[asset.owner]))
+end
+
+local handlers = {
+	[dctenums.event.DCT_EVENT_DEAD] = handleDead,
+	[dctenums.event.DCT_EVENT_ADD_ASSET] = handleAddAsset,
+}
+
+function Region:onDCTEvent(event)
+	local side = coalition.side
+	local handler = handlers[event.id]
+
+	if handler == nil or
+	   dctenums.assetClass.STRATEGIC[event.initiator.type] == nil then
 		return
 	end
 
-	-- create airspace asset based on the centroid of this region
-	if centroid.point == nil then
-		centroid.point = { ["x"] = 0, ["y"] = 0, ["z"] = 0, }
+	handler(self, event)
+
+	if self.weight[side.RED] == 0 or self.weight[side.BLUE] == 0 then
+		if self.weight[side.RED] - self.weight[side.BLUE] == 0 then
+			self.owner = STATUS.NEUTRAL
+		else
+			if self.weight[side.RED] > self.weight[side.BLUE] then
+				self.owner = STATUS.RED
+			else
+				self.owner = STATUS.BLUE
+			end
+		end
+		return
 	end
-	self.location = centroid.point
-	local airspacetpl = Template({
-		["objtype"]    = "airspace",
-		["name"]       = "airspace",
-		["regionname"] = self.name,
-		["regionprio"] = 1000,
-		["desc"]       = "airspace",
-		["coalition"]  = coalition.side.NEUTRAL,
-		["location"]   = self.location,
-		["volume"]     = {
-			["point"]  = self.location,
-			["radius"] = 55560,  -- 30NM
-		},
-	})
-	self:addTemplate(airspacetpl)
-	addAndSpawnAsset(self, airspacetpl.name, assetmgr)
+
+	local c = 2
+	local ratioB = self.weight[side.BLUE] / self.weight[side.RED]
+
+	if ratioB > c then
+		self.owner = STATUS.BLUE
+	elseif ratioB < 1/c then
+		self.owner = STATUS.RED
+	else
+		self.owner = STATUS.CONTESTED
+	end
 end
 
 return Region

data/DCT/theaters/Caucasus_dctdemo/Senaki/region.def
@@ -1,6 +1,7 @@
 region = {
 		["name"] = "Senaki",
 		["priority"] = 30,
+		["location"] = { x = -280761.24089216, y = 646192.46982575, },
 		["limits"] = 	{
 			["airbase"] = 		{
 				["min"] = 2,

data/DCT/theaters/Caucasus_dctdemo/Sukhumi/region.def
@@ -1,6 +1,7 @@
 region = {
 		["name"] = "Sukhumi",
 		["priority"] = 20,
+		["location"] = { x = -201361.8782457, y = 557387.28601366, },
 		["limits"] = 	{
 			["ammodump"] = 		{
 				["min"] = 1,

src/dct/Theater.lua
@@ -7,7 +7,6 @@
 require("os")
 require("io")
 require("lfs")
-local utils       = require("libs.utils")
 local containers  = require("libs.containers")
 local json        = require("libs.json")
 local enum        = require("dct.enum")
@@ -16,14 +15,13 @@ local Observable  = require("dct.libs.Observable")
 local uicmds      = require("dct.ui.cmds")
 local STM         = require("dct.templates.STM")
 local Template    = require("dct.templates.Template")
-local Region      = require("dct.templates.Region")
 local AssetManager= require("dct.assets.AssetManager")
 local Commander   = require("dct.ai.Commander")
 local Command     = require("dct.Command")
 local Logger      = dct.Logger.getByName("Theater")
 local settings    = _G.dct.settings.server
 
-local STATE_VERSION = "1"
+local STATE_VERSION = "2"
 
 local function isPlayerGroup(grp, _, _)
 	local slotcnt = 0
@@ -90,7 +88,6 @@ function Theater:__init()
 	self:setTimings(settings.schedfreq, settings.tgtfps,
 		settings.percentTimeAllowed)
 	self.statef    = false
-	self.regions   = {}
 	self.qtimer    = require("dct.libs.Timer")(self.quanta, os.clock)
 	self.cmdq      = containers.PriorityQueue()
 	self.assetmgr  = AssetManager(self)
@@ -104,7 +101,6 @@ function Theater:__init()
 	end
 
 	self:loadSystems()
-	self:loadRegions()
 	self:queueCommand(5, Command(self.__clsname..".delayedInit",
 		self.delayedInit, self))
 	self:queueCommand(100, Command(self.__clsname..".export",
@@ -157,26 +153,11 @@ function Theater:loadSystems()
 		"dct.systems.bldgPersist",
 		"dct.systems.weaponstracking",
 		"dct.systems.blasteffects",
+		"dct.templates.RegionManager",
 	}
 
 	for _, syspath in ipairs(systems) do
 		self:addSystem(syspath)
-	end
-end
-
-function Theater:loadRegions()
-	for filename in lfs.dir(settings.theaterpath) do
-		if filename ~= "." and filename ~= ".." and
-			filename ~= ".git" and filename ~= "settings" then
-			local fpath = settings.theaterpath..utils.sep..filename
-			local fattr = lfs.attributes(fpath)
-			if fattr.mode == "directory" then
-				local r = Region(fpath)
-				assert(self.regions[r.name] == nil, "duplicate regions " ..
-					"defined for theater: " .. settings.theaterpath)
-				self.regions[r.name] = r
-			end
-		end
 	end
 end
 
@@ -229,9 +210,7 @@ function Theater:loadOrGenerate()
 		self:getAssetMgr():unmarshal(self.statetbl.assetmgr)
 	else
 		Logger:info("generating new theater")
-		for _, r in pairs(self.regions) do
-			r:generate(self.assetmgr)
-		end
+		self:getRegionMgr():generate()
 		-- TODO: temperary, spawn all generated assets
 		-- eventually we will want to spawn only a set of assets
 		local assetnames = self.assetmgr:filterAssets(function()
@@ -362,6 +341,10 @@ function Theater:export(_)
 	statefile:flush()
 	statefile:close()
 	return self.savestatefreq
+end
+
+function Theater:getRegionMgr()
+	return self:getSystem("dct.templates.RegionManager")
 end
 
 function Theater:getAssetMgr()

commit a63133cf1fc40ed09af60b69272878a874e153d7

region: do not require location be set
Do not require location to be defined in the region.def file.

Fixes: b5d9f539ada0 ("region: create a region manager")

data/DCT/theaters/Caucasus_dctdemo/BlackSea/region.def
@@ -1,6 +1,5 @@
 name = "BlackSea"
 priority = 30
-location = { x = -343565.90226497, y = 504579.59151324, }
 limits = 	{
 	["airbase"] = 		{
 		["min"] = 1,

data/DCT/theaters/Caucasus_dctdemo/Krasnodar/region.def
@@ -1,7 +1,6 @@
 region = {
 	["name"] = "Krasnodar",
 	["priority"] = 10,
-	["location"] = { x = 19695.256648279, y = 362381.31643785, },
 	["limits"] = 	{
 		["fueldump"] = 		{
 			["min"] = 1,

data/DCT/theaters/Caucasus_dctdemo/Krymsk/region.def
@@ -1,7 +1,6 @@
 region = {
 	["name"] = "Krymsk",
 	["priority"] = 30,
-	["location"] = { x = -5708.4484423691, y = 289578.30846916, },
 	["limits"] = 	{
 		["airbase"] = 		{
 			["min"] = 1,

data/DCT/theaters/Caucasus_dctdemo/Novorossiysk/region.def
@@ -1,7 +1,6 @@
 region = {
 		["name"] = "Novorossiysk",
 		["priority"] = 20,
-		["location"] = { x = -34693.616778727, y = 279318.25638389, },
 		["limits"] = 	{
 			["port"] = 		{
 				["min"] = 1,

src/dct/templates/Region.lua
@@ -74,6 +74,7 @@ local function loadMetadata(self, regiondefpath)
 		}, {
 			["name"] = "location",
 			["type"] = "table",
+			["default"] = {},
 			["check"] = Template.checklocation
 		}, {
 			["name"] = "limits",