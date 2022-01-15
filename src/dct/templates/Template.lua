jtoppins/dct

commit 955f88179ee145b9dbd7cbb5309082281913eb75

template: allow mixed coalition units if template owner is specified

src/dct/templates/Template.lua
@@ -122,8 +122,9 @@ local function overrideGroupOptions(grp, idx, tpl)
 	if grp.data.dct_deathgoal ~= nil then
 		tpl.hasDeathGoals = true
 	end
-	grp.data.name = tpl.regionname.."_"..tpl.name.." "..tpl.coalition.." "..
-		utils.getkey(Unit.Category, grp.category).." "..tostring(idx)
+	local side = coalition.getCountryCoalition(grp.countryid)
+	grp.data.name = string.format("%s_%s %d %s %d", tpl.regionname, tpl.name,
+		side, utils.getkey(Unit.Category, grp.category), idx)
 
 	for i, unit in ipairs(grp.data.units or {}) do
 		overrideUnitOptions(unit, i, tpl, grp.data.name)
@@ -145,8 +146,7 @@ local function checkbldgdata(keydata, tpl)
 
 	for _, bldg in ipairs(tpl[keydata.name]) do
 		local bldgdata = {}
-		bldgdata.countryid = 0
-		bldgdata.category  = enum.UNIT_CAT_SCENERY
+		bldgdata.category = enum.UNIT_CAT_SCENERY
 		bldgdata.data = {
 			["dct_deathgoal"] = goalFromName(bldg.goal,
 				Goal.objtype.SCENERY),
@@ -182,6 +182,8 @@ local function checkside(keydata, tbl)
 	elseif type(tbl[keydata.name]) == "string" and
 		coalition.side[string.upper(tbl[keydata.name])] ~= nil then
 		tbl[keydata.name] = coalition.side[string.upper(tbl[keydata.name])]
+		return true
+	elseif tbl[keydata.name] == nil then
 		return true
 	end
 	return false
@@ -466,6 +468,36 @@ function Template:__init(data)
 	self.fromFile = nil
 end
 
+Template.checklocation = checklocation
+
+-- checks if either there is a manually defined coalition,
+-- or if all units in the template are of the same coalition
+local function validateCoalition(tpl)
+	if tpl.coalition == nil then
+		for _, grp in pairs(tpl.tpldata) do
+			if grp.countryid ~= nil then
+				local groupCoalition = coalition.getCountryCoalition(grp.countryid)
+				tpl.coalition = tpl.coalition or groupCoalition
+				assert(tpl.coalition == groupCoalition, string.format(
+					"template '%s' contains mixed coalitions; one group belongs to "..
+					"country '%s', which is in the '%s' coalition, "..
+					"but another group in the template is in the '%s' coalition\n"..
+					"note: coalition checks are made according to the .miz, not the .stm\n"..
+					"note: if this is intentional, consider setting the coalition "..
+					"manually in the .dct",
+					tpl.name,
+					country.name[grp.countryid],
+					utils.getkey(coalition.side, groupCoalition),
+					utils.getkey(coalition.side, tpl.coalition)
+				))
+			end
+		end
+	end
+	assert(tpl.coalition ~= nil, string.format(
+		"cannot determine the coalition of template '%s' because it has no units; "..
+		"please set it manually in the .dct", tpl.name))
+end
+
 function Template:validate()
 	utils.checkkeys({ [1] = {
 		["name"]  = "objtype",
@@ -474,6 +506,7 @@ function Template:validate()
 	},}, self)
 
 	utils.checkkeys(getkeys(self.objtype), self)
+	validateCoalition(self)
 end
 
 -- PUBLIC INTERFACE

data/DCT/theaters/Caucasus_dctdemo/Novorossiysk/misc/NovoStolenAPCs.dct
@@ -1,0 +1,4 @@
+objtype = "specialforces"
+intel = 4
+cost = 2
+coalition = "red"

src/dct/templates/STM.lua
@@ -122,7 +122,7 @@ end
 --    }}}
 --]]
 
-function STM.transform(stmdata, file)
+function STM.transform(stmdata)
 	local template   = {}
 	local lookupname =  function(name)
 		if name == nil then
@@ -135,20 +135,6 @@ function STM.transform(stmdata, file)
 		end
 		return newname
 	end
-	local trackUniqueCoalition = function(_, cntryid, _)
-		local side = coalition.getCountryCoalition(cntryid)
-		if template.coalition == nil then
-			template.coalition = side
-		end
-		assert(template.coalition == side, string.format(
-			"runtime error: invalid STM; country(%s) does not belong "..
-			"to '%s' coalition, country belongs to '%s' coalition; file: %s",
-			country.name[cntryid],
-			tostring(utils.getkey(coalition.side, template.coalition)),
-			tostring(utils.getkey(coalition.side, side)),
-			file))
-		return true
-	end
 
 	template.name    = lookupname(stmdata.name)
 	template.theater = lookupname(stmdata.theatre)
@@ -156,11 +142,9 @@ function STM.transform(stmdata, file)
 	template.tpldata = {}
 
 	for _, coa_data in pairs(stmdata.coalition) do
-		for _, grp in ipairs(STM.processCoalition(coa_data,
-				lookupname,
-				trackUniqueCoalition,
-				modifyStatic)) do
-			table.insert(template.tpldata, grp)
+		local groups = STM.processCoalition(coa_data, lookupname, nil, modifyStatic)
+		for _, group in ipairs(groups) do
+			table.insert(template.tpldata, group)
 		end
 	end
 	return template

tests/test-init.lua
@@ -7,11 +7,11 @@ require("dct")
 local function main()
 	dct.init()
 	_G.dct.theater:exec(50)
-	local expected = 32
+	local expected = 34
 	assert(dctcheck.spawngroups == expected,
 		string.format("group spawn broken; expected(%d), got(%d)",
 		expected, dctcheck.spawngroups))
-	expected = 29
+	expected = 36
 	assert(dctcheck.spawnstatics == expected,
 		string.format("static spawn broken; expected(%d), got(%d)",
 		expected, dctcheck.spawnstatics))

tests/test-theater.lua
@@ -111,11 +111,11 @@ local function main()
 	_G.dct.theater = theater
 	theater.startdate = startdate
 	theater:exec(50)
-	local expected = 32
+	local expected = 34
 	assert(dctcheck.spawngroups == expected,
 		string.format("group spawn broken; expected(%d), got(%d)",
 		expected, dctcheck.spawngroups))
-	expected = 29
+	expected = 36
 	assert(dctcheck.spawnstatics == expected,
 		string.format("static spawn broken; expected(%d), got(%d)",
 		expected, dctcheck.spawnstatics))

tests/test-ui-cmds.lua
@@ -26,9 +26,9 @@ local unit1 = Unit({
 
 local briefingtxt = "Package: #5720\n"..
 			"IFF Codes: M1(50), M3(5720)\n"..
-			"Target AO: 88°07.38'N 063°27.36'W (TOKYO)\n"..
+			"Target AO: 88°07.38'N 063°27.36'W (DALLAS)\n"..
 			"Briefing:\n"..
-			"We have reason to believe there is"..
+			"Reconnaissance elements have located"..
 			" a fuel storage facility at 88°07.38'N 063°27.36'W,"..
 			" East of Krasnodar-Center.\n\n"..
 			"Primary Objectives: Destroy the fuel tanks embedded in "..

commit 396f55600b867d3dd3d7318633c848d35a44a854

template: fixup merge commit
Fixes: 955f88179ee1 ("template: allow mixed coalition units if template owner is specified")

src/dct/templates/Template.lua
@@ -468,8 +468,6 @@ function Template:__init(data)
 	self.fromFile = nil
 end
 
-Template.checklocation = checklocation
-
 -- checks if either there is a manually defined coalition,
 -- or if all units in the template are of the same coalition
 local function validateCoalition(tpl)