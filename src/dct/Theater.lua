d8b653a3401d59d30f5904d199fba0df9fa97993
Jonathan Toppins <jtoppins@users.noreply.github.com>
2022-08-23 05:26:45 -0400

    rename: move scratchpad to systems
    Also cleaned up the comment section in Theater.

--- src/dct/Theater.lua
+++ src/dct/Theater.lua
@@ -20,19 +20,17 @@ local Logger      = dct.Logger.getByName("Theater")
 local settings    = _G.dct.settings.server
 local STATE_VERSION = "3"
 
---[[--
- Component system. Defines a generic way for initializing components
- of the system without directly tying the two systems together.
- A system can provide the following methods:
-
- @function __init initialization only init local system data,
-   do not depend on external systems
- @function marshal all the system's data for serialization
- @function unmarshal initializes the system from the saved data
- @function generate Assets the system may need
- @function postinit run after __init and generate, guarantees all
-   assets are generated and all templates have been loaded
---]]
+--- Component system. Defines a generic way for initializing components
+-- of the system without directly tying the two systems together.
+-- A system can provide the following methods:
+--
+-- @function __init initialization only init local system data,
+--   do not depend on external systems
+-- @function marshal all the system's data for serialization
+-- @function unmarshal initializes the system from the saved data
+-- @function generate Assets the system may need
+-- @function postinit run after __init and generate, guarantees all
+--   assets are generated and all templates have been loaded
 local Systems = class("System")
 function Systems:__init()
 	self._systemscnt = 0
@@ -40,7 +38,7 @@ function Systems:__init()
 
 	local systems = {
 		"dct.assets.AssetManager",
-		"dct.ui.scratchpad",
+		"dct.systems.scratchpad",
 		"dct.systems.tickets",
 		"dct.systems.weaponstracking",
 		"dct.systems.blasteffects",
