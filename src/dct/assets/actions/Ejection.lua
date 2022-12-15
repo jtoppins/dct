jtoppins/dct

commit c8866a4e4e9d74fbcc8328d6a60b1347ba1846e3

assets: rewrite asset class hierarchy to use composition
Rewrote the asset class hierarchy to use composition inside a single
Agent class. Then layered on top a goal oriented action planner to allow
agents to react to the changing game world. This single Agent class
composed of different sensors, actions, and goals should remove the need
for the class inheritance hierarchy that existed previously.

This commit only covers the Player use case. We create player slot
Templates in a custom "__builtin" region as with the new Agent model
Player templates must exist in a region otherwise it will mess up the
template proxy key retrival in the Agent that is planned.

src/dct/assets/actions/Ejection.lua
@@ -1,0 +1,54 @@
+--- SPDX-License-Identifier: LGPL-3.0
+
+local WS = require("dct.assets.worldstate")
+
+local function is_eject_event(fact)
+	return fact.type == WS.Facts.types.EVENT and
+	       fact.event.id == world.event.S_EVENT_EJECTION
+end
+
+local function any_event(fact)
+	return fact.type == WS.Facts.types.EVENT
+end
+
+--- @class Ejection
+--
+local Ejection = require("libs.namedclass")("Ejection", WS.Action)
+function Ejection:__init(agent)
+	WS.Action.__init(self, agent, 90, {
+		-- pre-conditions
+		-- none
+	}, {
+		-- effects
+		WS.Property(WS.ID.REACTEDTOEVENT, true),
+	})
+	self.factkey = nil
+end
+
+function Ejection:checkProceduralPreconditions(--[[agent]])
+	local rc, key = self.agent:hasFact(is_eject_event)
+
+	if rc then
+		self.factkey = key
+	else
+		self.factkey = nil
+	end
+	return rc
+end
+
+function Ejection:enter()
+	--local fact = self.agent:getFact(self.factkey)
+
+	-- delete the fact from the Agent
+	self.agent:setFact(self, self.factkey, nil)
+	self.factkey = nil
+
+	-- TODO: create pilot agent
+	trigger.action.outText("TODO: pilot agent created", 10, false)
+
+	if not self.agent:hasFact(any_event) then
+		self.agent:WS():get(WS.ID.REACTEDTOEVENT).value = true
+	end
+end
+
+return Ejection

src/dct/assets/actions/PlayerAttack.lua
@@ -1,0 +1,52 @@
+--- SPDX-License-Identifier: LGPL-3.0
+
+local DCTEvents = require("dct.libs.DCTEvents")
+local Timer     = require("dct.libs.Timer")
+local WS        = require("dct.assets.worldstate")
+local UPDATE_TIME = 60
+
+local function is_target(fact)
+	return fact.type == WS.Facts.types.CONTACT and
+	       fact.threat and fact.threat.value >= 1
+end
+
+--- @class PlayerAttack
+-- Monitors the player Agent object and waits until all mission targets
+-- are dead.
+local PlayerAttack = require("libs.namedclass")("PlayerAttack", WS.Action,
+	DCTEvents)
+function PlayerAttack:__init(agent, cost)
+	WS.Action.__init(self, agent, cost, {
+		-- pre-conditions
+		-- none
+	}, {
+		-- effects
+		WS.Property(WS.ID.TARGETDEAD, true),
+	})
+	self.timer = Timer(UPDATE_TIME)
+end
+
+function PlayerAttack:enter()
+	self.timer:reset()
+	self.timer:start()
+end
+
+--- Test if the agent's fact list has any targets left. A "target" is defined
+-- as any contact fact with a threat of one.
+function PlayerAttack:isComplete()
+	self.timer:update()
+	if not self.timer:expired() then
+		return false
+	end
+
+	self.timer:reset()
+	for _, fact in self.agent:iterateFacts() do
+		if is_target(fact) then
+			return false
+		end
+	end
+	self:WS():get(WS.IS.TARGETDEAD).value = true
+	return true
+end
+
+return PlayerAttack

src/dct/assets/actions/PlayerJoin.lua
@@ -1,0 +1,74 @@
+--- SPDX-License-Identifier: LGPL-3.0
+
+local dctenum  = require("dct.enum")
+local dctutils = require("dct.libs.utils")
+local uimenu   = require("dct.ui.groupmenu")
+local WS       = require("dct.assets.worldstate")
+
+local function is_join_event(fact)
+	return fact.type == WS.Facts.types.EVENT and
+	       fact.event.id == dctenum.event.DCT_EVENT_PLAYER_JOIN
+end
+
+local function any_event(fact)
+	return fact.type == WS.Facts.types.EVENT
+end
+
+local PlayerJoin = require("libs.namedclass")("PlayerJoin", WS.Action)
+function PlayerJoin:__init(agent)
+	WS.Action.__init(self, agent, 1, {
+		-- pre-conditions
+		-- none
+	}, {
+		-- effects
+		WS.Property(WS.ID.REACTEDTOEVENT, true),
+	})
+	self.factkey = nil
+end
+
+function PlayerJoin:checkProceduralPreconditions()
+	local rc, key = self.agent:hasFact(is_join_event)
+
+	if rc then
+		self.factkey = key
+	else
+		self.factkey = nil
+	end
+	return rc
+end
+
+function PlayerJoin:enter()
+	local fact = self.agent:getFact(self.factkey)
+	local grp = Group.getByName(self.agent.name)
+	local unit = Unit.getByName(fact.event.unit)
+	local msn = self.agent:getMission()
+
+	self.factkey = nil
+	self.agent:deleteFacts(is_join_event)
+
+	if not grp or not unit then
+		self.agent:replan()
+		return
+	end
+
+	local groupId = grp:getID()
+	self.agent:setDead(false)
+	self.agent.desc.groupId = groupId
+	self.agent:WS():get(WS.ID.INAIR).value = unit:inAir()
+	if not self.agent:hasFact(any_event) then
+		self.agent:WS():get(WS.ID.REACTEDTOEVENT).value = true
+	end
+	uimenu.createMenu(self.agent)
+
+	if msn then
+		trigger.action.outTextForGroup(groupId, string.format(
+			"Welcome. Mission %d is already assigned to this "..
+			"slot, use the F10 menu to get the briefing or "..
+			"find another.", msn:getID()), 20, false)
+		-- TODO: pull all mission facts and add them to the agent
+	end
+
+	trigger.action.outTextForGroup(groupId, dctutils.notifymsg, 20, false)
+end
+
+return PlayerJoin

src/dct/assets/actions/PlayerKick.lua
@@ -1,0 +1,88 @@
+--- SPDX-License-Identifier: LGPL-3.0
+
+local dctenum  = require("dct.enum")
+local dctutils = require("dct.libs.utils")
+local WS       = require("dct.assets.worldstate")
+
+local reasons = {
+	[dctenum.kickCode.LOADOUT] =
+		"You have been removed to spectator for flying "..
+		"with an invalid loadout. "..dctutils.notifymsg,
+	[dctenum.kickCode.DEAD] =
+		"You have been kicked from the slot because the game"..
+		" thinks you are dead.",
+	[dctenum.kickCode.EMPTY] =
+		"You have been kicked from the slot because the game"..
+		" thought the slot was empty and slot management was"..
+		" resetting the slot state.",
+}
+
+local function is_kick_event(fact)
+	return fact.type == WS.Facts.types.EVENT and
+	       fact.event.id == dctenum.event.DCT_EVENT_PLAYER_KICK
+end
+
+--- @class PlayerKick request player to be kicked from slot
+--
+-- Posts a request for the player to be kicked from the slot.
+-- This depends on an outside DCS hooks script to be running
+-- which will kick the player from the slot and reset the
+-- kick flag.
+-- This will then allow the player state the be reset allowing
+-- another player to join the slot.
+--
+-- Assign static action cost values so the order of execution of actions
+-- is preserved.
+-- PlayerJoin - least expensive
+-- Eject      - in the middle
+-- PlayerKick - most expensive
+local PlayerKick = require("libs.namedclass")("PlayerKick", WS.Action)
+function PlayerKick:__init(agent)
+	WS.Action.__init(self, agent, 100, {
+		-- pre-conditions
+		-- none
+	}, {
+		-- effects
+		WS.Property(WS.ID.REACTEDTOEVENT, true),
+	})
+	self.factkey = nil
+end
+
+function PlayerKick:checkProceduralPreconditions()
+	local rc, key = self.agent:hasFact(is_kick_event)
+
+	if rc then
+		self.factkey = key
+	else
+		self.factkey = nil
+	end
+	return rc
+end
+
+function PlayerKick:enter()
+	local flagname = dctutils.build_kick_flagname(self.agent.name)
+	local msg = reasons[self.fact.event.code] or
+		"You have been kicked from the slot for an unknown reason."
+	local fact = self.agent:getFact(self.factkey)
+
+	trigger.action.outTextForGroup(self.agent.desc.groupId, msg, 20, true)
+	trigger.action.setUserFlag(flagname, self.fact.event.code or
+		dctenum.kickCode.UNKNOWN)
+	self.agent._logger:debug("requesting kick: %s; reason: %d",
+		flagname, self.fact.event.code)
+
+	if self.agent.desc.loseticket then
+		self.agent:setDead(true)
+	end
+
+	-- tell the player sensor the agent has sync'ed its state
+	fact.event.psensor:setSync(true)
+	fact.event.psensor:doEnable()
+
+	-- clear all facts from agent
+	self.factkey = nil
+	self.agent:deleteAllFacts()
+	self.agent:WS():get(WS.ID.REACTEDTOEVENT).value = true
+end
+
+return PlayerKick

src/dct/assets/goals/ReactToEvent.lua
@@ -1,0 +1,27 @@
+--- SPDX-License-Identifier: LGPL-3.0
+
+local WS = require("dct.assets.worldstate")
+
+local function is_event(fact)
+	return fact.type == WS.Facts.types.EVENT
+end
+
+--- @class ReactToEvent
+-- Have an agent react to an event registered in the Agent's memory.
+local ReactToEvent = require("libs.namedclass")("ReactToEvent", WS.Goal)
+function ReactToEvent:__init()
+	WS.Goal.__init(WS.WorldState({
+			WS.Property(WS.ID.REACTEDTOEVENT, true),
+		}), 2)
+end
+
+function ReactToEvent:relevance(agent)
+	local score = 0
+
+	if agent:hasFact(is_event) then
+		score = self.weight
+	end
+	return score
+end
+
+return ReactToEvent

.luacheckrc
@@ -36,7 +36,7 @@ read_globals = {
 }
 
 files["mission/dct-mission-init.lua"] = {
-	globals = {"dctsettings", "luapath",},
+	globals = {"dctsettings", "luapath", "dct",},
 }
 files["src/dct/Region.lua"] = { globals = {"region",} }
 files["src/dct/settings.lua"] = { globals = {"dctserverconfig",} }

mission/dct-mission-init.lua
@@ -37,5 +37,6 @@ do
 		package.path = package.path .. ";" .. modpath .. "\\lua\\?.lua;"
 		require("dct")
 		dct.init()
+		dct.modpath = modpath
 	end
 end

src/dct.lua
@@ -11,6 +11,9 @@ dct.settings  = require("dct.settings")()
 dct.Logger    = require("dct.libs.Logger")
 dct.init      = require("dct.init")
 dct.Theater   = require("dct.Theater")
+if os.getenv("DCT_SRC_ROOT") then
+	dct.modpath = os.getenv("DCT_SRC_ROOT").."/src"
+end
 
 env.info(dct._DESCRIPTION.."; "..dct._COPYRIGHT.."; version: "..
     dct._VERSION)

commit 584ff8d06b7c4020108430d934f5e0f5d472aff0

agent: update test function signature
In various actions and goals the test function was not updated after the
signature in the Agent class was updated.

Fixes: c8866a4e4e9d ("assets: rewrite asset class hierarchy to use composition")

src/dct/assets/actions/Ejection.lua
@@ -2,12 +2,12 @@
 
 local WS = require("dct.assets.worldstate")
 
-local function is_eject_event(fact)
+local function is_eject_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT and
 	       fact.event.id == world.event.S_EVENT_EJECTION
 end
 
-local function any_event(fact)
+local function any_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT
 end
 

src/dct/assets/actions/PlayerAttack.lua
@@ -6,8 +6,8 @@ local WS        = require("dct.assets.worldstate")
 local UPDATE_TIME = 60
 
 local function is_target(fact)
-	return fact.type == WS.Facts.types.CONTACT and
-	       fact.threat and fact.threat.value >= 1
+	return fact.type == WS.Facts.types.CHARACTER and
+	       fact.object and fact.object.confidence >= 1
 end
 
 --- @classmod PlayerAttack

src/dct/assets/actions/PlayerJoin.lua
@@ -4,12 +4,12 @@ local dctenum  = require("dct.enum")
 local dctutils = require("dct.libs.utils")
 local WS       = require("dct.assets.worldstate")
 
-local function is_join_event(fact)
+local function is_join_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT and
 	       fact.event.id == dctenum.event.DCT_EVENT_PLAYER_JOIN
 end
 
-local function any_event(fact)
+local function any_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT
 end
 

src/dct/assets/actions/PlayerKick.lua
@@ -17,7 +17,7 @@ local reasons = {
 		" resetting the slot state.",
 }
 
-local function is_kick_event(fact)
+local function is_kick_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT and
 	       fact.event.id == dctenum.event.DCT_EVENT_PLAYER_KICK
 end

src/dct/assets/goals/ReactToEvent.lua
@@ -2,7 +2,7 @@
 
 local WS = require("dct.assets.worldstate")
 
-local function is_event(fact)
+local function is_event(_ --[[key]], fact)
 	return fact.type == WS.Facts.types.EVENT
 end
 