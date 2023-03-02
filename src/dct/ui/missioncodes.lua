jtoppins/dct

commit 1cf14592ee17bc18cf8f49df7fca58ce79f69132

enum: update assetTypes
The assetTypes originally created did not provide the approperate amount
of abstraction. The types of assets defined should not need to be
extended so a little while.

src/dct/ui/missioncodes.lua
@@ -5,8 +5,8 @@
 -- from the radio menu UI
 --]]
 
-local enum    = require("dct.enum")
 local Theater = require("dct.Theater")
+local Mission = require("dct.libs.Mission")
 local addmenu = missionCommands.addSubMenuForGroup
 local addcmd  = missionCommands.addCommandForGroup
 
@@ -17,8 +17,8 @@ local function addEmptyCommand(gid, path)
 end
 
 local validFirstDigit = {}
-for _, m1 in pairs(enum.squawkMissionType) do
-	validFirstDigit[m1] = true
+for _, msn in pairs(Mission.typeData) do
+	validFirstDigit[msn.codeType] = true
 end
 
 local function createJoinCmds(gid, name, parentMenu, halfCode)

src/dct/data/agents.lua
@@ -2,7 +2,7 @@
 
 local dctenum = require("dct.enum")
 
-local statics = {
+local static = {
 	["sensors"] = {
 		["DCSObjectsSensor"] = 0,
 		["PlanningSensor"]   = 0,
@@ -30,67 +30,67 @@ local airdefense = {
 
 local groundunits = airdefense
 
-local agents = {
-	[dctenum.assetType.AMMODUMP]    = statics,
-	[dctenum.assetType.FUELDUMP]    = statics,
-	[dctenum.assetType.PORT]        = statics,
-	[dctenum.assetType.FACILITY]    = statics,
-	[dctenum.assetType.BUNKER]      = statics,
-	[dctenum.assetType.CHECKPOINT]  = statics,
-	[dctenum.assetType.FACTORY]     = statics,
-	[dctenum.assetType.C2]          = statics,
-	[dctenum.assetType.FOB]         = statics,
-	[dctenum.assetType.BASEDEFENSE] = airdefense,
-	[dctenum.assetType.EWR]         = airdefense,
-	[dctenum.assetType.SAM]         = airdefense,
-	[dctenum.assetType.SHORAD]      = airdefense,
-	[dctenum.assetType.GROUND]      = groundunits,
-	[dctenum.assetType.JTAC]        = groundunits,
-	[dctenum.assetType.AIRBASE]     = {
-		["sensors"] = {
-			["PlanningSensor"]   = 0,
-			["RunwaySensor"]     = 0,
-		},
-		["actions"] = {
-			["RunwayRepair"]     = 1,
-		},
-		["goals"]   = {
-			["Idle"]             = 1,
-			["Heal"]             = 1,
-		},
+local agents = {}
+for _, assettype in pairs(dctenum.assetType) do
+	agents[assettype] = static
+end
+
+agents[dctenum.assetType.BASEDEFENSE] = airdefense
+agents[dctenum.assetType.EWR]         = airdefense
+agents[dctenum.assetType.SAM]         = airdefense
+agents[dctenum.assetType.SHORAD]      = airdefense
+agents[dctenum.assetType.GROUND]      = groundunits
+agents[dctenum.assetType.JTAC]        = groundunits
+agents[dctenum.assetType.AIRBASE]     = {
+	["sensors"] = {
+		["RunwaySensor"]     = 0,
+		["PlanningSensor"]   = 0,
 	},
-	[dctenum.assetType.CV]          = {
-		["sensors"] = {
-			["MissionSensor"]    = 0,
-			["PlanningSensor"]   = 0,
-		},
-		["actions"] = {
-		},
-		["goals"] = {
-			["Idle"]             = 1,
-		},
+	["actions"] = {
+		["RunwayRepair"]     = 1,
 	},
-	[dctenum.assetType.SQUADRON]    = {
-		["sensors"] = {},
-		["actions"] = {},
-		["goals"] = {},
+	["goals"]   = {
+		["Idle"]             = 1,
+		["Heal"]             = 1,
 	},
-	[dctenum.assetType.PLAYER] = {
-		["sensors"] = {
-			["MissionSensor"]   = 0,
-			["PlanningSensor"]  = 0,
-			["PlayerSensor"]    = 0,
-		},
-		["actions"] = {
-			["PlayerJoin"]   = 1,
-			["Ejection"]     = 1,
-			["PlayerKick"]   = 1,
-			["PlayerAttack"] = 50,
-		},
-		["goals"]   = {
-			["Idle"]         = 1,
-			["ReactToEvent"] = 2,
-		},
+}
+agents[dctenum.assetType.CV]          = {
+	["sensors"] = {
+		["DCSObjectsSensor"] = 0,
+		["MissionSensor"]    = 0,
+		["PlanningSensor"]   = 0,
+	},
+	["actions"] = {
+	},
+	["goals"] = {
+		["Idle"]             = 1,
+	},
+}
+agents[dctenum.assetType.SQUADRON]    = {
+	["sensors"] = {
+		["PlanningSensor"]   = 0,
+	},
+	["actions"] = {
+	},
+	["goals"] = {
+		["Idle"]             = 1,
+	},
+}
+agents[dctenum.assetType.PLAYER] = {
+	["sensors"] = {
+		["MissionSensor"]   = 0,
+		["PlanningSensor"]  = 0,
+		["PlayerSensor"]    = 0,
+	},
+	["actions"] = {
+		["PlayerJoin"]   = 1,
+		["Ejection"]     = 1,
+		["PlayerKick"]   = 1,
+		["PlayerAttack"] = 50,
+	},
+	["goals"]   = {
+		["Idle"]         = 1,
+		["ReactToEvent"] = 2,
 	},
 }
 

src/dct/enum.lua
@@ -25,151 +25,149 @@ enum.airbaserecovery = {
 enum.assetType = {
 	["INVALID"]     = 0,
 	-- resource types
-	["AMMODUMP"]    = 1,
-	["FUELDUMP"]    = 2,
-	["PORT"]        = 3,
-	["FACILITY"]    = 4,
-	["BUNKER"]      = 4,
-	["CHECKPOINT"]  = 4,
-	["FACTORY"]     = 4,
-	["C2"]          = 5,
-	["FOB"]         = 6,
+	["RESOURCE"]    = 1,  -- an agent that supplies resources to its
+			      -- commander; resources can be one or all
+			      -- of: ammo, supply
+	["AMMODUMP"]    = 2,
+	["FUELDUMP"]    = 3,
+	["C2"]          = 4,
+	["BUNKER"]      = 5,
+	["CHECKPOINT"]  = 6,
 
 	-- strategic assets
-	["MISSILE"]     = 10,
-	["OCA"]         = 11,
+	["MISSILE"]     = 11,
+	["OCA"]         = 12,
 
-	-- air defense
-	["BASEDEFENSE"] = 20,
-	["EWR"]         = 21,
-	["SAM"]         = 22,
-	["SHORAD"]      = 23,
+	-- Base assets accept character objects from an HQ and spawn
+	-- the characters into the world according to the base's specific
+	-- criteria
+	["ARMYBASE"]    = 21,
+	["AIRBASE"]     = 22,
+	["PORT"]        = 23,
+	["CV"]          = 24,
+	["HELOCARRIER"] = 25,
+	["FARP"]        = 26,
 
-	-- HQ's / bases
-	["AIRBASE"]        = 31,
-	["SQUADRONPLAYER"] = 32,
-	["SQUADRON"]       = 32,
+	-- Headquarters are children of Bases they are also the only
+	-- Agents the commander sends requests to.
+	["SQUADRON"]    = 31,
+	["ARMYGROUP"]   = 32,
+	["FLEET"]       = 33,
 
-	-- tactical land
-	["GROUND"]        = 41,
-	["LOGISTICS"]     = 41,
-	["SPECIALFORCES"] = 41,
-	["JTAC"]          = 42,
-
-	-- tactical sea
-	["SEA"]         = 51,
-	["CV"]          = 52,
-
-	-- tactical air
-	["AIRPLANE"]    = 61,
-	["HELO"]        = 62,
+	-- tactical units are the "game pieces", some can move and some
+	-- just occupy an area defined by their template.
+	["GROUND"]      = 41,
+	["JTAC"]        = 42,
+	["AIRPLANE"]    = 43,
+	["HELO"]        = 44,
+	["BASEDEFENSE"] = 45,
+	["EWR"]         = 46,
+	["SAM"]         = 47,
+	["SHORAD"]      = 48,
+	["INFANTRY"]    = 49,
+	["PILOT"]       = 50,
+	["SHIP"]        = 51,
 
 	-- players
 	["PLAYER"]      = 71,
 
 	-- control primitives
 	["SCRIPT"]      = 101,  -- no agent is associated, it is just a
-				-- template that spawns DCS objects
-	["NODE"]        = 102,
+				-- template that spawns DCS objects, the
+				-- objects are not even tracked or targetable
+	["NODE"]        = 102,  -- navigation points in the DCT object space
+				-- they can also be smartobjects and do things
+				-- like transmit a beacon
 }
 
-enum.missionType = {
-	["CAS"]        = 1,
-	["CAP"]        = 2,
-	["STRIKE"]     = 3,
-	["SEAD"]       = 4,
-	["BAI"]        = 5,
-	["OCA"]        = 6,
-	["ARMEDRECON"] = 7,
+enum.assetTypeDeprecated = {
+	["FACTORY"]        = 1,
+	["FACILITY"]       = 1,
+	["FOB"]            = 21,
+	["SQUADRONPLAYER"] = 31,
+	["LOGISTICS"]      = 41,
+	["SPECIALFORCES"]  = 41,
 }
-
-enum.squawkMissionType = {
-	[enum.missionType.CAP]        = 2,
-	[enum.missionType.SEAD]       = 3,
-	[enum.missionType.CAS]        = 5,
-	[enum.missionType.STRIKE]     = 5,
-	[enum.missionType.BAI]        = 5,
-	[enum.missionType.OCA]        = 5,
-	[enum.missionType.ARMEDRECON] = 5,
-}
-
-enum.squawkMissionSubType = {
-	[enum.missionType.STRIKE]     = 0,
-	[enum.missionType.OCA]        = 0,
-	[enum.missionType.BAI]        = 1,
-	[enum.missionType.ARMEDRECON] = 2,
-	[enum.missionType.CAS]        = 3,
-}
-
-for _, msntype in pairs(enum.missionType) do
-	assert(enum.squawkMissionType[msntype],
-		"not all mission types are mapped to squawk codes")
-end
 
 enum.assetClass = {
 	["INITIALIZE"] = {
+		[enum.assetType.RESOURCE]    = true,
 		[enum.assetType.AMMODUMP]    = true,
 		[enum.assetType.FUELDUMP]    = true,
 		[enum.assetType.C2]          = true,
-		[enum.assetType.EWR]         = true,
+		[enum.assetType.BUNKER]      = true,
+		[enum.assetType.CHECKPOINT]  = true,
 		[enum.assetType.MISSILE]     = true,
 		[enum.assetType.OCA]         = true,
-		[enum.assetType.PORT]        = true,
-		[enum.assetType.SAM]         = true,
-		[enum.assetType.FACILITY]    = true,
-		[enum.assetType.BUNKER]      = true,
-		[enum.assetType.CHECKPOINT]  = true,
-		[enum.assetType.FACTORY]     = true,
-		[enum.assetType.SHORAD]      = true,
+		[enum.assetType.ARMYBASE]    = true,
 		[enum.assetType.AIRBASE]     = true,
-		[enum.assetType.SPECIALFORCES] = true,
-		[enum.assetType.FOB]           = true,
-		[enum.assetType.LOGISTICS]     = true,
-	},
-	-- strategic list is used in calculating ownership of a region
-	-- among other things
-	["STRATEGIC"] = {
-		[enum.assetType.AMMODUMP]    = true,
-		[enum.assetType.FUELDUMP]    = true,
-		[enum.assetType.C2]          = true,
+		[enum.assetType.PORT]        = true,
+		[enum.assetType.CV]          = true,
+		[enum.assetType.HELOCARRIER] = true,
+		[enum.assetType.FARP]        = true,
+		[enum.assetType.GROUND]      = true,
+		[enum.assetType.JTAC]        = true,
+		[enum.assetType.AIRPLANE]    = true,
+		[enum.assetType.HELO]        = true,
 		[enum.assetType.EWR]         = true,
-		[enum.assetType.MISSILE]     = true,
-		[enum.assetType.PORT]        = true,
 		[enum.assetType.SAM]         = true,
-		[enum.assetType.FACILITY]    = true,
-		[enum.assetType.BUNKER]      = true,
-		[enum.assetType.CHECKPOINT]  = true,
-		[enum.assetType.FACTORY]     = true,
+		[enum.assetType.SHORAD]      = true,
+		[enum.assetType.SHIP]        = true,
+	},
+	["TACTICAL"] = {
+		[enum.assetType.MISSILE]     = true,
+		[enum.assetType.GROUND]      = true,
+		[enum.assetType.JTAC]        = true,
+		[enum.assetType.AIRPLANE]    = true,
+		[enum.assetType.HELO]        = true,
+		[enum.assetType.BASEDEFENSE] = true,
+		[enum.assetType.EWR]         = true,
+		[enum.assetType.SAM]         = true,
+		[enum.assetType.SHORAD]      = true,
+		[enum.assetType.INFANTRY]    = true,
+		[enum.assetType.SHIP]        = true,
+	},
+	["HQ"] = {
+		[enum.assetType.SQUADRON]    = true,
+		[enum.assetType.ARMYGROUP]   = true,
+		[enum.assetType.FLEET]       = true,
+	},
+	["SPAWNER"] = {
+		[enum.assetType.ARMYBASE]    = true,
 		[enum.assetType.AIRBASE]     = true,
-		[enum.assetType.FOB]         = true,
-	},
-	["AGENTS"] = {
-	},
-	["HEADQUARTERS"] = {
-		--[enum.assetType.SQUADRON] = true,
+		[enum.assetType.PORT]        = true,
+		[enum.assetType.CV]          = true,
+		[enum.assetType.HELOCARRIER] = true,
+		[enum.assetType.FARP]        = true,
 	},
 }
 
-enum.missionTypeMap = {
-	[enum.assetType.AMMODUMP]	= enum.missionType.STRIKE,
-	[enum.assetType.FUELDUMP]	= enum.missionType.STRIKE,
-	[enum.assetType.C2]		= enum.missionType.STRIKE,
-	[enum.assetType.MISSILE]	= enum.missionType.STRIKE,
-	[enum.assetType.PORT]		= enum.missionType.STRIKE,
-	[enum.assetType.FACILITY]	= enum.missionType.STRIKE,
-	[enum.assetType.BUNKER]		= enum.missionType.STRIKE,
-	[enum.assetType.CHECKPOINT]	= enum.missionType.STRIKE,
-	[enum.assetType.FACTORY]	= enum.missionType.STRIKE,
-	[enum.assetType.EWR]		= enum.missionType.SEAD,
-	[enum.assetType.SAM]		= enum.missionType.SEAD,
-	[enum.assetType.SHORAD]		= enum.missionType.SEAD,
-	[enum.assetType.OCA]		= enum.missionType.OCA,
-	[enum.assetType.AIRBASE]	= enum.missionType.OCA,
-	[enum.assetType.LOGISTICS]	= enum.missionType.BAI,
-	[enum.assetType.JTAC]		= enum.missionType.CAS,
-	[enum.assetType.SPECIALFORCES]	= enum.missionType.ARMEDRECON,
-	[enum.assetType.FOB]		= enum.missionType.ARMEDRECON,
-	[enum.assetType.SEA]		= enum.missionType.SEA,
+enum.missionType = {
+	["INVALID"]    = 0,
+	["MOVETO"]     = 11,
+
+	["GUARD"]      = 20,
+	["CAS"]        = 21,
+	["CAP"]        = 22,
+	["SEAD"]       = 23,
+	["TANKER"]     = 24,
+
+	["ATTACK"]     = 30,
+	["STRIKE"]     = 31,
+	["BAI"]        = 32,
+	["OCA"]        = 33,
+	["ANTISHIP"]   = 34,
+	["DEAD"]       = 35,
+	["ARMEDRECON"] = 36,
+
+	["SEARCH"]     = 40,
+	["RECON"]      = 41,
+	["INTERCEPT"]  = 42,
+	["ESCORT"]     = 43,
+
+	["TRANSPORT"]  = 50,
+	["CSAR"]       = 51,
+	["RESUPPLY"]   = 52,
 }
 
 enum.missionResult = {

src/dct/libs/Mission.lua
@@ -51,7 +51,129 @@ function Mission:__init(cmdr, missiontype, goalq, timeout)
 		g:addObserver(self.onDCTEvent, self, string.format(
 			"Misison(%s).onDCTEvent", self.__clsname))
 	end
+
+	self.typeData = nil
 end
+
+Mission.typeData = {
+	[dctenum.missionType.CAS] = {
+		["name"]        = "Close Air Support",
+		["short"]       = "CAS",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.GUARD,
+		["codeType"]    = 5,
+		["codeSubType"] = 3,
+	},
+	[dctenum.missionType.CAP] = {
+		["name"]        = "Combat Air Patrol",
+		["short"]       = "CAP",
+		["symbol"]      = "A",
+		["mType"]       = dctenum.missionType.GUARD,
+		["codeType"]    = 2,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.SEAD] = {
+		["name"]        = "Suppression of Enemy Air Defense",
+		["short"]       = "SEAD",
+		["symbol"]      = "A",
+		["mType"]       = dctenum.missionType.GUARD,
+		["codeType"]    = 3,
+	},
+	[dctenum.missionType.TANKER] = {
+		["name"]        = "Refueling Tanker",
+		["short"]       = "TKR",
+		["symbol"]      = "C",
+		["mType"]       = dctenum.missionType.GUARD,
+		["codeType"]    = 0,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.STRIKE] = {
+		["name"]        = "Precision Strike",
+		["short"]       = "STRIKE",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ATTACK,
+		["codeType"]    = 5,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.BAI] = {
+		["name"]        = "Interdiction",
+		["short"]       = "IA",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ATTACK,
+		["codeType"]    = 5,
+		["codeSubType"] = 1,
+	},
+	[dctenum.missionType.OCA] = {
+		["name"]        = "Offensive Counter Air",
+		["short"]       = "OCA",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ATTACK,
+		["codeType"]    = 5,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.ANTISHIP] = {
+		["name"]        = "Anti-Shipping",
+		["short"]       = "SHP",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ATTACK,
+		["codeType"]    = 5,
+	},
+	[dctenum.missionType.DEAD] = {
+		["name"]        = "Destruction of Enemy Air Defense",
+		["short"]       = "DEAD",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ATTACK,
+		["codeType"]    = 5,
+	},
+	[dctenum.missionType.TRANSPORT] = {
+		["name"]        = "Air Transport",
+		["short"]       = "TRANS",
+		["symbol"]      = "C",
+		["mType"]       = dctenum.missionType.TRANSPORT,
+		["codeType"]    = 0,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.CSAR] = {
+		["name"]        = "Combat Search and Rescue",
+		["short"]       = "CSAR",
+		["symbol"]      = "A",
+		["mType"]       = dctenum.missionType.TRANSPORT,
+		["codeType"]    = 5,
+		["codeSubType"] = 2,
+	},
+	[dctenum.missionType.RESUPPLY] = {
+		["name"]        = "Resupply",
+		["short"]       = "RES",
+		["symbol"]      = "C",
+		["mType"]       = dctenum.missionType.TRANSPORT,
+		["codeType"]    = 0,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.RECON] = {
+		["name"]        = "Recon",
+		["short"]       = "RCN",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.INVESTIGATE,
+		["codeType"]    = 0,
+		["codeSubType"] = 0,
+	},
+	[dctenum.missionType.INTERCEPT] = {
+		["name"]        = "Air Intercept",
+		["short"]       = "ITC",
+		["symbol"]      = "A",
+		["mType"]       = dctenum.missionType.INVESTIGATE,
+		["codeType"]    = 2,
+		["codeSubType"] = 1,
+	},
+	[dctenum.missionType.ESCORT] = {
+		["name"]        = "Escort",
+		["short"]       = "ESC",
+		["symbol"]      = "B",
+		["mType"]       = dctenum.missionType.ESCORT,
+		["codeType"]    = 5,
+		["codeSubType"] = 0,
+	},
+}
 
 --- remove any references or return any targets that may have been
 -- assigned back to the commander before the Misison object is destroyed.

src/dct/settings/theater.lua
@@ -56,7 +56,13 @@ local function validate_codenamedb(cfgdata, tbl)
 			string.format("invalid codename category '%s'; file: %s",
 			key, cfgdata.file))
 
-		local k = enum.assetType[string.upper(key)]
+		local upper = string.upper(key)
+		local k = enum.assetType[upper]
+
+		if k == nil then
+			k = enum.assetTypeDeprecated[upper]
+		end
+
 		if k ~= nil then
 			newkey = k
 		elseif key == "default" then

src/dct/templates/Template.lua
@@ -12,9 +12,9 @@ local Agent    = require("dct.assets.Agent")
 local Logger   = dct.Logger.getByName("Template")
 
 local norenametype = {
-	[dctenum.assetType.SQUADRONPLAYER] = true,
-	[dctenum.assetType.PLAYER]         = true,
-	[dctenum.assetType.AIRBASE]        = true,
+	[dctenum.assetType.SQUADRON]  = true,
+	[dctenum.assetType.PLAYER]    = true,
+	[dctenum.assetType.AIRBASE]   = true,
 }
 
 -- TODO: setup a respawn attribute

src/dct/templates/checkers/CheckCommon.lua
@@ -1,8 +1,12 @@
 --- SPDX-License-Identifier: LGPL-3.0
 
 local class   = require("libs.namedclass")
+local utils   = require("libs.utils")
 local dctenum = require("dct.enum")
 local Check   = require("dct.templates.checkers.Check")
+
+local assettypes = utils.mergetables({}, dctenum.assetTypeDeprecated)
+assettypes = utils.mergetables(assettypes, dctenum.assetType)
 
 local CheckCommon = class("CheckCommon", Check)
 function CheckCommon:__init()
@@ -10,7 +14,7 @@ function CheckCommon:__init()
 		["objtype"] = {
 			["agent"] = true,
 			["type"] = Check.valuetype.TABLEKEYS,
-			["values"] = dctenum.assetType,
+			["values"] = assettypes,
 			["description"] = [[
 Defines the type of game object (Asset) that will be created from the
 template. Allowed values can be found in `assetType` table.]],
@@ -100,6 +104,12 @@ template. Allowed values can be found in `assetType` table.]],
 end
 
 function CheckCommon:check(data)
+	if dctenum.assetTypeDeprecated[string.upper(data.objtype)] ~= nil then
+		dct.Logger.getByName("Template"):warn(
+			"%s: is a deprecated objtype; file %s",
+			tostring(data.objtype), tostring(data.filedct))
+	end
+
 	local ok, key, msg = Check.check(self, data)
 
 	if not ok then

src/dct/templates/checkers/CheckSquadron.lua
@@ -5,7 +5,7 @@ local dctenum = require("dct.enum")
 local Check   = require("dct.templates.checkers.Check")
 
 local issquadron = {
-	[dctenum.assetType.SQUADRONPLAYER] = true,
+	[dctenum.assetType.SQUADRON] = true,
 }
 
 local function check_ato(data)

commit 8fe03898ee1872d0ffd69ae2f4f8378cc7c8a0b4

missioncodes: fix mission enum reference
Fixes: 1cf14592ee17 ("enum: update assetTypes")

src/dct/ui/missioncodes.lua
@@ -5,6 +5,7 @@
 -- from the radio menu UI
 --]]
 
+local dctenum = require("dct.enum")
 local Theater = require("dct.Theater")
 local Mission = require("dct.libs.Mission")
 local addmenu = missionCommands.addSubMenuForGroup
@@ -28,7 +29,7 @@ local function createJoinCmds(gid, name, parentMenu, halfCode)
 			addcmd(gid, string.format("Mission %s", missionCode), parentMenu,
 				Theater.playerRequest, {
 					["name"]  = name,
-					["type"]  = enum.uiRequestType.MISSIONJOIN,
+					["type"]  = dctenum.uiRequestType.MISSIONJOIN,
 					["value"] = missionCode,
 				}
 			)