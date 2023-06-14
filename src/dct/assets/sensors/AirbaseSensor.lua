jtoppins/dct

commit 9dd2d1d7b700ab01c3152492475d7e531e406d72

airbase: add navigation aid support
Allow designers to define what TACAN and ICLS channels are used for a
given carrier. Then add support to periodically refresh this
configuration on the carrier. Further when the carrier is not in a state
where it is conducting flight ops turn the navaids off along with the
tower.

src/dct/assets/sensors/AirbaseSensor.lua
@@ -18,6 +18,7 @@ function AirbaseSensor:__init(agent)
 	WS.Sensor.__init(self, agent, 50)
 	DCTEvents.__init(self)
 	self.timer = Timer(UPDATE_TIME)
+	self.navaid_counter = 0
 
 	local handlers = {}
 
@@ -42,6 +43,65 @@ AirbaseSensor.linkmap = {
 	[dctenum.assetType.FARP]    = "helipadId",
 }
 
+function AirbaseSensor:activateNavaids(ab)
+	if ab == nil or Object.getCategory(ab) ~= Object.Category.UNIT then
+		return
+	end
+
+	local tasklist = {}
+	local atc = self.agent:getDescKey("atc")
+	local tacan = self.agent:getDescKey("tacan")
+	local icls = self.agent:getDescKey("icls")
+
+	if atc then
+		table.insert(tasklist, aitasks.wraptask(
+			aitasks.command.setFrequency(atc.frequency,
+						     atc.modulation)))
+	end
+
+	if tacan then
+		table.insert(tasklist, aitasks.wraptask(
+			aitasks.command.createTACAN(ab,
+						    tacan.callsign,
+						    tacan.channel,
+						    tacan.mode, nil,
+						    false, true, false)))
+	end
+
+	if icls then
+		table.insert(tasklist, aitasks.wraptask(
+			aitasks.command.activateICLS(ab, icls)))
+	end
+
+	aitasks.execute(Unit.getController(ab), tasklist)
+end
+
+function AirbaseSensor:deactivateNavaids(ab)
+	if ab == nil or Object.getCategory(ab) ~= Object.Category.UNIT then
+		return
+	end
+
+	aitasks.execute(Unit.getController(ab), {
+		aitasks.command.deactivateBeacon()
+	})
+end
+
+function AirbaseSensor:refreshNavaids()
+	local ab = Airbase.getByName(self.agent.name)
+
+	if ab == nil or Object.getCategory(ab) ~= Object.Category.UNIT then
+		return
+	end
+
+	self.navaid_counter = self.navaid_counter + 1
+	if (self.navaid_counter % NAVAID_REFRESH) ~= 0 then
+		return
+	end
+
+	self.navaid_counter = 0
+	self:activateNavaids(ab)
+end
+
 --- An airbase is defined to be operational if its health state is
 -- operational and it is spawned.
 function AirbaseSensor:isOperational()
@@ -58,6 +118,12 @@ function AirbaseSensor:setSilent(silent)
 
 	if ab then
 		ab:setRadioSilentMode(silent)
+
+		if silent then
+			self:deactivateNavaids(ab)
+		else
+			self:activateNavaids(ab)
+		end
 	end
 end
 
@@ -98,6 +164,21 @@ function AirbaseSensor:handleCapture(event)
 	self.agent._logger:debug("captured - setting dead")
 	self:setSilent(true)
 	self.agent:setHealth(WS.Health.DEAD)
+end
+
+function AirbaseSensor:update()
+	self.timer:update()
+	if not self.timer:expired() then
+		return false
+	end
+
+	if self.isOperational() then
+		self:refreshNavaids()
+	end
+
+	self.timer:reset()
+	self.timer:start()
+	return false
 end
 
 return AirbaseSensor

src/dct/ai/tacan.lua
@@ -1,0 +1,82 @@
+-- SPDX-License-Identifier: LGPL-3.0
+
+--- Validates and decodes TACAN channels into radio frequencies
+
+local check = require("libs.check")
+local aienum = require("dct.ai.enum")
+local Tacan = {}
+
+Tacan.MHZ = 1000 * 1000
+Tacan.GND_BASE_X = 961
+Tacan.GND_BASE_Y = 1087
+Tacan.GND_BASE_INV = 64
+Tacan.MIN_CHANNEL = 1
+Tacan.MAX_CHANNEL = 126
+
+function Tacan.getChannelNumber(channel)
+	return tonumber(string.match(channel, "^(%d+)%a"))
+end
+
+function Tacan.getChannelMode(channel)
+	return string.upper(string.match(channel, "^%d+(%a)"))
+end
+
+function Tacan.getCallsign(channel)
+	return string.match(channel, "^%d+%a%s+(%w.+)$")
+end
+
+function Tacan.isValidChannel(channel)
+	local number = Tacan.getChannelNumber(channel)
+	local mode = Tacan.getChannelMode(channel)
+	local valid = aienum.BEACON.TACANMODE[mode] ~= nil and
+		      number >= Tacan.MIN_CHANNEL and
+		      number <= Tacan.MAX_CHANNEL
+
+	if valid then
+		return true, number, mode
+	else
+		return false
+	end
+end
+
+function Tacan.getFrequencyBase(number, mode)
+	if number < Tacan.GND_BASE_INV then
+		if mode == aienum.BEACON.TACANMODE.X then
+			return Tacan.GND_BASE_X
+		elseif mode == aienum.BEACON.TACANMODE.Y then
+			return Tacan.GND_BASE_Y
+		end
+	else
+		-- starting from channel 64, X and Y bases are swapped
+		if mode == aienum.BEACON.TACANMODE.X then
+			return Tacan.GND_BASE_Y
+		elseif mode == aienum.BEACON.TACANMODE.Y then
+			return Tacan.GND_BASE_X
+		end
+	end
+end
+
+function Tacan.getFrequency(chan, mode)
+	check.range(chan, 1, 126)
+	check.tblkey(mode, aienum.BEACON.TACANMODE, "BEACON.TACANMODE")
+
+	local base = Tacan.getFrequencyBase(chan, mode)
+
+	return (chan + base) * Tacan.MHZ
+end
+
+function Tacan.decodeChannel(channel)
+	local valid, number, mode = Tacan.isValidChannel(channel)
+
+	if not valid then
+		return nil
+	end
+
+	return {
+		callsign = Tacan.getCallsign(channel),
+		channel = number,
+		mode = mode,
+	}
+end
+
+return Tacan

src/dct/ai/tasks.lua
@@ -104,8 +104,8 @@ end
 -- Commands
 
 tasks.command = {}
-function tasks.command.activateBeacon(freq, bcntype, system, callsign,
-				      name, extratbl)
+function tasks.command.activateBeacon(unit, freq, bcntype, system,
+				      callsign, name, extratbl)
 	assert(type(extratbl) == "table" or extratbl == nil,
 		"value error: extratbl must be a table or nil.")
 	extratbl = extratbl or {}
@@ -117,6 +117,10 @@ function tasks.command.activateBeacon(freq, bcntype, system, callsign,
 		["callsign"] = check.string(callsign),
 		["frequency"] = check.number(freq),
 	}
+
+	if unit then
+		params.unitId = unit:getID()
+	end
 
 	if name then
 		params.name = check.string(name)
@@ -160,11 +164,11 @@ function tasks.command.activateLink4(unit, freq, name)
 		aienum.TASKTYPE.COMMAND
 end
 
-function tasks.command.createTACAN(callsign, channel, mode,
+function tasks.command.createTACAN(unit, callsign, channel, mode,
 				   name, aa, bearing, mobile)
 	local bcntype = aienum.BEACON.TYPE.TACAN
 	local system = aienum.BEACON.SYSTEM.TACAN
-	local freq = require("dct.libs.utils").calcTACANFreq(channel, mode)
+	local freq = require("dct.ai.tacan").getFrequency(channel, mode)
 	local extra = {}
 
 	extra.channel = channel
@@ -193,8 +197,8 @@ function tasks.command.createTACAN(callsign, channel, mode,
 		end
 	end
 
-	return tasks.command.activateBeacon(freq, bcntype, system, callsign,
-		name, extra)
+	return tasks.command.activateBeacon(unit, freq, bcntype, system,
+		callsign, name, extra)
 end
 
 function tasks.command.eplrs(enable)

src/dct/assets/worldstate.lua
@@ -41,10 +41,12 @@ local healthType = {
 --- Stance types the agent can have
 local stanceType = {
 	["DEFAULT"]   = "default",    -- whatever the setting were at spawn
+	["MOVING"]    = "moving",     -- agent is relocating
 	["FLEEING"]   = "fleeing",    -- running away, weapons hold
 	["GUARDING"]  = "guarding",   -- combat ready, weapons engage per tasking
 	["SEARCHING"] = "searching",  -- not combat ready, sensors on
 	["ATTACKING"] = "attacking",  -- fangs out
+	["LAUNCHING"] = "launching",  -- agent is launching aircraft
 }
 
 --- Fact types

src/dct/libs/utils.lua
@@ -323,29 +323,6 @@ utils.notifymsg =
 	"Please read the loadout limits in the briefing and "..
 	"use the F10 Menu to validate your loadout before departing."
 
-function utils.calcTACANFreq(chan, mode)
-	local aienum = require("dct.ai.enum")
-	check.range(chan, 1, 126)
-	check.tblkey(mode, aienum.BEACON.TACANMODE, "BEACON.TACANMODE")
-	local A = 1151
-	local B = 64
-
-	if chan < 64 then
-		B = 1
-	end
-	if mode == aienum.BEACON.TACANMODE.Y then
-		A = 1025
-		if chan < 64 then
-			A = 1088
-		end
-	else
-		if chan < 64 then
-			A = 962
-		end
-	end
-	return (A + chan - B) * 1000000
-end
-
 utils.buildevent = {}
 --- DEAD definition:
 --   id = id of this event

src/dct/templates/checkers/CheckAirbase.lua
@@ -4,6 +4,7 @@ local class    = require("libs.namedclass")
 local utils    = require("libs.utils")
 local dctenum  = require("dct.enum")
 local dctutils = require("dct.libs.utils")
+local TACAN    = require("dct.ai.tacan")
 local Check    = require("dct.templates.checkers.Check")
 
 local takeoffvalues = {
@@ -49,6 +50,22 @@ be despawned after 5 minutes of the land event firing]],
 	},
 }
 
+local airbases = {
+	[dctenum.assetType.AIRBASE] = true,
+	[dctenum.assetType.FARP]    = true,
+	[dctenum.assetType.CV]      = true,
+	[dctenum.assetType.HELOCARRIER] = true,
+}
+
+local isship = {
+	[dctenum.assetType.CV] = true,
+	[dctenum.assetType.HELOCARRIER] = true,
+}
+
+local iscarrier = {
+	[dctenum.assetType.CV] = true,
+}
+
 local CheckAirbase = class("CheckAirbase", Check)
 function CheckAirbase:__init()
 	Check.__init(self, "Airbase", {
@@ -79,28 +96,86 @@ the field. The possible options are:
 Ground spots, if defined, will only be used for helicopters and only if
 recovery is land or taxi.]],
 		},
+		["tacan"] = {
+			["default"] = "",
+			["type"]    = Check.valuetype.STRING,
+			["description"] = [[
+For carriers. Allows the designer to specify the TACAN configuration for
+the carrier. This is typically applied to NATO carriers. The format of the
+definition is as follows: `<channel><mode> <callsign>`, for example
+`31X CVN`, where `31` is the channel, `X` is the TACAN mode, and `CVN` is
+the callsign.]],
+		},
+		["icls"] = {
+			["default"] = 0,
+			["type"]    = Check.valuetype.RANGE,
+			["values"]  = {1, 20},
+			["description"] = [[
+For carriers. Allows the designer to specify the ICLS channel for the
+carrier.]],
+		},
+		["atc"] = {
+			["default"] = "",
+			["type"]    = Check.valuetype.STRING,
+			["values"]  = {100, 300},
+			["description"] = [[
+For carriers. Define the frequency on which the carrier's tower operations
+will respond. Only one frequency is allowed and has the following format:
+`<number>[(AM|FM)]`, example `127.5AM`. Where `127.5` is the frequency in
+megahertz and `AM` is the modulation. If not defined what was defined
+in the unit definition stm or miz will be used.]],
+		},
 	})
 end
 
-local airbases = {
-	[dctenum.assetType.AIRBASE] = true,
-	[dctenum.assetType.CV]      = true,
-	[dctenum.assetType.FARP]    = true,
-}
-
-function CheckAirbase:check(data)
-	if airbases[data.objtype] == nil then
+function CheckAirbase:checkCarrier(data)
+	if isship[data.objtype] == nil then
+		data.tacan = nil
+		data.icls = nil
+		data.atc = nil
 		return true
 	end
 
-	data.rename = false
+	if data.atc ~= "" then
+		local freq = tonumber(string.match(data.atc,
+						   "^(%d+[.]?%d*)%a"))
+		local mod = string.upper(string.match(data.atc,
+						      "^%d+[.]?%d*(%a)"))
+		if freq == nil or mod == nil then
+			return false, "atc", "invalid frequency or modulation"
+		end
 
-	local ok, key, msg = Check.check(self, data)
-	if not ok then
-		return ok, key, msg
+		mod = radio.modulation[mod]
+		if mod == nil then
+			return false, "atc", "invalid modulation"
+		end
+
+		data.atc = {
+			["frequency"] = freq * 1000 * 1000,
+			["modulation"] = mod,
+		}
+	else
+		data.atc = nil
 	end
 
+	if data.tacan ~= "" then
+		data.tacan = TACAN.decodeChannel(data.tacan)
+		if data.tacan == nil then
+			return false, "tacan", "invalid channel or mode"
+		end
+	else
+		data.tacan = nil
+	end
+
+	if iscarrier[data.objtype] == nil or data.icls == 0 then
+		data.icls = nil
+	end
+	return true
+end
+
+function CheckAirbase:checkAirbase(data)
 	local ab = Airbase.getByName(data.name)
+
 	if ab == nil then
 		if data.objtype == dctenum.assetType.AIRBASE then
 			return false, "location", string.format(
@@ -111,17 +186,39 @@ function CheckAirbase:check(data)
 				" data defined", data.name)
 		end
 	else
-		if data.objtype ~= dctenum.assetType.AIRBASE and
+		local abcategory = ab:getDesc().category
+
+		if abcategory == Airbase.Category.SHIP and
 		   data.tpldata == nil then
 			local miz_groups = dctutils.get_miz_groups()
 			local U = Unit.getByName(data.name)
 			local G = U:getGroup()
 			local gdata = miz_groups[G:getName()]
 
-			data.tpldata = utils.deepcopy(gdata.data)
+			data.tpldata = utils.deepcopy(gdata)
 			data.overwrite = false
 		end
 		data.location = ab:getPoint()
+		utils.mergetables(data.attributes, ab:getDesc().attributes)
+	end
+
+	return true
+end
+
+function CheckAirbase:check(data)
+	if airbases[data.objtype] == nil then
+		return true
+	end
+
+	data.rename = false
+
+	for _, check in ipairs({ Check.check,
+				 self.checkAirbase,
+				 self.checkCarrier, }) do
+		local ok, key, msg = check(self, data)
+		if not ok then
+			return ok, key, msg
+		end
 	end
 
 	return true

tests/test-tacan.lua
@@ -1,0 +1,35 @@
+#!/usr/bin/lua
+
+require("os")
+local Tacan = require("dct.ai.tacan")
+
+local function assert_equal(a, b)
+	assert(a == b, string.format(
+		"assertion failed! values not equal:\na: %s\nb: %s",
+		tostring(a), tostring(b)))
+end
+
+local function main()
+	local tacan
+
+	assert_equal(Tacan.getChannelNumber("59X"), 59)
+	assert_equal(Tacan.getChannelMode("59X"), "X")
+	assert_equal(Tacan.isValidChannel("126Y"), true)
+	assert_equal(Tacan.isValidChannel("126Y TKR"), true)
+	assert_equal(Tacan.isValidChannel("128X"), false)
+	assert_equal(Tacan.isValidChannel("35A"), false)
+	assert_equal(Tacan.decodeChannel("35A"), nil)
+	assert_equal(Tacan.decodeChannel("59X QJ").channel, 59)
+	assert_equal(Tacan.decodeChannel("59X QJ").mode, "X")
+	tacan = Tacan.decodeChannel("59X QJ")
+	assert_equal(Tacan.getFrequency(tacan.channel, tacan.mode), 1020000000)
+	assert_equal(Tacan.decodeChannel("59X QJ").callsign, "QJ")
+	tacan = Tacan.decodeChannel("73X GW")
+	assert_equal(Tacan.getFrequency(tacan.channel, tacan.mode), 1160000000)
+	tacan = Tacan.decodeChannel("16Y")
+	assert_equal(Tacan.getFrequency(tacan.channel, tacan.mode), 1103000000)
+	assert_equal(Tacan.decodeChannel("16Y").callsign, nil)
+	return 0
+end
+
+os.exit(main())

tests/test-tasks.lua
@@ -8,7 +8,7 @@ local aienum  = require("dct.ai.enum")
 local aitasks = require("dct.ai.tasks")
 
 local function main()
-	local task = aitasks.command.createTACAN("TST", 74,
+	local task = aitasks.command.createTACAN(nil, "TST", 74,
 		aienum.BEACON.TACANMODE.X, "test", false, false, true)
 	assert(type(task) == "table")
 

commit cfbf8f9e59e6cb316765bf9a766a9cc512e67bb0

airbase: fix method call
Fixes: 9dd2d1d7b700 ("airbase: add navigation aid support")
Fixes: d3eb2dab3138 ("airbase: add departure action")

src/dct/assets/sensors/AirbaseSensor.lua
@@ -221,7 +221,7 @@ function AirbaseSensor:handleCapture(event)
 end
 
 function AirbaseSensor:handleDeparture(event)
-	if not self.isOperational() then
+	if not self:isOperational() then
 		return
 	end
 
@@ -236,7 +236,7 @@ function AirbaseSensor:update()
 		return false
 	end
 
-	if self.isOperational() then
+	if self:isOperational() then
 		self:setValues()
 		self:refreshNavaids()
 	end