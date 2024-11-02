d84f91e3efd7ca60a44270645f537b0a23915d37
Jonathan Toppins <jtoppins@users.noreply.github.com>
2024-11-02 01:16:42 -0400

    theater settings wip

--- src/dct/settings/data/player.lua
+++ src/dct/settings/data/player.lua
@@ -1,91 +1,109 @@
 -- SPDX-License-Identifier: LGPL-3.0
 
 --- Default aircraft-specific settings.
+-- @module dct.settings.theater.ui
+
+local uihuman  = require("dct.ui.human")
+
+local player_ui_default = {
+	["gridfmt"]     = uihuman.posfmt.DMS,
+	["distfmt"]     = uihuman.distancefmt.NAUTICALMILE,
+	["altfmt"]      = uihuman.altfmt.FEET,
+	["speedfmt"]    = uihuman.speedfmt.KNOTS,
+	["pressurefmt"] = uihuman.pressurefmt.INHG,
+	["tempfmt"]     = uihuman.tempfmt.F,
+}
 
 local function UNITS_IMPERIAL_MBAR_DDM()
-	return {
-		["gridfmt"] = "ddm",
-		["pressurefmt"] = "mbar",
-	}
+	return setmetatable({
+		["gridfmt"] = uihuman.posfmt.DDM,
+		["pressurefmt"] = uihuman.pressurefmt.MBAR,
+	}, {
+		__index = player_ui_default,
+	})
 end
 
 local function UNITS_IMPERIAL_MGRS()
-	return {
-		["gridfmt"] = "mgrs",
-	}
+	return setmetatable({
+		["gridfmt"] = uihuman.posfmt.MGRS,
+	}, {
+		__index = player_ui_default,
+	})
 end
 
 local function UNITS_METRIC()
-	return {
-		["distfmt"]  = "kilometer",
-		["altfmt"]   = "meter",
-		["speedfmt"] = "kph",
-		["tempfmt"]  = "C",
-	}
+	return setmetatable({
+		["distfmt"]  = uihuman.distancefmt.KILOMETER,
+		["altfmt"]   = uihuman.altfmt.METER,
+		["speedfmt"] = uihuman.speed.KPH,
+		["tempfmt"]  = uihuman.tempfmt.C,
+	}, {
+		__index = player_ui_default,
+	})
 end
 
 local function UNITS_METRIC_HPA()
 	local u = UNITS_METRIC()
-	u.pressurefmt = "hpa"
+	u.pressurefmt = uihuman.pressurefmt.HPA
 	return u
 end
 
 local function UNITS_METRIC_INHG()
 	local u = UNITS_METRIC()
-	u.pressurefmt = "inhg"
+	u.pressurefmt = uihuman.pressurefmt.INHG
 	return u
 end
 
 local function UNITS_METRIC_MMHG()
 	local u = UNITS_METRIC()
-	u.pressurefmt = "mmhg"
+	u.pressurefmt = uihuman.pressurefmt.MMHG
 	return u
 end
 
 local function UNITS_METRIC_MMHG_DDM()
 	local u = UNITS_METRIC()
-	u.pressurefmt = "mmhg"
-	u.gridfmt     = "ddm"
+	u.pressurefmt = uihuman.pressurefmt.MMHG
+	u.gridfmt     = uihuman.posfmt.DDM
 	return u
 end
 
-local playeruicfg = {
+local _ui = {
 	["A-10A"]         = UNITS_IMPERIAL_MGRS(),
 	["A-10C"]         = UNITS_IMPERIAL_MGRS(),
 	["A-10C_2"]       = UNITS_IMPERIAL_MGRS(),
 	["AH-64D_BLK_II"] = {
-		gridfmt = "mgrs",
-		distfmt = "kilometer",
+		gridfmt = uihuman.posfmt.MGRS,
+		distfmt = uihuman.distancefmt.KILOMETER,
 	},
 	["AJS37"]         = UNITS_METRIC_HPA(),
 	["Bf-109K-4"]     = UNITS_METRIC_HPA(),
 	["F-5E-3"]        = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["F-14A-95-GR"]   = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["F-14A-135-GR"]  = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["F-14B"]         = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["F-15ESE"]       = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["F-16C_50"]      = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["FA-18C_hornet"] = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["FW-190A8"]      = UNITS_METRIC_HPA(),
 	["FW-190D9"]      = UNITS_METRIC_HPA(),
 	["I-16"]          = UNITS_METRIC_MMHG(),
 	["Ka-50"]         = UNITS_METRIC_MMHG_DDM(),
 	["Ka-50_3"]       = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["M-2000C"]       = UNITS_IMPERIAL_MBAR_DDM(),
 	["Mi-8MT"]        = UNITS_METRIC_MMHG_DDM(),
@@ -101,10 +119,10 @@ local playeruicfg = {
 	["MiG-29S"]       = UNITS_METRIC_MMHG(),
 	["MiG-29G"]       = UNITS_METRIC_INHG(),
 	["SA342M"]        = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["SA342L"]        = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 	["Su-25"]         = UNITS_METRIC_MMHG(),
 	["Su-25T"]        = UNITS_METRIC_MMHG(),
@@ -112,8 +130,13 @@ local playeruicfg = {
 	["Su-33"]         = UNITS_METRIC_HPA(),
 	["Yak-52"]        = UNITS_METRIC_MMHG(),
 	["UH-1H"]         = {
-		gridfmt = "ddm",
+		gridfmt = uihuman.posfmt.DDM,
 	},
 }
+setmetatable(_ui, {
+	__index = function(--[[tbl, key]])
+		return player_ui_default
+	end
+})
 
-return playeruicfg
+return _ui
--- src/dct/settings/theater.lua
+++ src/dct/settings/theater.lua
@@ -1,8 +1,47 @@
---[[
 -- SPDX-License-Identifier: LGPL-3.0
+
+-- TODO: move theater settings to the theater class and its component
+-- system.
 --
--- Provides config facilities.
---]]
+-- Problem:
+-- Provide a way to enable/disable systems registered with a given theater
+-- instance. The idea is that different mission designers will want to use
+-- different features of DCT.
+--
+-- Examples of optional features:
+-- * Restricted Weapons / Point Buy System
+-- * Blast Effects Enhancements
+-- * Ticket System - the theater might implement a custom way to terminate
+--                   the theater.
+--
+-- Solution:
+-- A simple option is to declare if the system should be used by marking
+-- each system definition as enabled. This would make initialization look
+-- like;
+--
+-- if not dct.systems.isEnabled(dct.systems.BlastEffects) then
+--     dct.systems.setEnabled(dct.systems.BlastEffects, true)
+-- end
+-- local theater = Theater()
+-- -- non-DCT systems can call Theater methods systemRegister and
+-- -- systemOverride to replace or add to the systems run.
+--
+-- How is this different from just passing a table to the Theater
+-- constructor?
+-- sysstates = {
+--   ["BlastEffects"] = true,
+--   ["WeaponsTracking"] = false,
+-- }
+-- local theater = Theater(sysstates)
+-- -- will not forcefully override another system and throw an error if
+-- the system of the same name is already registered.
+-- theater:systemRegister(AnInstanceOfMySpecialNewSystem)
+-- -- will replace a core system with the specified replacement
+-- theater:systemOverride(AnInstanceOfMySpecialTicketSystem)
+-- theater:initialize()
+--
+-- And the sysstates table could come from server configuration to allow
+-- server admins to prevent the use of certain systems.
 
 --- Provides facilities for reading theater level configuration.
 -- @submodule dct.settings
@@ -12,154 +51,23 @@ local class    = libs.classnamed
 local utils    = libs.utils
 local dctenum  = require("dct.enum")
 local dctutils = require("dct.libs.utils")
-local uihuman  = require("dct.ui.human")
-local Checker  = require("dct.templates.checkers.Check")
+local Check    = require("dct.libs.Check")
+local playerui = require("dct.settings.data.player")
 
-local defaultpayload = {}
-for _, v in pairs(dctenum.weaponCategory) do
-	defaultpayload[v] = dctenum.WPNINFCOST - 1
-end
+local validators = {
+	["restrictedweapons"] = check_restricted_weapons,
+	["blasteffects"]      = todo,
+	["agents"]            = todo,
+	["general"]           = todo,
+}
 
-local CheckPerEntry = class("CheckPerEntry", Checker)
-function CheckPerEntry:check(data)
-	for key, val in pairs(data) do
-		local ok, errkey, msg = Checker.check(self, val)
-		if not ok then
-			return false, string.format(
-				"%s: invalid `%s` %s", tostring(key),
-				tostring(errkey), tostring(msg))
-		end
-	end
-	return true
-end
-
-local function validate_weapon_restrictions(cfg, tbl)
-	local check = CheckPerEntry(nil, {
-			["cost"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.INT,
-			},
-			["category"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = dctenum.weaponCategory,
-			},
-		})
-
+local function generic_validator(cfg, tbl)
+        local check = 1-- TODO: lookup validator
 	local ok, msg = check:check(tbl)
 	if not ok then
 		error(string.format("%s; file: %s", msg, cfg.file))
 	end
 	return tbl
-end
-
---- validates payload limits configuration
--- This is per airframe with a "default" entry in case an airframe is
--- not defined. The default is unlimited.
-local function validate_payload_limits(cfg, tbl)
-	local newlimits = {}
-
-	for planetype, limits in pairs(tbl) do
-		newlimits[planetype] = {}
-		local tmptbl = {}
-
-		for wpncat, val in pairs(limits) do
-			local w = dctenum.weaponCategory[string.upper(wpncat)]
-
-			if w == nil then
-				error(string.format(
-					"invalid weapon category '%s' - "..
-					"plane type %s; file: %s",
-					wpncat, planetype, cfg.file))
-			end
-			tmptbl[w] = val
-		end
-		utils.mergetables(newlimits[planetype], defaultpayload)
-		utils.mergetables(newlimits[planetype], tmptbl)
-	end
-	newlimits["default"] = defaultpayload
-	return newlimits
-end
-
-local function validate_codenamedb(cfg, tbl)
-	local newtbl = {}
-	for key, list in pairs(tbl) do
-		local newkey
-		local upper = string.upper(key)
-		local k = dctenum.assetType[upper]
-
-		if k == nil then
-			k = dctenum.assetTypeDeprecated[upper]
-		end
-
-		if k ~= nil then
-			newkey = k
-		elseif key == "default" then
-			newkey = key
-		else
-			error(string.format("invalid codename category "..
-				"'%s'; file: %s", key, cfg.file))
-		end
-
-		if type(list) ~= "table" then
-			error(string.format("invalid codename value for "..
-				"category '%s', must be a table; file: %s",
-				key, cfg.file))
-		end
-		newtbl[newkey] = list
-	end
-	return newtbl
-end
-
-local function validate_ui(cfg, tbl)
-	local check = CheckPerEntry(nil, {
-			["gridfmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.posfmt,
-				["default"] = uihuman.posfmt.DMS,
-			},
-			["distfmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.distancefmt,
-				["default"] = uihuman.distancefmt.NAUTICALMILE,
-			},
-			["altfmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.altfmt,
-				["default"] = uihuman.altfmt.FEET,
-			},
-			["speedfmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.speedfmt,
-				["default"] = uihuman.speedfmt.KNOTS,
-			},
-			["pressurefmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.pressurefmt,
-				["default"] = uihuman.pressurefmt.INHG,
-			},
-			["tempfmt"] = {
-				["nodoc"] = true,
-				["type"] = Checker.valuetype.TABLEKEYS,
-				["values"] = uihuman.tempfmt,
-				["default"] = uihuman.tempfmt.F,
-			},
-		})
-	local newtbl = {}
-
-	utils.mergetables(newtbl, cfg.default)
-	utils.mergetables(newtbl, tbl)
-
-	local ok, msg = check:check(newtbl)
-	if not ok then
-		error(string.format("%s; file: %s", msg, cfg.file))
-	end
-	return newtbl
 end
 
 local function merge_defaults(cfgdata, tbl)
@@ -169,45 +77,19 @@ local function merge_defaults(cfgdata, tbl)
 	return newtbl
 end
 
-local function validate_ato(cfg, tbl)
-	local ntbl = {}
-
-	for ac, mlist in pairs(tbl) do
-		local ok, nlist = dctutils.check_ato(mlist)
-
-		if not ok then
-			error(string.format("%s; aircraft: %s; file: %s",
-				nlist, ac, cfg.file))
-		end
-		ntbl[ac] = nlist
-	end
-	return ntbl
-end
-
-local function validate_cost(cfg, tbl)
-	for ac, cost in pairs(tbl) do
-		if type(cost) ~= "number" then
-			error(string.format(
-				"cost not a number; aircraft: %s; file: %s",
-				ac, cfg.file))
-		end
-	end
-	return tbl
-end
-
 local function validate_general(cfg, tbl)
-	local check = Checker(nil, {
+	local check = Check(nil, {
 			["airbase_nosilence"] = {
 				["description"] = [[
 This setting will determine if the ATC tower is disabled by default
 until a DCT airbase object takes ownership. By default all airbases
 are silenced unless DCT is controlling the airbase. Set to true to
 keep DCS's ATC system on for all airbases, including ships.]],
-				["type"] = Checker.valuetype.BOOL,
+				["type"] = Check.valuetype.BOOL,
 				["default"] = false,
 			},
 		})
-	local ok, msg = check.check(tbl)
+	local ok, msg = check:check(tbl)
 
 	if not ok then
 		error(string.format("%s; file: %s", msg, cfg.file))
@@ -215,67 +97,11 @@ keep DCS's ATC system on for all airbases, including ships.]],
 	return tbl
 end
 
--- We have a few levels of configuration:
--- 	* server defined config file; <dcs-saved-games>/Config/dct.cfg
--- 	* theater defined configuration; <theater-path>/settings/<config-files>
--- 	* default config values
--- simple algorithm; assign the defaults, then apply the server and
--- theater configs
 local function theatercfgs(config)
 	local basepath = utils.join_paths(config.server.theaterpath,
 					  "settings")
 	local cfgs = {
 		{
-			["name"] = "restrictedweapons",
-			["file"] = utils.join_paths(basepath,
-					"restrictedweapons.cfg"),
-			["cfgtblname"] = "restrictedweapons",
-			["validate"] = validate_weapon_restrictions,
-			["default"] = require("dct.data.restrictedweapons"),
-			["env"] = {
-				["INFCOST"] = dctenum.WPNINFCOST,
-			},
-		}, {
-			["name"] = "payloadlimits",
-			["file"] = utils.join_paths(basepath,
-						    "payloadlimits.cfg"),
-			["cfgtblname"] = "payloadlimits",
-			["validate"] = validate_payload_limits,
-		}, {
-			["name"] = "codenamedb",
-			["file"] = utils.join_paths(basepath,
-						    "codenamedb.cfg"),
-			["validate"] = validate_codenamedb,
-			["default"] = require("dct.data.codenamedb"),
-		}, {
-			["name"] = "ui",
-			["file"] = utils.join_paths(basepath, "ui.cfg"),
-			["validate"] = validate_ui,
-			["cfgtblname"] = "ui",
-			["default"] = require("dct.data.playerui"),
-		}, {
-			["name"] = "blasteffects",
-			["file"] = utils.join_paths(basepath,
-						    "blasteffects.cfg"),
-			["validate"] = merge_defaults,
-			["default"] = require("dct.data.blasteffects"),
-		}, {
-			["name"] = "agents",
-			["file"] = utils.join_paths(basepath, "agents.cfg"),
-			["validate"] = merge_defaults,
-			["default"] = require("dct.data.agents"),
-		}, {
-			["name"] = "ato",
-			["file"] = utils.join_paths(basepath, "ato.cfg"),
-			["validate"] = validate_ato,
-			["cfgtblname"] = "ato",
-		}, {
-			["name"] = "airframecost",
-			["file"] = utils.join_paths(basepath,
-						    "airframecost.cfg"),
-			["validate"] = validate_cost,
-			["cfgtblname"] = "cost",
-		}, {
 			["name"] = "general",
 			["file"] = utils.join_paths(basepath,
 						    "general.cfg"),
@@ -292,17 +118,5 @@ end
 -- @table theater
 -- @field one blah
 local theater = {}
-
--- settings.theater.
---   * features.
---   * playeraircraft.
---     - table keyed on aircraft type maps to table of values for a player aircraft;
---       cost, ui,
---   * restrictedweapons.
---     - table keyed on weapon type maps to table of attributes (cost, category)
---   * codenamedb.
---     - table keyed on asset type maps to a name list
---   * blasteffects.
---     - table keyed on weapon type maps to explosive mass
 
 return theatercfgs
