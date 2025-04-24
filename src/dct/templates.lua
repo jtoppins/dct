df78b077003c1fa59e61867ad7d1da8217acce44
Jonathan Toppins <jtoppins@users.noreply.github.com>
2025-04-24 01:33:04 -0400

    templates: enable template module

---
+++ src/dct/templates.lua
@@ -0,0 +1,10 @@
+-- SPDX-License-Identifier: LGPL-3.0
+
+local __a = {}
+
+__a.checkers   = require("dct.templates.checkers")
+__a.extract    = require("dct.templates.extract")
+__a.MIZ        = require("dct.templates.MIZ")
+__a.Template   = require("dct.templates.Template")
+
+return __a
