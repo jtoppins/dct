c632a5798fe100b68af30900f0c2041473154f16
Jonathan Toppins <jtoppins@users.noreply.github.com>
2025-05-17 20:57:49 -0400

    dcsstubs: add two new AI.Option values

--- tests/testlibs/dcsstubs.lua
+++ tests/testlibs/dcsstubs.lua
@@ -308,6 +308,8 @@ AI.Option = {
 			-- OPTION_RADIO_* value: attribute list
 			["JETT_TANKS_IF_EMPTY"]     = 25,
 			["FORCED_ATTACK"]           = 26,
+			["PREFER_VERTICAL"]         = 32,
+			["ALLOW_FORMATION_SIDE_SWAP"] = 35,
 		},
 		["val"] = {
 			-- ROE Descriptions:
